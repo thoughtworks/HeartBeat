bb03ae69486094feddf286dc83b3edaf0d23afc6
chaoyangyes <chao.yang1@thoughtworks.com>
2022-07-27 18:17:11 +0800

    feat: calculate percentage of different block reasons in the latest iteration

--- backend/src/services/GenerateReporter/GenerateSprintReporterService.ts
+++ backend/src/services/GenerateReporter/GenerateSprintReporterService.ts
@@ -5,6 +5,7 @@ import {
 } from "../../contract/GenerateReporter/GenerateReporterRequestBody";
 import { StoryPointsAndCycleTimeRequest } from "../../contract/kanban/KanbanStoryPointParameterVerify";
 import { JiraCardResponse } from "../../contract/kanban/KanbanStoryPointResponse";
+import { JiraBlockReasonEnum } from "../../models/kanban/JiraBlockReasonEnum";
 import { Cards } from "../../models/kanban/RequestKanbanResults";
 import { Sprint } from "../../models/kanban/Sprint";
 import { CalculateCardCycleTime } from "../kanban/CalculateCycleTime";
@@ -19,6 +20,7 @@ const KanbanKeyIdentifierMap: { [key: string]: "projectKey" | "teamName" } = {
 
 export class GenerateSprintReporterService {
   private cards?: Cards;
+  private sprints?: Sprint[];
   private blockPercentage?: Map<string, any>;
   async fetchIterationInfoFromKanban(
     request: GenerateReportRequest
@@ -29,36 +31,117 @@ export class GenerateSprintReporterService {
       kanbanSetting.token,
       kanbanSetting.site
     );
-    let model: StoryPointsAndCycleTimeRequest =
-      new StoryPointsAndCycleTimeRequest(
-        kanbanSetting.token,
-        kanbanSetting.type,
-        kanbanSetting.site,
-        kanbanSetting[KanbanKeyIdentifierMap[kanbanSetting.type]],
-        kanbanSetting.boardId,
-        kanbanSetting.doneColumn,
-        request.startTime,
-        request.endTime,
-        kanbanSetting.targetFields,
-        kanbanSetting.treatFlagCardAsBlock
-      );
+    let model: StoryPointsAndCycleTimeRequest = new StoryPointsAndCycleTimeRequest(
+      kanbanSetting.token,
+      kanbanSetting.type,
+      kanbanSetting.site,
+      kanbanSetting[KanbanKeyIdentifierMap[kanbanSetting.type]],
+      kanbanSetting.boardId,
+      kanbanSetting.doneColumn,
+      request.startTime,
+      request.endTime,
+      kanbanSetting.targetFields,
+      kanbanSetting.treatFlagCardAsBlock
+    );
     this.cards = await kanban.getStoryPointsAndCycleTime(
       model,
       kanbanSetting.boardColumns,
       kanbanSetting.users
     );
     if (kanban instanceof Jira) {
-      let sprintPercentageMap: Map<string, any> =
-        this.calculateIterationBlockedAndDevelopingPercentage(
-          this.mapCardsByIteration(this.cards.matchedCards),
-          kanbanSetting.boardColumns
-        );
+      let sprintPercentageMap: Map<
+        string,
+        any
+      > = this.calculateIterationBlockedAndDevelopingPercentage(
+        this.mapCardsByIteration(this.cards.matchedCards),
+        kanbanSetting.boardColumns
+      );
       let sprints: Sprint[] = await kanban.getAllSprintsByBoardId(model);
       this.blockPercentage = this.sortBySprintStartDate(
         sprintPercentageMap,
         sprints
       );
     }
+  }
+  getLatestIterationSprintName(sprints: Sprint[]): string {
+    const sortedSprints = sprints
+      .filter((sprint) => sprint.startDate)
+      .sort((a, b) => Date.parse(a.startDate) - Date.parse(b.startDate));
+    return sortedSprints.length > 0
+      ? sortedSprints[sortedSprints.length - 1].name
+      : "";
+  }
+
+  getAllCardsByLatestSprintName(
+    cards: JiraCardResponse[],
+    sprintName: string
+  ): JiraCardResponse[] {
+    const allCardsInLatestSprint: JiraCardResponse[] = [];
+    cards.forEach((card) => {
+      if (card.baseInfo.fields.sprint === sprintName) {
+        allCardsInLatestSprint[allCardsInLatestSprint.length] = card;
+      }
+    });
+    return allCardsInLatestSprint;
+  }
+
+  calculateBlockReasonPercentage(
+    cards: JiraCardResponse[],
+    boardColumns: RequestKanbanColumnSetting[] = [],
+    sprints: Sprint[]
+  ): Map<string, number> {
+    let totalBlockTime = 0;
+
+    const initBlockTimeForEveryReasonMap: Map<
+      string,
+      number
+    > = this.initTotalBlockTimeForEveryReasonMap();
+    const sprintName = this.getLatestIterationSprintName(sprints);
+    const matchedCards = this.getAllCardsByLatestSprintName(cards, sprintName);
+    if (matchedCards.length === 0) {
+      return initBlockTimeForEveryReasonMap;
+    }
+
+    for (const card of matchedCards) {
+      const blockReason = card.baseInfo.fields.label || "";
+      const cardCycleTime = CalculateCardCycleTime(card, boardColumns);
+      totalBlockTime += cardCycleTime.steps.blocked;
+
+      if (!initBlockTimeForEveryReasonMap.has(blockReason)) {
+        initBlockTimeForEveryReasonMap.set(
+          JiraBlockReasonEnum.OTHERS,
+          initBlockTimeForEveryReasonMap.get(JiraBlockReasonEnum.OTHERS) ||
+            0 + cardCycleTime.steps.blocked
+        );
+      } else {
+        initBlockTimeForEveryReasonMap.set(
+          blockReason,
+          initBlockTimeForEveryReasonMap.get(blockReason) ||
+            0 + cardCycleTime.steps.blocked
+        );
+      }
+    }
+
+    const blockTimePercentageForEveryReasonMap: Map<string, number> = new Map<
+      string,
+      number
+    >();
+
+    initBlockTimeForEveryReasonMap.forEach((value, key) => {
+      const blockedReasonPercentage = parseFloat(
+        (Math.floor((value / totalBlockTime) * 100) / 100).toFixed(2)
+      );
+      blockTimePercentageForEveryReasonMap.set(key, blockedReasonPercentage);
+    });
+    return blockTimePercentageForEveryReasonMap;
+  }
+
+  initTotalBlockTimeForEveryReasonMap(): Map<string, number> {
+    const totalBlockTimeForEveryReasonMap = new Map<string, number>();
+    for (let reason in JiraBlockReasonEnum) {
+      totalBlockTimeForEveryReasonMap.set(reason, 0);
+    }
+    return totalBlockTimeForEveryReasonMap;
   }
   mapCardsByIteration(
     cards: JiraCardResponse[]

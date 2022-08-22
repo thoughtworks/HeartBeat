ce7ac5b25337ba87151f6344d69dbe8695dd3259
WenZhaosen <110078167+WenZhaosen@users.noreply.github.com>
2022-08-22 15:46:37 +0800

    [ADM-244] fix: correct the import path.

--- frontend/src/app/dora/components/config-stepper/reports/classification/classification.component.spec.ts
+++ frontend/src/app/dora/components/config-stepper/reports/classification/classification.component.spec.ts
@@ -1,6 +1,6 @@
 /* tslint:disable:no-unused-variable */
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { ClassificationReportComponent } from '../../..';
+import { ClassificationReportComponent } from './classification.component';
 
 describe('ReportsClassificationComponent', () => {
   let component: ClassificationReportComponent;

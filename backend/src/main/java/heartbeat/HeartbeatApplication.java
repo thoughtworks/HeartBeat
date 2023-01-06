2a2a6d0ab6e0ff73f38f17203003a35b2ebe9426
huijunnn <huijun.hong@thoughtworks.com>
2023-01-06 14:41:22 +0800

    ADM-338 add swagger dependency

--- backend/src/main/java/heartbeat/HeartbeatApplication.java
+++ backend/src/main/java/heartbeat/HeartbeatApplication.java
@@ -1,9 +1,13 @@
 package heartbeat;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Products API", version = "3.0", description = "Products Information"))
 public class HeartbeatApplication {
 
 	public static void main(String[] args) {

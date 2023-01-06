37d84cfbb0a1c588f8242378f8676a33f63f5a59
Devonzhang <devonzhang1024@gmail.com>
2023-01-06 16:52:52 +0800

    ADM-338 add log4j2 dependency and log pattern

--- backend/src/main/java/heartbeat/service/SimpleService.java
+++ backend/src/main/java/heartbeat/service/SimpleService.java
@@ -1,8 +1,23 @@
 package heartbeat.service;
 
+import jakarta.annotation.PostConstruct;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Service;
+
+@Service
+@Log4j2
 public class SimpleService {
 
     public String hello() {
         return "Hello World";
+    }
+
+    @PostConstruct
+    public void showLog() {
+        log.trace("A TRACE Message");
+        log.debug("A DEBUG Message");
+        log.info("An INFO Message");
+        log.warn("A WARN Message");
+        log.error("An ERROR Message");
     }
 }

e2199ee9b1b9eab60d1dbf7a43f12a859f55d81c
Devonzhang <devonzhang1024@gmail.com>
2023-01-06 11:05:54 +0800

    ADM-338 add spring backend package

---
+++ backend/src/main/java/heartbeat/HeartbeatApplication.java
@@ -0,0 +1,13 @@
+package heartbeat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeartbeatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeartbeatApplication.class, args);
+	}
+
+}

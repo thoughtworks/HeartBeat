7ea4eb75fb7f482c557e04a2067f434c0d0f57c2
Lanxin Wang <lanxin.wang@thoughtworks.com>
2023-01-06 10:36:56 +0800

    ADM-336 deleted frontend project

--- frontend/src/app/core/core.module.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { HttpClientModule } from '@angular/common/http';
-
-@NgModule({
-  declarations: [],
-  imports: [CommonModule, HttpClientModule],
-})
-export class CoreModule {}

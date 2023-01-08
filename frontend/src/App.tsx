3bd469fa0bacc8799b6756fb40052789dcc0f2c7
fengxinhou <fengxin.hou@thoughtworks.com>
2023-01-08 10:13:05 +0800

    ADM-336 install Material UI and test succeeded

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,10 @@
+import { Button } from '@mui/material';
+
 function App() {
   return (
     <div>
       <p>Hello World</p>
+      <Button variant="contained">Import Project</Button>
     </div>
   );
 }

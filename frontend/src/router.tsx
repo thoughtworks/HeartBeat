d366628c54688761845948f36b3829baba8e3252
fengxinhou <fengxin.hou@thoughtworks.com>
2023-01-09 09:47:35 +0800

    ADM-336 optimizing page interaction using suspense component

---
+++ frontend/src/router.tsx
@@ -0,0 +1,16 @@
+import { Suspense } from 'react';
+import { Route, Routes } from 'react-router-dom';
+import routes from './config/routes';
+
+const Router = () => {
+  const myRoutes = routes.map((item) => {
+    return <Route key={item.path} path={item.path} element={<item.component />} />;
+  });
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <Routes>{myRoutes}</Routes>
+    </Suspense>
+  );
+};
+
+export default Router;

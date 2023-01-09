126dce8ee3342979926ede26034c94cbab02400b
fengxinhou <fengxin.hou@thoughtworks.com>
2023-01-09 09:48:38 +0800

    ADM-336 load the routing component in the App component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,19 @@
 import { Button } from '@mui/material';
 import './App.css';
+import { BrowserRouter, Link } from 'react-router-dom';
+import Router from './router';
 function App() {
   return (
     <div className="App">
       <p>Hello World</p>
       <Button variant="contained">Import Project</Button>
+      <hr />
+      <BrowserRouter>
+        <Link to="/">Home Page</Link>
+        <br />
+        <Link to="/about">About Page</Link>
+        <Router />
+      </BrowserRouter>
     </div>
   );
 }

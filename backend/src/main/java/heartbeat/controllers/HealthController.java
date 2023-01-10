d0d89247d4df9b77c3aa2fa4ff82ec88107f11f0
Devonzhang <devonzhang1024@gmail.com>
2023-01-10 10:53:24 +0800

    ADM-338 add health endpoint

---
+++ backend/src/main/java/heartbeat/controllers/HealthController.java
@@ -0,0 +1,20 @@
+package heartbeat.controllers;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.actuate.health.HealthComponent;
+import org.springframework.boot.actuate.health.HealthEndpoint;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/health")
+@RequiredArgsConstructor
+public class HealthController {
+    private final HealthEndpoint healthEndpoint;
+
+    @GetMapping
+    public HealthComponent getHealthStatus() {
+        return healthEndpoint.health();
+    }
+}

a3bd8c94c5ca5051ae63e8aab85c6b2d4799e865
fengxinhou <fengxin.hou@thoughtworks.com>
2023-01-10 14:23:09 +0800

    ADM-336 rename app as hooks folder

--- frontend/__tests__/src/App.test.tsx
+++ frontend/__tests__/src/App.test.tsx
@@ -5,8 +5,8 @@ import { Provider } from 'react-redux';
 import { index } from '../../src/store';
 import React from 'react';
 
-describe('render app', () => {
-  test('should show hello World when render app component', () => {
+describe('render hooks', () => {
+  test('should show hello World when render hooks component', () => {
     const { getByText } = render(
       <Provider store={index}>
         <App />

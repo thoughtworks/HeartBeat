b9941fb88b42b5e0f03d259801a779c51f5657da
fengxinhou <fengxin.hou@thoughtworks.com>
2023-01-10 10:39:22 +0800

    ADM-336 rename myRoutes to appRoutes

--- frontend/src/router.tsx
+++ frontend/src/router.tsx
@@ -3,12 +3,12 @@ import { Route, Routes } from 'react-router-dom';
 import routes from './config/routes';
 
 const Router = () => {
-  const myRoutes = routes.map((item) => {
+  const appRoutes = routes.map((item) => {
     return <Route key={item.path} path={item.path} element={<item.component />} />;
   });
   return (
     <Suspense fallback={<div>Loading...</div>}>
-      <Routes>{myRoutes}</Routes>
+      <Routes>{appRoutes}</Routes>
     </Suspense>
   );
 };

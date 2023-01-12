49f3b26a27e9b3e61b69dc45b73b6c7b843ed773
Lanxin Wang <lanxin.wang@thoughtworks.com>
2023-01-12 13:14:03 +0800

    ADM-336 add app tests

--- frontend/__tests__/src/App.test.tsx
+++ frontend/__tests__/src/App.test.tsx
@@ -1,19 +1,40 @@
-import { render } from '@testing-library/react';
+import { fireEvent, render, waitFor } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import App from '@src/App';
 import { Provider } from 'react-redux';
 import { store } from '@src/store/store';
 import React from 'react';
 
-describe('render hooks', () => {
-  test('should show hello World when render hooks component', () => {
-    const { getByText } = render(
+describe('render app', () => {
+  const setup = () => {
+    return render(
       <Provider store={store}>
         <App />
       </Provider>
     );
+  };
+  it('should show hello World when render app', () => {
+    const { getByText } = setup();
     const text = getByText('Hello World');
 
     expect(text).toBeInTheDocument();
+  });
+
+  it('should show home page when click home page', async () => {
+    const { getByText } = setup();
+    fireEvent.click(getByText('Home Page'));
+
+    await waitFor(() => {
+      expect(getByText('This is Home Page')).toBeInTheDocument();
+    });
+  });
+
+  it('should show about page when click about page', async () => {
+    const { getByText } = setup();
+    fireEvent.click(getByText('About Page'));
+
+    await waitFor(() => {
+      expect(getByText('This is About Page')).toBeInTheDocument();
+    });
   });
 });

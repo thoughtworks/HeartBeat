d754cba82dcfac9d2cc3e9eed00d6ef793402f0e
guzhongren <guzhongren@live.cn>
2023-01-22 20:20:25 +0800

    ADM-355 fix e2e

--- frontend/e2e/app.test.ts
+++ frontend/e2e/app.test.ts
@@ -1,9 +1,11 @@
 import { test, expect } from '@playwright/test';
 
+const PROJECT_NAME = 'Heartbeat';
+
 test('Hello world', async ({ page }) => {
   await page.goto('/index.html');
-  const locator = page.locator('.title');
 
-  expect(locator).toContainText(['Hello World']);
-  await expect(locator).toBeVisible();
+  const projectTitle = page.getByText(PROJECT_NAME);
+
+  await expect(projectTitle).toBeVisible();
 });

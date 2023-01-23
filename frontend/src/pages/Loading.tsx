8cc831864f634f6e824faa3679ea511a60501776
guzhongren <guzhongren@live.cn>
2023-01-23 10:31:54 +0800

    ADM-355 replace css with styled for Loading page

--- frontend/src/pages/Loading.tsx
+++ frontend/src/pages/Loading.tsx
@@ -1,12 +1,43 @@
-import RotateRightIcon from '@mui/icons-material/RotateRight';
+import styled from '@emotion/styled';
+import { keyframes } from '@emotion/react';
+import RotateRight from '@mui/icons-material/RotateRight';
+
+const LoadingWarp = styled.div({
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  height: '100%',
+  width: '100%',
+});
+
+const LoadingContainer = styled.span({
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+});
+
+const rotateKeyframe = keyframes`
+  0 {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+`;
+
+const RotateRightIcon = styled(RotateRight)({
+  height: '4rem',
+  width: '4rem',
+  animation: `${rotateKeyframe} 1s linear infinite`,
+});
 
 export const Loading = () => {
   return (
-    <div className='h-full w-full flex items-center justify-center'>
-      <span className='flex flex-col items-center'>
-        <RotateRightIcon className='animate-spin text-lg' />
+    <LoadingWarp>
+      <LoadingContainer>
+        <RotateRightIcon />
         <span>Loading...</span>
-      </span>
-    </div>
+      </LoadingContainer>
+    </LoadingWarp>
   );
 };

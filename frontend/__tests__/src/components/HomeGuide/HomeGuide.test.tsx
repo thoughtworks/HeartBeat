d7928026b4d204457ad0e0b35d2d0cb5de90dcf6
fengxinhou <fengxin.hou@thoughtworks.com>
2023-01-28 15:59:10 +0800

    ADM-357 add test about HomeGuide file

--- frontend/__tests__/src/components/HomeGuide/HomeGuide.test.tsx
+++ frontend/__tests__/src/components/HomeGuide/HomeGuide.test.tsx
@@ -1,8 +1,23 @@
 import HomeGuide from '@src/components/HomeGuide'
-import { render } from '@testing-library/react'
+import { fireEvent, render } from '@testing-library/react'
+import '@testing-library/jest-dom'
+import * as router from 'react-router'
 
 describe('HomeGuide', () => {
   it('should show 2 buttons', () => {
-    render(<HomeGuide />)
+    const { getByText } = render(<HomeGuide />)
+
+    expect(getByText('Import project from file')).toBeInTheDocument()
+    expect(getByText('Create a new project')).toBeInTheDocument()
+  })
+  it('should go to metrics page when click create a new project button', async () => {
+    const navigate = jest.fn()
+    jest.spyOn(router, 'useNavigate').mockImplementation(() => navigate)
+    const { getByText } = render(<HomeGuide />)
+
+    fireEvent.click(getByText('Create a new project'))
+
+    expect(navigate).toHaveBeenCalledTimes(1)
+    expect(navigate).toHaveBeenCalledWith('/metrics')
   })
 })

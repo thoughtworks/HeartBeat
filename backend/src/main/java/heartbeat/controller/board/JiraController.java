2e2eda7e2c88928f9151d913330d7ee3a1616d7e
Devonzhang <devonzhang1024@gmail.com>
2023-01-29 10:57:56 +0800

    ADM-360 add jira controller to get jira board config

---
+++ backend/src/main/java/heartbeat/controller/board/JiraController.java
@@ -0,0 +1,22 @@
+package heartbeat.controller.board;
+
+import heartbeat.controller.board.vo.request.BoardRequest;
+import heartbeat.controller.board.vo.response.BoardConfigResponse;
+import heartbeat.service.board.jira.JiraService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/board/jira")
+public class JiraController {
+	private final JiraService jiraService;
+
+	@PostMapping("/config")
+	public BoardConfigResponse getJiraBoardConfig(@RequestBody BoardRequest boardRequest) {
+		return jiraService.getJiraReconfiguration(boardRequest);
+	}
+}

e4a8f74130cefc35fea3411c9a6205fcada9e55e
Lanxin Wang <lanxin.wang@thoughtworks.com>
2023-01-30 17:38:06 +0800

    ADM-357 add stepper to store

--- frontend/src/store/store.ts
+++ frontend/src/store/store.ts
@@ -1,8 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit'
-import counterReducer from '../features/counter/counterSlice'
+import stepperReducer from '../features/stepper/StepperSlice'
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
+    stepper: stepperReducer,
   },
 })
 

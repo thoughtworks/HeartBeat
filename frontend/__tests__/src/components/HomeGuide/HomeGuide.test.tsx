d739bb0de2e89b1520c499fea05aaaa710439e46
fengxinhou <fengxin.hou@thoughtworks.com>
2023-01-30 11:00:15 +0800

    ADM-357 refactor extract constant and change css style

--- frontend/__tests__/src/components/HomeGuide/HomeGuide.test.tsx
+++ frontend/__tests__/src/components/HomeGuide/HomeGuide.test.tsx
@@ -3,18 +3,20 @@ import { fireEvent, render } from '@testing-library/react'
 import '@testing-library/jest-dom'
 import * as router from 'react-router-dom'
 
+const CREATE_NEW_PROJECT = 'Create a new project'
 describe('HomeGuide', () => {
   it('should show 2 buttons', () => {
     const { getByText } = render(<HomeGuide />)
+
     expect(getByText('Import project from file')).toBeInTheDocument()
-    expect(getByText('Create a new project')).toBeInTheDocument()
+    expect(getByText(CREATE_NEW_PROJECT)).toBeInTheDocument()
   })
   it('should go to metrics page when click create a new project button', async () => {
     const navigate = jest.fn()
     jest.spyOn(router, 'useNavigate').mockImplementation(() => navigate)
     const { getByText } = render(<HomeGuide />)
 
-    fireEvent.click(getByText('Create a new project'))
+    fireEvent.click(getByText(CREATE_NEW_PROJECT))
 
     expect(navigate).toHaveBeenCalledTimes(1)
     expect(navigate).toHaveBeenCalledWith('/metrics')
--- frontend/__tests__/src/components/metrics/MetricsStepper/MetricsStepper.test.tsx
+++ frontend/__tests__/src/components/metrics/MetricsStepper/MetricsStepper.test.tsx
@@ -2,39 +2,43 @@ import { fireEvent, render } from '@testing-library/react'
 import MetricsStepper from '@src/components/metrics/MetricsStepper'
 import '@testing-library/jest-dom'
 
+const NEXT = 'Next'
+const BACK = 'Back'
+const ExportBoardData = 'Export board data'
+const steps = ['Config', 'Metrics', 'Export']
+
 describe('MetricsStepper', () => {
   it('should show metrics stepper', () => {
-    const steps = ['Config', 'Metrics', 'Export']
     const { getByText } = render(<MetricsStepper />)
 
     steps.map((label) => {
       expect(getByText(label)).toBeInTheDocument()
     })
-    expect(getByText('Next')).toBeInTheDocument()
-    expect(getByText('Back')).toBeInTheDocument()
+    expect(getByText(NEXT)).toBeInTheDocument()
+    expect(getByText(BACK)).toBeInTheDocument()
   })
 
   it('should show metrics stepper 2 when click next button given stepper 1', async () => {
     const { getByText } = render(<MetricsStepper />)
 
-    fireEvent.click(getByText('Next'))
+    fireEvent.click(getByText(NEXT))
     expect(getByText('Step 2')).toBeInTheDocument()
   })
 
   it('should show metrics stepper 1 when click back button given stepper 2', async () => {
     const { getByText } = render(<MetricsStepper />)
 
-    fireEvent.click(getByText('Next'))
-    fireEvent.click(getByText('Back'))
+    fireEvent.click(getByText(NEXT))
+    fireEvent.click(getByText(BACK))
     expect(getByText('Step 1')).toBeInTheDocument()
   })
 
   it('should show metrics stepper 3 when click next button given stepper 3', async () => {
     const { getByText } = render(<MetricsStepper />)
 
-    fireEvent.click(getByText('Next'))
-    fireEvent.click(getByText('Next'))
-    fireEvent.click(getByText('Export board data'))
+    fireEvent.click(getByText(NEXT))
+    fireEvent.click(getByText(NEXT))
+    fireEvent.click(getByText(ExportBoardData))
     expect(getByText('Step 3')).toBeInTheDocument()
   })
 })

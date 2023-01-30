e5a187671d99a364a803613f4b145f61c70c4f2b
Lanxin Wang <lanxin.wang@thoughtworks.com>
2023-01-30 17:37:07 +0800

    ADM-357 add a StepperSlice

---
+++ frontend/src/features/stepper/StepperSlice.tsx
@@ -0,0 +1,29 @@
+import { createSlice } from '@reduxjs/toolkit'
+import type { RootState } from '@src/store/store'
+
+export interface StepState {
+  value: number
+}
+
+const initialState: StepState = {
+  value: 0,
+}
+
+export const stepperSlice = createSlice({
+  name: 'stepper',
+  initialState,
+  reducers: {
+    nextStep: (state) => {
+      state.value += 1
+    },
+    backStep: (state) => {
+      state.value = state.value === 0 ? 0 : state.value - 1
+    },
+  },
+})
+
+export const { nextStep, backStep } = stepperSlice.actions
+
+export const selectStep = (state: RootState) => state.stepper.value
+
+export default stepperSlice.reducer

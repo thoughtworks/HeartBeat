9cf3ef4f19ed2603e12368bdc2549245a15f7329
fengxinhou <fengxin.hou@thoughtworks.com>
2023-01-30 11:01:52 +0800

    ADM-357 change theme color

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import { Provider } from 'react-redux'
 import { store } from './store/store'
+import { ThemeProvider } from '@mui/material'
+import theme from '@src/theme'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <ThemeProvider theme={theme}>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </ThemeProvider>
   </React.StrictMode>
 )

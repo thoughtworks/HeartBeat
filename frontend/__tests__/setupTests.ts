38260ab7db0efc87365dbba88bd6a17baeb891ed
guzhongren <guzhongren@live.cn>
2023-01-30 22:26:20 +0800

    ADM-357 fix eslint issue

--- frontend/__tests__/setupTests.ts
+++ frontend/__tests__/setupTests.ts
@@ -2,6 +2,6 @@ import '@testing-library/jest-dom'
 export const navigateMock = jest.fn()
 
 jest.mock('react-router-dom', () => ({
-  ...(jest.requireActual('react-router-dom') as any),
+  ...(jest.requireActual('react-router-dom') as Record<string, unknown>),
   useNavigate: () => navigateMock,
 }))

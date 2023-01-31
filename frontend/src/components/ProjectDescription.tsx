70dace4612d850c189f10ca27d59f9c9a36fe112
fengxinhou <fengxin.hou@thoughtworks.com>
2023-01-31 12:11:12 +0800

    ADM-357 fix export issue

--- frontend/src/components/ProjectDescription.tsx
+++ frontend/src/components/ProjectDescription.tsx
@@ -1,5 +1,5 @@
 import styled from '@emotion/styled'
-import theme from '@src/theme'
+import { theme } from '@src/theme'
 
 const DescriptionContainer = styled.p({
   padding: '1rem',
@@ -7,11 +7,10 @@ const DescriptionContainer = styled.p({
   boxShadow: `0px 9px 8px -5px ${theme.palette.grey[500]}`,
 })
 
-const ProjectDescription = () => {
+export const ProjectDescription = () => {
   return (
     <DescriptionContainer role='description'>
       {`Heartbeat is a tool for tracking project delivery metrics that can help you get a better understanding of delivery performance. This product allows you easily get all aspects of source data faster and more accurate to analyze team delivery performance which enables delivery teams and team leaders focusing on driving continuous improvement and enhancing team productivity and efficiency.`}
     </DescriptionContainer>
   )
 }
-export default ProjectDescription
--- frontend/src/router.tsx
+++ frontend/src/router.tsx
@@ -1,6 +1,6 @@
 import { Suspense } from 'react'
 import { Route, Routes } from 'react-router-dom'
-import routes from './config/routes'
+import { routes } from './config/routes'
 import { Loading } from './pages/Loading'
 
 const Router = () => {

116970f8181ceff673e5a672fb4c37d1c1424337
fengxinhou <fengxin.hou@thoughtworks.com>
2023-01-31 09:46:48 +0800

    ADM-357 refactor e2e test

--- frontend/e2e/src/app.test.ts
+++ frontend/e2e/src/app.test.ts
@@ -2,17 +2,20 @@ import { test, expect } from '@playwright/test'
 
 const PROJECT_NAME = 'Heartbeat'
 
-test('should have project title', async ({ page }) => {
+const startApp = async (page) => {
   await page.goto('/index.html')
+}
+
+test('should have project title', async ({ page }) => {
+  await startApp(page)
 
   const projectTitle = page.getByTitle(PROJECT_NAME)
 
   await expect(projectTitle).toBeVisible()
 })
 test('should create a new project', async ({ page }) => {
-  await page.goto('/index.html')
-
+  await startApp(page)
   await page.getByRole('button', { name: 'Create a new project' }).click()
 
-  await expect(page).toHaveURL(/.*metrics/)
+  await expect(page).toHaveURL(/.metrics/)
 })

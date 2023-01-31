43aadd9cbf70cfba54647e08d731143b9e462daf
fengxinhou <fengxin.hou@thoughtworks.com>
2023-01-31 10:40:53 +0800

    ADM-357 fix e2e metrics test

--- frontend/e2e/src/pages/metrics.test.ts
+++ frontend/e2e/src/pages/metrics.test.ts
@@ -2,49 +2,52 @@ import { expect, test } from '@playwright/test'
 
 const steps = ['Config', 'Metrics', 'Export']
 
-const createNewProject = async (page) => {
+const clickCreateNewProjectButton = async (page) => {
   await page.goto('/index.html')
   await page.getByRole('button', { name: 'Create a new project' }).click()
 }
 
-const nextStep = async (page) => {
+const clickNextButton = async (page) => {
   await page.getByRole('button', { name: 'Next' }).click()
 }
 
-const checkMetricsConfigPage = async (page) => {
+const checkConfigStepPage = async (page) => {
   steps.map(async (label) => {
     await expect(page.getByText(label)).toBeVisible()
   })
   await expect(page.getByText('Step 1')).toBeVisible()
-  await expect(page.getByText('Next')).toBeVisible()
-  await expect(page.getByText('Back')).toBeVisible()
+  await expect(page.getByRole('button', { name: 'Next' })).toBeVisible()
+  await expect(page.getByRole('button', { name: 'Back' })).toBeVisible()
 }
 
-const checkMetricsPage = async (page) => {
+const checkMetricsStepPage = async (page) => {
   steps.map(async (label) => {
     await expect(page.getByText(label)).toBeVisible()
   })
   await expect(page.getByText('Step 2')).toBeVisible()
-  await expect(page.getByText('Next')).toBeVisible()
-  await expect(page.getByText('Back')).toBeVisible()
+  await expect(page.getByRole('button', { name: 'Next' })).toBeVisible()
+  await expect(page.getByRole('button', { name: 'Back' })).toBeVisible()
 }
 
-const checkExpectExportPage = async (page) => {
+const checkExportStepPage = async (page) => {
+  steps.map(async (label) => {
+    await expect(page.getByText(label, { exact: true })).toBeVisible()
+  })
   await expect(page.getByText('Step 3')).toBeVisible()
-  await expect(page.getByText('Export board data')).toBeVisible()
-  await expect(page.getByText('Back')).toBeVisible()
+  await expect(page.getByRole('button', { name: 'Export board data' })).toBeVisible()
+  await expect(page.getByRole('button', { name: 'Back' })).toBeVisible()
 }
 
 test('should render metrics page', async ({ page }) => {
-  await createNewProject(page)
+  await clickCreateNewProjectButton(page)
 
-  await checkMetricsConfigPage(page)
+  await checkConfigStepPage(page)
 
-  await nextStep(page)
+  await clickNextButton(page)
 
-  await checkMetricsPage(page)
+  await checkMetricsStepPage(page)
 
-  await nextStep(page)
+  await clickNextButton(page)
 
-  await checkExpectExportPage(page)
+  await checkExportStepPage(page)
 })

8242c066117f2c8b3e49efbf264d7d8ce5d690f8
fengxinhou <fengxin.hou@thoughtworks.com>
2023-01-31 16:50:16 +0800

    ADM-358 add collection date

--- frontend/src/components/metrics/ConfigStep/index.tsx
+++ frontend/src/components/metrics/ConfigStep/index.tsx
@@ -1,19 +1,26 @@
-import { Box, TextField } from '@mui/material'
-import React from 'react'
+import { FormControl, FormControlLabel, Radio, RadioGroup, TextField } from '@mui/material'
+import React, { ChangeEvent, useState } from 'react'
+import { CHINA_CALENDAR, REGULAR_CALENDAR } from '@src/constants'
 
 export const ConfigStep = () => {
+  const [calendarType, setCalendarType] = useState(REGULAR_CALENDAR)
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setCalendarType((event.target as HTMLInputElement).value)
+  }
   return (
-    <Box
-      component='form'
-      sx={{
-        '& .MuiTextField-root': { m: 1, width: '25ch' },
-      }}
-      noValidate
-      autoComplete='off'
-    >
-      <div>
-        <TextField required id='standard-error-helper-text' label='Project Name' variant='standard' />
-      </div>
-    </Box>
+    <FormControl>
+      <TextField required id='standard-error-helper-text' label='Project Name' variant='standard' />
+      <h3>Collection Date</h3>
+      <RadioGroup
+        aria-labelledby='demo-controlled-radio-buttons-group'
+        name='controlled-radio-buttons-group'
+        value={calendarType}
+        onChange={handleChange}
+      >
+        <FormControlLabel value={REGULAR_CALENDAR} control={<Radio />} label={REGULAR_CALENDAR} />
+        <FormControlLabel value={CHINA_CALENDAR} control={<Radio />} label={CHINA_CALENDAR} />
+      </RadioGroup>
+    </FormControl>
   )
 }
--- frontend/src/constants.ts
+++ frontend/src/constants.ts
@@ -3,3 +3,7 @@ export const PROJECT_NAME = 'Heartbeat'
 export const FIVE_HUNDRED = 500
 
 export const ZERO = 0
+
+export const REGULAR_CALENDAR = 'Regular Calendar(Weekend Considered)'
+
+export const CHINA_CALENDAR = 'Calendar with Chinese Holiday'

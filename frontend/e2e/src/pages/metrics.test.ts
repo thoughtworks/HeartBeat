ca68227b7625e796c73958342820be27f926a868
fengxinhou <fengxin.hou@thoughtworks.com>
2023-02-05 10:42:26 +0800

    ADM-358 add e2e test to config step

--- frontend/e2e/src/pages/metrics.test.ts
+++ frontend/e2e/src/pages/metrics.test.ts
@@ -1,6 +1,8 @@
 import { expect, test } from '@playwright/test'
 
 const steps = ['Config', 'Metrics', 'Export']
+export const REGULAR_CALENDAR = 'Regular Calendar(Weekend Considered)'
+export const CHINA_CALENDAR = 'Calendar with Chinese Holiday'
 
 const clickCreateNewProjectButton = async (page) => {
   await page.goto('/index.html')
@@ -16,8 +18,28 @@ const checkConfigStepPage = async (page) => {
     await expect(page.getByText(label)).toBeVisible()
   })
   await expect(page.getByText('Project Name')).toBeVisible()
+
+  const defaultValue = page.getByRole('radio', { name: REGULAR_CALENDAR })
+  const regularCalendar = page.getByRole('radio', { name: REGULAR_CALENDAR })
+  const chinaCalendar = page.getByRole('radio', { name: CHINA_CALENDAR })
+
+  await expect(regularCalendar).toBeVisible()
+  await expect(chinaCalendar).toBeVisible()
+  await expect(defaultValue).toBeChecked()
+  await expect(chinaCalendar).not.toBeChecked()
+
   await expect(page.getByRole('button', { name: 'Next' })).toBeVisible()
   await expect(page.getByRole('button', { name: 'Back' })).toBeVisible()
+
+  chinaCalendar.click()
+
+  await expect(chinaCalendar).toBeChecked()
+  await expect(regularCalendar).not.toBeChecked()
+
+  regularCalendar.click()
+
+  await expect(regularCalendar).toBeChecked()
+  await expect(chinaCalendar).not.toBeChecked()
 }
 
 const checkMetricsStepPage = async (page) => {

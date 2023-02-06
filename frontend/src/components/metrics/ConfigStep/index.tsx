038151d74fc5972031bca4c4cb449b5c0433500b
fengxinhou <fengxin.hou@thoughtworks.com>
2023-02-06 17:18:17 +0800

    ADM-358 add require data and fix project name type

--- frontend/src/components/metrics/ConfigStep/index.tsx
+++ frontend/src/components/metrics/ConfigStep/index.tsx
@@ -1,14 +1,42 @@
-import { FormControl, FormControlLabel, Radio, RadioGroup, TextField } from '@mui/material'
+import {
+  Checkbox,
+  FormControl,
+  FormControlLabel,
+  FormHelperText,
+  InputLabel,
+  ListItemText,
+  MenuItem,
+  Radio,
+  RadioGroup,
+  Select,
+  SelectChangeEvent,
+  TextField,
+} from '@mui/material'
 import React, { useState } from 'react'
-import { CHINA_CALENDAR, REGULAR_CALENDAR } from '@src/constants'
+import { CHINA_CALENDAR, REGULAR_CALENDAR, REQUIRE_DATA } from '@src/constants'
 
 export const ConfigStep = () => {
-  const [projectName, setProjectName] = useState<undefined | string>(undefined)
-  const [isEmptyProjectName, setIsEmptyProjectName] = useState(false)
+  const [projectName, setProjectName] = useState<string>('')
+  const [isEmptyProjectName, setIsEmptyProjectName] = useState<boolean>(false)
+
+  const [requireData, setRequireData] = useState<string[]>([])
+  const [isEmptyRequireData, setIsEmptyProjectData] = useState<boolean>(false)
+  const changeRequireData = (event: SelectChangeEvent<typeof requireData>) => {
+    const {
+      target: { value },
+    } = event
+    setRequireData(typeof value === 'string' ? value.split(',') : value)
+    if (value.length === 0) {
+      setIsEmptyProjectData(true)
+    } else {
+      setIsEmptyProjectData(false)
+    }
+  }
 
   return (
-    <FormControl required variant='standard'>
+    <>
       <TextField
+        sx={{ m: 1, minWidth: 150, maxWidth: 500 }}
         required
         id='standard-error-helper-text'
         label='Project Name'
@@ -36,6 +64,26 @@ export const ConfigStep = () => {
         <FormControlLabel value={REGULAR_CALENDAR} control={<Radio />} label={REGULAR_CALENDAR} />
         <FormControlLabel value={CHINA_CALENDAR} control={<Radio />} label={CHINA_CALENDAR} />
       </RadioGroup>
-    </FormControl>
+      <FormControl variant='standard' required sx={{ m: 1, minWidth: 150, maxWidth: 500 }} error={isEmptyRequireData}>
+        <InputLabel id='demo-multiple-checkbox-label'>Require Data</InputLabel>
+        <Select
+          labelId='demo-multiple-checkbox-label'
+          id='demo-multiple-checkbox'
+          multiple
+          value={requireData}
+          onChange={changeRequireData}
+          label='Require Data'
+          renderValue={(selected) => selected.join(', ')}
+        >
+          {REQUIRE_DATA.map((data) => (
+            <MenuItem key={data} value={data}>
+              <Checkbox checked={requireData.indexOf(data) > -1} />
+              <ListItemText primary={data} />
+            </MenuItem>
+          ))}
+        </Select>
+        {isEmptyRequireData ? <FormHelperText>Metrics is required</FormHelperText> : null}
+      </FormControl>
+    </>
   )
 }

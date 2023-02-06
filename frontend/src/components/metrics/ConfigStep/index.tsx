97ce3ca2bb9d143e9be8ec1e84872b154bf48dd1
fengxinhou <fengxin.hou@thoughtworks.com>
2023-02-06 15:21:16 +0800

    ADM-358 add project name condition verification

--- frontend/src/components/metrics/ConfigStep/index.tsx
+++ frontend/src/components/metrics/ConfigStep/index.tsx
@@ -1,11 +1,31 @@
 import { FormControl, FormControlLabel, Radio, RadioGroup, TextField } from '@mui/material'
-import React from 'react'
+import React, { useState } from 'react'
 import { CHINA_CALENDAR, REGULAR_CALENDAR } from '@src/constants'
 
 export const ConfigStep = () => {
+  const [projectName, setProjectName] = useState<undefined | string>(undefined)
+  const [isEmptyProjectName, setIsEmptyProjectName] = useState(false)
+
   return (
-    <FormControl>
-      <TextField required id='standard-error-helper-text' label='Project Name' variant='standard' />
+    <FormControl required variant='standard'>
+      <TextField
+        required
+        id='standard-error-helper-text'
+        label='Project Name'
+        variant='standard'
+        value={projectName}
+        onChange={(e) => {
+          setProjectName(e.target.value)
+          if (e.target.value === '') {
+            setIsEmptyProjectName(true)
+          } else {
+            setIsEmptyProjectName(false)
+          }
+        }}
+        error={isEmptyProjectName}
+        helperText={isEmptyProjectName ? 'Project Name is required' : ''}
+      />
+
       <h3>Collection Date</h3>
       <RadioGroup
         data-testid='radio-test'

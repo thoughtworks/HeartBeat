4207b522d71cdce7a6f19fa0a11e18e014147fd7
Lanxin Wang <lanxin.wang@thoughtworks.com>
2023-02-08 14:03:11 +0800

    ADM-358 fix dateRangePicker tests

--- frontend/__tests__/src/components/metrics/ConfigStep/DateRangePicker.test.tsx
+++ frontend/__tests__/src/components/metrics/ConfigStep/DateRangePicker.test.tsx
@@ -1,6 +1,12 @@
 import { fireEvent, getByRole, render } from '@testing-library/react'
 import { DateRangePicker } from '@src/components/metrics/ConfigStep/DateRangePicker'
 
+const today = new Date()
+  .toLocaleDateString('en-US')
+  .split('/')
+  .map((num) => (Number(num) < 10 ? 0 + num : num))
+  .join('/')
+
 describe('DateRangePicker', () => {
   it('should render DateRangePicker', () => {
     const { getByText } = render(<DateRangePicker />)
@@ -12,28 +18,27 @@ describe('DateRangePicker', () => {
   it('should show right start date when input a valid date given init start date is null ', () => {
     const { getByRole } = render(<DateRangePicker />)
     const startDateInput = getByRole('textbox', { name: 'From' }) as HTMLInputElement
+    fireEvent.change(startDateInput, { target: { value: today } })
 
-    fireEvent.change(startDateInput, { target: { value: '03/02/2022' } })
-
-    expect(startDateInput.value).toEqual('03/02/2022')
+    expect(startDateInput.value).toEqual(today)
   })
 
   it('should show right end date when input a valid date given init end date is null ', () => {
     const { getByRole } = render(<DateRangePicker />)
     const endDateInput = getByRole('textbox', { name: 'To' }) as HTMLInputElement
 
-    fireEvent.change(endDateInput, { target: { value: '03/02/2022' } })
+    fireEvent.change(endDateInput, { target: { value: today } })
 
-    expect(endDateInput.value).toEqual('03/02/2022')
+    expect(endDateInput.value).toEqual(today)
   })
 
   it('should show error when input a invalid start date given init start date is null ', () => {
     const { getByRole, getByText } = render(<DateRangePicker />)
     const startDateInput = getByRole('textbox', { name: 'From' }) as HTMLInputElement
 
-    fireEvent.change(startDateInput, { target: { value: '03/02/20' } })
+    fireEvent.change(startDateInput, { target: { value: '02/03/' } })
 
-    expect(startDateInput.value).toEqual('03/02/20')
+    expect(startDateInput.value).toEqual('02/03/')
     expect(getByText('From')).toHaveStyle(`color: #d32f2f`)
   })
 
@@ -41,9 +46,9 @@ describe('DateRangePicker', () => {
     const { getByRole, getByText } = render(<DateRangePicker />)
     const endDateInput = getByRole('textbox', { name: 'To' }) as HTMLInputElement
 
-    fireEvent.change(endDateInput, { target: { value: '03/02/20' } })
+    fireEvent.change(endDateInput, { target: { value: '03/02/' } })
 
-    expect(endDateInput.value).toEqual('03/02/20')
+    expect(endDateInput.value).toEqual('03/02/')
     expect(getByText('To')).toHaveStyle(`color: #d32f2f`)
   })
 

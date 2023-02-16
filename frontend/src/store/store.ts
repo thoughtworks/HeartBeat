d6c7b67e8268b1b32b8361e5e10f6658945bbf9a
Lanxin Wang <lanxin.wang@thoughtworks.com>
2023-02-16 11:16:27 +0800

    ADM-359 add config Slice into store config

--- frontend/src/store/store.ts
+++ frontend/src/store/store.ts
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit'
 import stepperReducer from '../features/stepper/StepperSlice'
+import configReducer from '../features/config/configSlice'
 import boardReducer from '../features/board/boardSlice'
 export const store = configureStore({
   reducer: {
     stepper: stepperReducer,
+    config: configReducer,
     board: boardReducer,
   },
 })

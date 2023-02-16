54ecef601a8421cf3ffb3c21aed1bc08b2bc725b
Lanxin Wang <lanxin.wang@thoughtworks.com>
2023-02-16 11:54:05 +0800

    ADM-359 test for use redux to store calendar type

--- frontend/__tests__/src/features/configSlice.test.tsx
+++ frontend/__tests__/src/features/configSlice.test.tsx
@@ -1,20 +1,35 @@
-import configReducer, { updateProjectName } from '@src/features/config/configSlice'
+import configReducer, { updateCalendarType, updateProjectName } from '@src/features/config/configSlice'
+import { CHINA_CALENDAR, REGULAR_CALENDAR } from '../fixtures'
 
 describe('config reducer', () => {
-  it('should be empty project name when init render config page', () => {
+  it('should be default value when init render config page', () => {
     const config = configReducer(undefined, { type: 'unknown' })
 
     expect(config.projectName).toEqual('')
+    expect(config.calendarType).toEqual(REGULAR_CALENDAR)
   })
 
   it('should update project name when change project name input', () => {
     const config = configReducer(
       {
         projectName: '',
+        calendarType: REGULAR_CALENDAR,
       },
       updateProjectName('mock project name')
     )
 
     expect(config.projectName).toEqual('mock project name')
+  })
+
+  it('should update calendar when change calendar types', () => {
+    const config = configReducer(
+      {
+        projectName: '',
+        calendarType: REGULAR_CALENDAR,
+      },
+      updateCalendarType(CHINA_CALENDAR)
+    )
+
+    expect(config.calendarType).toEqual(CHINA_CALENDAR)
   })
 })

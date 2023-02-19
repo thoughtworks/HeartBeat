6bba3eca0a38174e4faffa27a9bd17e85642847f
fengxinhou <fengxin.hou@thoughtworks.com>
2023-02-19 14:17:18 +0800

    ADM-359 add board service test

---
+++ frontend/__tests__/src/service/BoardService.test.tsx
@@ -0,0 +1,17 @@
+import { verifyBoard } from '@src/service/boardService'
+import { setupServer } from 'msw/node'
+import { rest } from 'msw'
+
+const server = setupServer(
+  rest.get('https://jsonplaceholder.typicode.com/posts', (req, res, ctx) => {
+    return res(ctx.status(200))
+  })
+)
+describe('config step', () => {
+  beforeAll(() => server.listen())
+  afterAll(() => server.close())
+  it('should return 200 status when verifyBoard is succeed', async () => {
+    const result = await verifyBoard()
+    expect(result.status).toEqual(200)
+  })
+})

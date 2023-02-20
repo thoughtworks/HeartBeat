4aad3b828c98d950ee3795ab64eb8fe3ac448345
Guoqing Li <guoqing.li@thoughtworks.com>
2023-02-20 18:05:15 +0800

    ADM-363 processColumns.

--- backend/src/main/java/heartbeat/service/board/jira/JiraService.java
+++ backend/src/main/java/heartbeat/service/board/jira/JiraService.java
@@ -6,6 +6,7 @@ import feign.FeignException;
 import heartbeat.client.JiraFeignClient;
 import heartbeat.client.dto.JiraBoardConfigDTO;
 import heartbeat.client.dto.JiraColumn;
+import heartbeat.client.dto.JiraColumnStatus;
 import heartbeat.controller.board.vo.request.BoardRequest;
 import heartbeat.controller.board.vo.response.BoardConfigResponse;
 import heartbeat.controller.board.vo.response.ColumnResponse;
@@ -14,14 +15,19 @@ import heartbeat.controller.board.vo.response.StatusSelf;
 import heartbeat.exception.RequestFailedException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import lombok.val;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import java.net.URI;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
+
 @Service
 @RequiredArgsConstructor
 @Log4j2
@@ -36,28 +42,55 @@ public class JiraService {
 		JiraBoardConfigDTO jiraBoardConfigDTO;
 		try {
 			jiraBoardConfigDTO = jiraFeignClient.getJiraBoardConfiguration(URI.create(url), boardRequest.getBoardId(),
-					boardRequest.getToken());
+				boardRequest.getToken());
 
-//			jiraBoardConfigDTO.getColumnConfig().getColumns().stream()
-//				.map(jiraColumn -> jiraColumn.getStatuses().stream()
-//					.map(jiraColumnStatus -> getColumnStatusCategory(jiraColumnStatus.getSelf(), boardRequest.getToken()).getUntranslatedNames().stream()
-//						.map(String::toUpperCase)).toList().stream())
-//				.map()
-
-
+			val columns = jiraBoardConfigDTO.getColumnConfig().getColumns();
+			val processColumns = processColumns(columns, boardRequest.getToken());
 
 			return BoardConfigResponse.builder()
 				.jiraColumns(jiraBoardConfigDTO.getColumnConfig()
 					.getColumns()
 					.stream()
-					.map(this::mapToResponseColumn)
+				.map(this::mapToResponseColumn)
 					.collect(Collectors.toList()))
 				.build();
-		}
-		catch (FeignException e) {
+		} catch (FeignException e) {
 			log.error("Failed when call Jira to get board config", e);
 			throw new RequestFailedException(e);
 		}
+	}
+
+
+	private ArrayList<ColumnResponse> processColumns(List<JiraColumn> columns, String token) {
+		val jiraColumnNames = new ArrayList<ColumnResponse>();
+		RestTemplate restTemplate = new RestTemplate();
+
+		for (JiraColumn column : columns) {
+			if (column.getStatuses().size() > 0) {
+				ColumnValue columnValue = ColumnValue.builder().name(column.getName()).build();
+
+				for (JiraColumnStatus status : column.getStatuses()) {
+					HttpHeaders headers = new HttpHeaders();
+					headers.set(HttpHeaders.AUTHORIZATION, token);
+					HttpEntity<?> entity = new HttpEntity<>(headers);
+
+					ResponseEntity<StatusSelf> response = restTemplate.exchange(
+						status.getSelf(),
+						HttpMethod.GET,
+						entity,
+						StatusSelf.class
+					);
+
+					for (String untranslatedName : response.getBody().getUntranslatedNames()) {
+						String cardStatusName = untranslatedName.toUpperCase();
+						columnValue.getStatuses().add(cardStatusName);
+					}
+				}
+
+				jiraColumnNames.add(ColumnResponse.builder().value(columnValue).build());
+			}
+		}
+		return jiraColumnNames;
 	}
 
 	public StatusSelf getColumnStatusCategory(String url, String token) {
--- backend/src/test/java/heartbeat/service/jira/JiraBoardConfigDTOFixture.java
+++ backend/src/test/java/heartbeat/service/jira/JiraBoardConfigDTOFixture.java
@@ -17,14 +17,14 @@ public class JiraBoardConfigDTOFixture {
 
 	public static JiraBoardConfigDTO.JiraBoardConfigDTOBuilder JIRA_BOARD_CONFIG_RESPONSE_BUILDER() {
 
-		return JiraBoardConfigDTO.builder()
-			.id(BOARD_ID)
-			.name(BOARD_NAME)
-			.columnConfig(
-					JiraColumnConfig.builder().columns(List.of(JiraColumn.builder()
-					.name("TODO")
-					.statuses(List.of(new JiraColumnStatus("1", SELF)))
-					.build())).build());
+		return JiraBoardConfigDTO.builder().id(BOARD_ID).name(BOARD_NAME).columnConfig(
+			JiraColumnConfig.builder().columns(
+					List.of(JiraColumn.builder()
+						.name("TODO")
+						.statuses(List.of(new JiraColumnStatus("1", SELF)))
+						.build())
+				)
+				.build());
 	}
 
 }

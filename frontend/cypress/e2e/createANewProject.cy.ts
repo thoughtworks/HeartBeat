d713f96605084b3dbb0c8d7a493260be3d00346a
fengxinhou <fengxin.hou@thoughtworks.com>
2023-02-21 11:36:17 +0800

    ADM-359 add board e2e test

--- frontend/cypress/e2e/createANewProject.cy.ts
+++ frontend/cypress/e2e/createANewProject.cy.ts
@@ -14,5 +14,9 @@ describe('Create a new project', () => {
     metricsPage.selectDateRange(`${day}`, `${day + 1}`)
 
     metricsPage.selectVelocityAndCycleTime()
+
+    metricsPage.fillBoardFieldsInfo('2', 'mockEmail@qq.com', 'mockKey', '1', 'mockToken')
+
+    metricsPage.resetBoardInfo()
   })
 })

c820bbbcdc5b076f5c8d38f03f76562ec5faac6d
shawn he <64237577+spadio9587@users.noreply.github.com>
2023-02-23 11:15:03 +0800

    ADM-350 add pipelineTool in metricsTypeCheckbox

--- frontend/src/components/Metrics/ConfigStep/MetricsTypeCheckbox/index.tsx
+++ frontend/src/components/Metrics/ConfigStep/MetricsTypeCheckbox/index.tsx
@@ -3,6 +3,7 @@ import { REQUIRED_DATAS } from '@src/constants'
 import React, { useState } from 'react'
 import { RequireDataSelections } from '@src/components/Metrics/ConfigStep/MetricsTypeCheckbox/style'
 import { Board } from '@src/components/Metrics/ConfigStep/Board'
+import { PipelineTool } from '@src/components/Metrics/ConfigStep/PipelineTool'
 import { useAppDispatch, useAppSelector } from '@src/hooks'
 import { selectRequiredData, updateRequiredData } from '@src/features/config/configSlice'
 
@@ -11,6 +12,7 @@ export const MetricsTypeCheckbox = () => {
   const requireData = useAppSelector(selectRequiredData)
   const [isShowBoard, setIsShowBoard] = useState(false)
   const [isEmptyRequireData, setIsEmptyProjectData] = useState<boolean>(false)
+  const [isShowPipelineTool, setIsShowPipelineTool] = useState(false)
 
   const handleRequireDataChange = (event: SelectChangeEvent<typeof requireData>) => {
     const {
@@ -20,6 +22,12 @@ export const MetricsTypeCheckbox = () => {
     value.length === 0 ? setIsEmptyProjectData(true) : setIsEmptyProjectData(false)
     setIsShowBoard(
       value.includes(REQUIRED_DATAS[0]) || value.includes(REQUIRED_DATAS[1]) || value.includes(REQUIRED_DATAS[2])
+    )
+    setIsShowPipelineTool(
+      value.includes(REQUIRED_DATAS[3]) ||
+        value.includes(REQUIRED_DATAS[4]) ||
+        value.includes(REQUIRED_DATAS[5]) ||
+        value.includes(REQUIRED_DATAS[6])
     )
   }
   return (
@@ -43,6 +51,7 @@ export const MetricsTypeCheckbox = () => {
         {isEmptyRequireData && <FormHelperText>Metrics is required</FormHelperText>}
       </RequireDataSelections>
       {isShowBoard && <Board />}
+      {isShowPipelineTool && <PipelineTool />}
     </>
   )
 }
--- frontend/src/store.ts
+++ frontend/src/store.ts
@@ -2,11 +2,13 @@ import { configureStore } from '@reduxjs/toolkit'
 import stepperReducer from './features/stepper/StepperSlice'
 import configReducer from './features/config/configSlice'
 import boardReducer from './features/board/boardSlice'
+import pipelineReducer from './features/pipelineTool/pipelineToolSlice'
 export const store = configureStore({
   reducer: {
     stepper: stepperReducer,
     config: configReducer,
     board: boardReducer,
+    pipelineTool: pipelineReducer,
   },
 })
 

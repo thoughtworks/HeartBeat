4eaa14d64e1ff2a48840f59b33fd1ed0dbc2c9ee
fengxinhou <fengxin.hou@thoughtworks.com>
2023-02-24 08:31:20 +0800

    ADM-359 change error notification position

--- frontend/src/components/ErrorNotifaction/style.tsx
+++ frontend/src/components/ErrorNotifaction/style.tsx
@@ -2,8 +2,10 @@ import { Snackbar } from '@mui/material'
 import { styled } from '@mui/material/styles'
 
 export const ErrorBar = styled(Snackbar)({
-  position: 'relative',
-  width: '100%',
+  position: 'absolute',
+  display: 'inherit',
+  top: '0',
+  left: '12rem !important',
+  width: '50%',
   opacity: '0.8',
-  justifyContent: 'center',
 })

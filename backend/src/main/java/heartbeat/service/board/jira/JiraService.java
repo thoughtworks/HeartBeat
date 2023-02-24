ac45096a944372f4170897231007b883560b70c2
YiChen Wang <yichen.wang@YiChens-MacBook-Pro.local>
2023-02-24 17:47:34 +0800

    ADM-359 Revert with format files

---
+++ backend/src/main/java/heartbeat/service/board/jira/JiraService.java
@@ -0,0 +1,50 @@
+package heartbeat.service.board.jira;
+
+import feign.FeignException;
+import heartbeat.client.JiraFeignClient;
+import heartbeat.client.dto.JiraBoardConfigDTO;
+import heartbeat.client.dto.JiraColumn;
+import heartbeat.controller.board.vo.request.BoardRequest;
+import heartbeat.controller.board.vo.response.BoardConfigResponse;
+import heartbeat.controller.board.vo.response.ColumnResponse;
+import heartbeat.controller.board.vo.response.ColumnValue;
+import heartbeat.exception.RequestFailedException;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Service;
+
+import java.net.URI;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+@Log4j2
+public class JiraService {
+
+	private final JiraFeignClient jiraFeignClient;
+
+	public BoardConfigResponse getJiraConfiguration(BoardRequest boardRequest) {
+		String url = "https://" + boardRequest.getSite() + ".atlassian.net";
+		JiraBoardConfigDTO jiraBoardConfigDTO;
+		try {
+			jiraBoardConfigDTO = jiraFeignClient.getJiraBoardConfiguration(URI.create(url), boardRequest.getBoardId(),
+					boardRequest.getToken());
+			return BoardConfigResponse.builder()
+				.jiraColumns(jiraBoardConfigDTO.getColumnConfig()
+					.getColumns()
+					.stream()
+					.map(this::mapToResponseColumn)
+					.collect(Collectors.toList()))
+				.build();
+		}
+		catch (FeignException e) {
+			log.error("Failed when call Jira to get board config", e);
+			throw new RequestFailedException(e);
+		}
+	}
+
+	private ColumnResponse mapToResponseColumn(JiraColumn column) {
+		return ColumnResponse.builder().value(ColumnValue.builder().name(column.getName()).build()).build();
+	}
+
+}

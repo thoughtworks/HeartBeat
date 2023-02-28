2eeef1dee11b50998096666019dc776bfadb9184
fengxinhou <fengxin.hou@thoughtworks.com>
2023-02-28 14:06:41 +0800

    ADM-359 fix board client

--- frontend/src/clients/BoardClient.ts
+++ frontend/src/clients/BoardClient.ts
@@ -3,7 +3,7 @@ import { HttpClient } from '@src/clients/Httpclient'
 export class BoardClient extends HttpClient {
   getVerifyBoard = async () => {
     try {
-      return await this.axiosInstance.get('/kanban/verify')
+      return await this.axiosInstance.get('/kanban/verify').then((res) => res)
     } catch (error) {
       throw new Error('error')
     }

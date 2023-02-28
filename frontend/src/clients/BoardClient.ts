ec8366b09c0c2272511451c77c259f46c7fd9d0d
Lanxin Wang <lanxin.wang@thoughtworks.com>
2023-02-28 18:39:03 +0800

    ADM-359 refactor about moving useVerifyBoardState hook logic functions to BoardClient

--- frontend/src/clients/BoardClient.ts
+++ frontend/src/clients/BoardClient.ts
@@ -1,12 +1,39 @@
 import { HttpClient } from '@src/clients/Httpclient'
 
 export class BoardClient extends HttpClient {
+  isBoardVerify = false
+  isNoDoneCard = false
+  isShowErrorNotification = false
+  response = {}
+
   getVerifyBoard = async () => {
     try {
-      return await this.axiosInstance.get('/kanban/verify').then((res) => res)
-    } catch (error) {
-      throw new Error('error')
+      const result = await this.axiosInstance.get('/kanban/verify').then((res) => res)
+      result.status === 204 ? this.handleBoardNoDoneCard() : this.handleBoardVerifySucceed(result.data)
+    } catch (e) {
+      this.handleBoardVerifyFailed()
     }
+    return {
+      response: this.response,
+      isBoardVerify: this.isBoardVerify,
+      isNoDoneCard: this.isNoDoneCard,
+      isShowErrorNotification: this.isShowErrorNotification,
+    }
+  }
+
+  handleBoardNoDoneCard = () => {
+    this.isBoardVerify = false
+    this.isNoDoneCard = true
+  }
+
+  handleBoardVerifySucceed = (res: object) => {
+    this.isBoardVerify = true
+    this.response = res
+  }
+
+  handleBoardVerifyFailed = () => {
+    this.isBoardVerify = false
+    this.isShowErrorNotification = true
   }
 }
 
--- frontend/src/hooks/useVerifyBoardState.ts
+++ frontend/src/hooks/useVerifyBoardState.ts
@@ -3,61 +3,30 @@ import { useState } from 'react'
 import { boardClient } from '@src/clients/BoardClient'
 
 export interface useVerifyBoardStateInterface {
-  verifyJira: () => Promise<{
-    isBoardVerify: boolean
-    isNoDoneCard: boolean
-    isShowErrorNotification: boolean
-    response: object
-  }>
+  verifyJira: () => Promise<
+    | {
+        isBoardVerify: boolean
+        isNoDoneCard: boolean
+        isShowErrorNotification: boolean
+        response: object
+      }
+    | undefined
+  >
   isVerifyLoading: boolean
-  showErrorMessage: string
 }
 
 export const useVerifyBoardState = (): useVerifyBoardStateInterface => {
-  let isBoardVerify = false
-  let isNoDoneCard = false
-  let isShowErrorNotification = false
-  let response = {}
   const [isVerifyLoading, setIsLoading] = useState(false)
-  const [showErrorMessage, setShowErrorMessage] = useState('')
 
   const verifyJira = async () => {
     setIsLoading(true)
-    try {
-      const response = await boardClient.getVerifyBoard()
-      if (response.status === 444) {
-        handleBoardNoDoneCard()
-      } else if (response.status == 200) {
-        handleBoardVerifySucceed(response.data)
-      } else {
-        handleBoardVerifyFailed()
-      }
-    } catch (e) {
-      handleBoardVerifyFailed()
-    } finally {
-      setIsLoading(false)
-    }
-    return { response, isBoardVerify, isNoDoneCard, isShowErrorNotification }
-  }
-
-  const handleBoardNoDoneCard = () => {
-    isNoDoneCard = true
-  }
-
-  const handleBoardVerifySucceed = (res: object) => {
-    isBoardVerify = true
-    response = res
-  }
-
-  const handleBoardVerifyFailed = () => {
-    isBoardVerify = false
-    isShowErrorNotification = true
-    setShowErrorMessage('Jira verify failed')
+    const res = await boardClient.getVerifyBoard()
+    setIsLoading(false)
+    return res
   }
 
   return {
     verifyJira,
     isVerifyLoading,
-    showErrorMessage,
   }
 }

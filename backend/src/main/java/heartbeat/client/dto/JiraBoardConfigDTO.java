cdb20a73fa520cb5aa0c0c5a96266899577db417
Lanxin Wang <lanxin.wang@thoughtworks.com>
2023-03-01 13:40:46 +0800

    ADM-359 merge main backend file

---
+++ backend/src/main/java/heartbeat/client/dto/JiraBoardConfigDTO.java
@@ -0,0 +1,22 @@
+package heartbeat.client.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class JiraBoardConfigDTO {
+
+	private String id;
+
+	private String name;
+
+	private JiraColumnConfig columnConfig;
+
+}

0e1f9d0abd2d346038a50e92fb6af71f05837b22
zhiqiao.yang <zhiqiao.yang@thoughtworks.com>
2023-03-01 10:56:37 +0800

    ADM-348 add sourceControl in configSlice file

--- frontend/src/features/config/configSlice.tsx
+++ frontend/src/features/config/configSlice.tsx
@@ -1,6 +1,6 @@
 import { createSlice } from '@reduxjs/toolkit'
 import type { RootState } from '@src/store'
-import { BOARD_TYPES, REGULAR_CALENDAR, PIPELINE_TOOL_TYPES } from '@src/constants'
+import { BOARD_TYPES, REGULAR_CALENDAR, PIPELINE_TOOL_TYPES, SOURCE_CONTROL_TYPES } from '@src/constants'
 
 export interface configState {
   projectName: string
@@ -12,6 +12,7 @@ export interface configState {
   requiredData: string[]
   boardFields: { board: string; boardId: string; email: string; projectKey: string; site: string; token: string }
   pipelineToolFields: { pipelineTool: string; token: string }
+  sourceControlFields: { sourceControl: string; token: string }
 }
 
 const initialState: configState = {
@@ -32,6 +33,10 @@ const initialState: configState = {
   },
   pipelineToolFields: {
     pipelineTool: PIPELINE_TOOL_TYPES.BUILD_KITE,
+    token: '',
+  },
+  sourceControlFields: {
+    sourceControl: SOURCE_CONTROL_TYPES.GIT_HUB,
     token: '',
   },
 }
@@ -59,6 +64,9 @@ export const configSlice = createSlice({
     updatePipelineToolFields: (state, action) => {
       state.pipelineToolFields = action.payload
     },
+    updateSourceControlFields: (state, action) => {
+      state.sourceControlFields = action.payload
+    },
   },
 })
 
@@ -69,6 +77,7 @@ export const {
   updateRequiredData,
   updateBoardFields,
   updatePipelineToolFields,
+  updateSourceControlFields,
 } = configSlice.actions
 
 export const selectProjectName = (state: RootState) => state.config.projectName
@@ -77,4 +86,5 @@ export const selectDateRange = (state: RootState) => state.config.dateRange
 export const selectRequiredData = (state: RootState) => state.config.requiredData
 export const selectBoardFields = (state: RootState) => state.config.boardFields
 export const selectPipelineToolFields = (state: RootState) => state.config.pipelineToolFields
+export const selectSourceControlFields = (state: RootState) => state.config.sourceControlFields
 export default configSlice.reducer

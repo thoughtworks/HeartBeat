1368f59caa2d395f66b2642ced7941ee711f21f7
shawn he <64237577+spadio9587@users.noreply.github.com>
2023-03-01 09:44:23 +0800

    ADM-350 add pipelineToolVerifyResponseSlice test file

---
+++ frontend/__tests__/src/features/pipelineToolVerifyResponseSlice.test.tsx
@@ -0,0 +1,26 @@
+import pipelineVerifyResponseReducer, {
+  updatePipelineToolVerifyResponse,
+} from '@src/features/pipelineToolVerifyResponse/pipelineToolVerifyResponseSlice'
+
+describe('pipelineToolVerifyResponse reducer', () => {
+  it('should show empty array when handle initial state', () => {
+    const pipelineVerifyResponse = pipelineVerifyResponseReducer(undefined, { type: 'unknown' })
+
+    expect(pipelineVerifyResponse.pipelineTool).toEqual([])
+  })
+
+  it('should store pipelineTool data when get network pipelineTool verify response', () => {
+    const mockPipelineToolVerifyResponse = {
+      pipelineTool: [],
+    }
+
+    const jiraVerifyResponse = pipelineVerifyResponseReducer(
+      { pipelineTool: [] },
+      updatePipelineToolVerifyResponse({
+        pipelineTool: mockPipelineToolVerifyResponse.pipelineTool,
+      })
+    )
+
+    expect(jiraVerifyResponse.pipelineTool).toEqual(mockPipelineToolVerifyResponse)
+  })
+})

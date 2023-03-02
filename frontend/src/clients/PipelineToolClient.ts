2469a36921cdce9f22736ec948a5206469e74f08
shawn he <64237577+spadio9587@users.noreply.github.com>
2023-03-02 15:03:12 +0800

    ADM-350 rebuild pipelineToolClient

--- frontend/src/clients/PipelineToolClient.ts
+++ frontend/src/clients/PipelineToolClient.ts
@@ -1,12 +1,41 @@
 import { HttpClient } from '@src/clients/Httpclient'
+import { AxiosError } from 'axios'
+import { BadRequestException } from '@src/exceptions/BadRequestException'
+import { BadServerException } from '@src/exceptions/BasServerException'
 
+export interface getVerifyPipelineToolParams {
+  type: string
+  token: string
+  startTime: string | null
+  endTime: string | null
+}
 export class PipelineToolClient extends HttpClient {
-  verifyPipelineTool = async () => {
+  isPipelineToolVerified = false
+  response = {}
+
+  verifyPipelineTool = async (params: getVerifyPipelineToolParams) => {
     try {
-      return await this.axiosInstance.post('/pipeline/fetch').then((res) => res)
-    } catch (error) {
-      throw new Error('error')
+      const result = await this.axiosInstance.post('/pipeline/fetch', { params: { ...params } }).then((res) => res)
+      this.handlePipelineToolVerifySucceed(result)
+    } catch (e) {
+      this.isPipelineToolVerified = false
+      const code = (e as AxiosError).response?.status
+      if (code === 404) {
+        throw new BadRequestException(params.type, 'verify failed')
+      }
+      if (code === 500) {
+        throw new BadServerException(params.type, 'verify failed')
+      }
     }
+    return {
+      response: this.response,
+      isPipelineToolVerified: this.isPipelineToolVerified,
+    }
+  }
+
+  handlePipelineToolVerifySucceed = (res: object) => {
+    this.isPipelineToolVerified = true
+    this.response = res
   }
 }
 

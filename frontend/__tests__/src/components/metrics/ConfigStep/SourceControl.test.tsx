046584be24b327ab7dcf44bc12f3451468e5e1ad
Ting Wang <ting.wang@thoughtworks.com>
2023-03-02 09:36:15 +0800

    ADM-348 add test for sourceControl component

--- frontend/__tests__/src/components/metrics/ConfigStep/SourceControl.test.tsx
+++ frontend/__tests__/src/components/metrics/ConfigStep/SourceControl.test.tsx
@@ -1,9 +1,15 @@
 import { setupStore } from '../../../utils/setupStoreUtil'
-import { render } from '@testing-library/react'
+import { render, screen, fireEvent, waitFor } from '@testing-library/react'
 import { Provider } from 'react-redux'
 import { SourceControl } from '@src/components/Metrics/ConfigStep/SourceControl'
-import { CONFIG_TITLE, SOURCE_CONTROL_FIELDS } from '../../../fixtures'
-import { SOURCE_CONTROL_TYPES } from '@src/constants'
+import { CONFIG_TITLE, ERROR_MESSAGE_COLOR, SOURCE_CONTROL_FIELDS, SOURCE_CONTROL_TYPES } from '../../../fixtures'
+
+export const fillSourceControlFieldsInformation = () => {
+  const mockInfo = 'mockToken'
+  const tokenInput = screen.getByTestId('sourceControlTextField').querySelector('input') as HTMLInputElement
+  fireEvent.change(tokenInput, { target: { value: mockInfo } })
+  expect(tokenInput.value).toEqual(mockInfo)
+}
 
 let store = setupStore()
 const setup = () => {
@@ -30,5 +36,54 @@ describe('SourceControl', () => {
     const sourceControlType = getByText(SOURCE_CONTROL_TYPES.GIT_HUB)
 
     expect(sourceControlType).toBeInTheDocument()
+  })
+
+  it('should clear all fields information when click reset button', async () => {
+    const { getByRole, getByText, queryByRole } = setup()
+    const tokenInput = screen.getByTestId('sourceControlTextField').querySelector('input') as HTMLInputElement
+    fillSourceControlFieldsInformation()
+
+    fireEvent.click(getByText('Verify'))
+    await waitFor(() => {
+      fireEvent.click(getByRole('button', { name: 'Reset' }))
+    })
+
+    expect(tokenInput.value).toEqual('')
+    expect(getByText(SOURCE_CONTROL_TYPES.GIT_HUB)).toBeInTheDocument()
+    expect(queryByRole('button', { name: 'Reset' })).not.toBeTruthy()
+    expect(queryByRole('button', { name: 'Verify' })).toBeDisabled()
+  })
+
+  it('should enable verify button when all fields checked correctly given disable verify button', () => {
+    const { getByRole } = setup()
+    const verifyButton = getByRole('button', { name: 'Verify' })
+
+    expect(verifyButton).toBeDisabled()
+
+    fillSourceControlFieldsInformation()
+    expect(verifyButton).toBeEnabled()
+  })
+
+  it('should show reset button  and verified button when verify successfully', async () => {
+    const { getByText } = setup()
+    fillSourceControlFieldsInformation()
+    fireEvent.click(getByText('Verify'))
+    await waitFor(() => {
+      expect(getByText('Reset')).toBeVisible()
+    })
+    await waitFor(() => {
+      expect(getByText('Verified')).toBeInTheDocument()
+    })
+  })
+
+  it('should show error message and error style when token is empty', () => {
+    const { getByText } = setup()
+    const TOKEN_ERROR_MESSAGE = 'token is required'
+    fillSourceControlFieldsInformation()
+
+    const tokenInput = screen.getByTestId('sourceControlTextField').querySelector('input') as HTMLInputElement
+    fireEvent.change(tokenInput, { target: { value: '' } })
+    expect(getByText(TOKEN_ERROR_MESSAGE)).toBeVisible()
+    expect(getByText(TOKEN_ERROR_MESSAGE)).toHaveStyle(ERROR_MESSAGE_COLOR)
   })
 })

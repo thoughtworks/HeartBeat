1cbd3da971fc230ca23c11c10b3812003d535f66
Lanxin Wang <lanxin.wang@thoughtworks.com>
2023-03-03 10:59:10 +0800

    ADM-359.1 fix stepperSlice tests

--- frontend/__tests__/src/features/stepperSlice.test.tsx
+++ frontend/__tests__/src/features/stepperSlice.test.tsx
@@ -3,41 +3,41 @@ import { ZERO } from '../fixtures'
 
 describe('stepper reducer', () => {
   it('should get 0 when handle initial state', () => {
-    const STEPPER = stepperReducer(undefined, { type: 'unknown' })
+    const stepper = stepperReducer(undefined, { type: 'unknown' })
 
-    expect(STEPPER.value).toEqual(ZERO)
+    expect(stepper.stepNumber).toEqual(ZERO)
   })
 
-  it('should get 1 when handle next step given value is 0', () => {
-    const STEPPER = stepperReducer(
+  it('should get 1 when handle next step given stepNumber is 0', () => {
+    const stepper = stepperReducer(
       {
-        value: 0,
+        stepNumber: 0,
       },
       nextStep()
     )
 
-    expect(STEPPER.value).toEqual(1)
+    expect(stepper.stepNumber).toEqual(1)
   })
 
-  it('should get 0 when handle back step given value is 0', () => {
-    const STEPPER = stepperReducer(
+  it('should get 0 when handle back step given stepNumber is 0', () => {
+    const stepper = stepperReducer(
       {
-        value: 0,
+        stepNumber: 0,
       },
       backStep()
     )
 
-    expect(STEPPER.value).toEqual(ZERO)
+    expect(stepper.stepNumber).toEqual(ZERO)
   })
 
-  it('should get 1 when handle back step given value is 2', () => {
-    const STEPPER = stepperReducer(
+  it('should get 1 when handle back step given stepNumber is 2', () => {
+    const stepper = stepperReducer(
       {
-        value: 2,
+        stepNumber: 2,
       },
       backStep()
     )
 
-    expect(STEPPER.value).toEqual(1)
+    expect(stepper.stepNumber).toEqual(1)
   })
 })

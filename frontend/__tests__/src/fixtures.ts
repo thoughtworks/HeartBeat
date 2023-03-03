40cf9030d2cc9b923fc1e29bbbfe410e9b261f4e
zhiqiao.yang <zhiqiao.yang@thoughtworks.com>
2023-03-03 13:50:19 +0800

    ADM-348 add mock sourceControl url for error test

--- frontend/__tests__/src/fixtures.ts
+++ frontend/__tests__/src/fixtures.ts
@@ -58,6 +58,7 @@ export const PIPELINE_TOOL_FIELDS = ['PipelineTool', 'Token']
 export const BOARD_FIELDS = ['Board', 'BoardId', 'Email', 'Project Key', 'Site', 'Token']
 export const SOURCE_CONTROL_FIELDS = ['sourceControl', 'token']
 export const MOCK_URL = '/api/v1/kanban/verify'
+export const MOCK_SOURCE_CONTROL_URL = '/api/v1/codebase/fetch/repos'
 export const JIRA_VERIFY_FAILED_MESSAGE = 'Jira verify failed'
 export const BUILD_KITE_VERIFY_FAILED_MESSAGE = 'BuildKite verify failed'
 export const MOCK_PIPELINE_URL = '/api/v1/pipeline/fetch'

6b424161a57f1e911c5007e587c7ae2663683b92
allenzph <zph13700225692@126.com>
2023-03-07 16:18:34 +0800

    ADM-309 log: add log in GithubService

--- backend/src/main/java/heartbeat/service/source/github/GithubService.java
+++ backend/src/main/java/heartbeat/service/source/github/GithubService.java
@@ -21,12 +21,30 @@ public class GithubService {
 
 	public GithubResponse verifyToken(String githubToken) {
 		String token = "token " + githubToken;
+		log.info("[Github] Start to query repository_url by token: https://api.github.com/user/repos");
 		final var githubReposByUser = githubFeignClient.getAllRepos(token)
 			.stream()
 			.map(GithubRepos::getHtml_url)
 			.toList();
+		log.info("[Github] Successfully queried repository_data by token");
+		log.info("[Github] Start to query organization_url by token: https://api.github.com/user/orgs");
+
 		final var githubOrganizations = githubFeignClient.getGithubOrgsInfo(token);
+		log.info("[Github] Successfully queried organization_data by token");
+
 		List<String> githubRepos = new ArrayList<>(githubReposByUser);
+		log.info("[Github] Start to query repository_url by organization_name and token: https://api.github.com/orgs/{organization-name}/repos");
+
+		getGithubReposByOrganizations(token, githubOrganizations, githubRepos);
+		log.info("[Github] Successfully queried repository_data by organizations and token");
+		log.info("[Github] Start to remove duplicate githubRepos list");
+		removeDuplicates(githubRepos);
+		log.info("[Github] Successfully remove duplicate githubRepos list");
+
+		return GithubResponse.builder().githubRepos(githubRepos).build();
+	}
+
+	private void getGithubReposByOrganizations(String token, List<GithubOrgsInfo> githubOrganizations, List<String> githubRepos) {
 		githubRepos.addAll(githubOrganizations.stream()
 			.map(GithubOrgsInfo::getLogin)
 			.flatMap(org -> githubFeignClient.getReposByOrganizationName(org, token)

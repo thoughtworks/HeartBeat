57d4fbddbbd595b5b2b10f0e280087ee6c0b01f6
Yuyu Zhang <1204891354@qq.com>
2023-03-09 14:20:41 +0800

    ADM-361 add CycleTime component and use it in MetricsStep component

--- frontend/src/components/Metrics/MetricsStep/index.tsx
+++ frontend/src/components/Metrics/MetricsStep/index.tsx
@@ -2,15 +2,17 @@ import React from 'react'
 import { Crews } from '@src/components/Metrics/MetricsStep/Crews'
 import { useAppSelector } from '@src/hooks'
 import { selectUsers } from '@src/context/board/jiraVerifyResponse/jiraVerifyResponseSlice'
+import { selectColumns } from '@src/context/board/jiraVerifyResponse/jiraVerifyResponseSlice'
 import { CycleTime } from '@src/components/Metrics/MetricsStep/CycleTime'
 
 export const MetricsStep = () => {
   const users = useAppSelector(selectUsers)
+  const columns = useAppSelector(selectColumns)
 
   return (
     <>
       <Crews options={users} title={'Crews Setting'} label={'Included Crews'} />
-      <CycleTime title={'Cycle Time Setting'} />
+      <CycleTime options={columns} title={'Cycle Time Setting'} />
     </>
   )
 }

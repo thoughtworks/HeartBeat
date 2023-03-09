be17d7a5d31e07250a44bbb83cdcaf6c10cdc0cc
Yuyu Zhang <1204891354@qq.com>
2023-03-09 14:20:41 +0800

    ADM-363 add CycleTime component and use it in MetricsStep component

--- frontend/src/components/Metrics/MetricsStep/CycleTime/index.tsx
+++ frontend/src/components/Metrics/MetricsStep/CycleTime/index.tsx
@@ -1,8 +1,19 @@
 import React from 'react'
-import MetricsSettingTitle from '@src/components/common/metricsSettingTitle'
+import { FormSelect } from '@src/components/common/FormSelect'
+import MetricsSettingTitle from '@src/components/common/MetricsSettingTitle'
 
 interface cycletimeProps {
+  options: string[]
   title: string
 }
 
-export const CycleTime = ({ title }: cycletimeProps) => <MetricsSettingTitle title={title} />
+export const CycleTime = ({ options, title }: cycletimeProps) => (
+  <>
+    <MetricsSettingTitle title={title} />
+    <FormSelect label={'TODO'} defaultSelected={['To do']} options={options} />
+    <FormSelect label={'Done'} defaultSelected={['Done']} options={options} />
+    <FormSelect label={'Doing'} defaultSelected={['In Dev']} options={options} />
+    <FormSelect label={'Testing'} defaultSelected={['Testing']} options={options} />
+    <FormSelect label={'Blocked'} defaultSelected={['Block']} options={options} />
+  </>
+)
--- frontend/src/components/Metrics/MetricsStep/index.tsx
+++ frontend/src/components/Metrics/MetricsStep/index.tsx
@@ -1,17 +1,20 @@
 import React from 'react'
 import { Crews } from '@src/components/Metrics/MetricsStep/Crews'
 import { useAppSelector } from '@src/hooks'
+import { selectColumns } from '@src/context/board/jiraVerifyResponse/jiraVerifyResponseSlice'
 import { CycleTime } from '@src/components/Metrics/MetricsStep/CycleTime'
 import { selectTargetFields, selectUsers } from '@src/context/board/jiraVerifyResponse/jiraVerifyResponseSlice'
 import { Classification } from '@src/components/Metrics/MetricsStep/Classification'
 
 export const MetricsStep = () => {
   const users = useAppSelector(selectUsers)
+  const columns = useAppSelector(selectColumns)
+
   const targetFields = useAppSelector(selectTargetFields)
   return (
     <>
       <Crews options={users} title={'Crews Setting'} label={'Included Crews'} />
-      <CycleTime title={'Cycle Time Setting'} />
+      <CycleTime options={columns} title={'Cycle Time Setting'} />
       <Classification options={targetFields} title={'Classification Setting'} label={'Distinguished By'} />
     </>
   )

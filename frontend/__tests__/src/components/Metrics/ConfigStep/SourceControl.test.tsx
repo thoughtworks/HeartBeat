c36be1fc57383620f2dac5526a684680e04fabd1
Ting Wang <ting.wang@thoughtworks.com>
2023-03-09 10:55:59 +0800

    ADM-348 add tests for token verify message

--- frontend/__tests__/src/components/Metrics/ConfigStep/SourceControl.test.tsx
+++ frontend/__tests__/src/components/Metrics/ConfigStep/SourceControl.test.tsx
@@ -116,6 +116,20 @@ describe('SourceControl', () => {
     expect(getByText(TOKEN_ERROR_MESSAGE)).toHaveStyle(ERROR_MESSAGE_COLOR)
   })
 
+  it('should show error message and error style when token is invalid', () => {
+    const { getByText } = setup()
+    const TOKEN_ERROR_MESSAGE = 'Invalid token'
+    const mockInfo = 'mockToken'
+    const tokenInput = screen.getByTestId('sourceControlTextField').querySelector('input') as HTMLInputElement
+
+    fireEvent.change(tokenInput, { target: { value: mockInfo } })
+
+    expect(tokenInput.value).toEqual(mockInfo)
+
+    expect(getByText(TOKEN_ERROR_MESSAGE)).toBeInTheDocument()
+    expect(getByText(TOKEN_ERROR_MESSAGE)).toHaveStyle(ERROR_MESSAGE_COLOR)
+  })
+
   it('should show error notification when sourceControl verify response status is 404', async () => {
     server.use(rest.get(MOCK_SOURCE_CONTROL_URL, (req, res, ctx) => res(ctx.status(404))))
     const { getByText, getByRole } = setup()

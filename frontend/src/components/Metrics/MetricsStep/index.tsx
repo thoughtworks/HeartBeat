4d27d8f2687f4c87eaa529aaf087d408b658c761
Yuyu Zhang <1204891354@qq.com>
2023-03-09 15:25:04 +0800

    ADM-363 add CYCLETIME_LIST constant and use it as options in CycleTime component, delete 'column' in ResponseSlice.

--- frontend/src/components/Metrics/MetricsStep/index.tsx
+++ frontend/src/components/Metrics/MetricsStep/index.tsx
@@ -1,20 +1,18 @@
 import React from 'react'
 import { Crews } from '@src/components/Metrics/MetricsStep/Crews'
 import { useAppSelector } from '@src/hooks'
-import { selectColumns } from '@src/context/board/jiraVerifyResponse/jiraVerifyResponseSlice'
 import { CycleTime } from '@src/components/Metrics/MetricsStep/CycleTime'
+import { CYCLETIME_LIST } from '@src/constants'
 import { selectTargetFields, selectUsers } from '@src/context/board/jiraVerifyResponse/jiraVerifyResponseSlice'
 import { Classification } from '@src/components/Metrics/MetricsStep/Classification'
 
 export const MetricsStep = () => {
   const users = useAppSelector(selectUsers)
-  const columns = useAppSelector(selectColumns)
-
   const targetFields = useAppSelector(selectTargetFields)
   return (
     <>
       <Crews options={users} title={'Crews Setting'} label={'Included Crews'} />
-      <CycleTime options={columns} title={'Cycle Time Setting'} />
+      <CycleTime options={CYCLETIME_LIST} title={'Cycle Time Setting'} />
       <Classification options={targetFields} title={'Classification Setting'} label={'Distinguished By'} />
     </>
   )
--- frontend/src/context/board/jiraVerifyResponse/jiraVerifyResponseSlice.tsx
+++ frontend/src/context/board/jiraVerifyResponse/jiraVerifyResponseSlice.tsx
@@ -5,7 +5,6 @@ export interface jiraVerifyResponseState {
   jiraColumns: []
   targetFields: { name: string; key: string; flag: boolean }[]
   users: string[]
-  columns: string[]
 }
 
 const initialState: jiraVerifyResponseState = {
@@ -19,11 +18,10 @@ export const jiraVerifyResponseSlice = createSlice({
   initialState,
   reducers: {
     updateJiraVerifyResponse: (state, action) => {
-      const { jiraColumns, targetFields, users, columns } = action.payload
+      const { jiraColumns, targetFields, users } = action.payload
       state.jiraColumns = jiraColumns
       state.targetFields = targetFields
       state.users = users
-      state.columns = columns
     },
   },
 })
@@ -31,8 +29,6 @@ export const jiraVerifyResponseSlice = createSlice({
 export const { updateJiraVerifyResponse } = jiraVerifyResponseSlice.actions
 
 export const selectUsers = (state: RootState) => state.jiraVerifyResponse.users
-
-export const selectColumns = (state: RootState) => state.jiraVerifyResponse.columns
 
 export const selectTargetFields = (state: RootState) => state.jiraVerifyResponse.targetFields
 

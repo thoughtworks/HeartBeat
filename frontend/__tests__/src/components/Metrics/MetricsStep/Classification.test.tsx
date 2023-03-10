dc7f0af1a2ad1551d431607514209ba692a66cf1
zhiqiao.yang <zhiqiao.yang@thoughtworks.com>
2023-03-10 16:13:56 +0800

    ADM-364 add test for Classification component

--- frontend/__tests__/src/components/Metrics/MetricsStep/Classification.test.tsx
+++ frontend/__tests__/src/components/Metrics/MetricsStep/Classification.test.tsx
@@ -1,10 +1,15 @@
-import { render } from '@testing-library/react'
+import { getByText, queryByText, render, within } from '@testing-library/react'
 import { Classification } from '@src/components/Metrics/MetricsStep/Classification'
+import userEvent from '@testing-library/user-event'
 
 const mockTitle = 'Classification Setting'
 const mockLabel = 'Distinguished By'
+const mockTargetField = [
+  { flag: false, key: 'issue', name: 'Issue' },
+  { flag: false, key: 'type', name: 'Type' },
+]
 const setup = () => {
-  return render(<Classification title={mockTitle} label={mockLabel} />)
+  return render(<Classification title={mockTitle} label={mockLabel} options={mockTargetField} />)
 }
 
 describe('Classification', () => {
@@ -13,5 +18,47 @@ describe('Classification', () => {
 
     expect(getByText(mockTitle)).toBeInTheDocument()
     expect(getByText(mockLabel)).toBeInTheDocument()
+  })
+
+  it('should not show options when initialization', () => {
+    const { queryByText } = setup()
+
+    expect(queryByText('Issue')).not.toBeInTheDocument()
+    expect(queryByText('Type')).not.toBeInTheDocument()
+  })
+
+  it('should show all options when click selectBox', async () => {
+    const { getByRole, getByText } = setup()
+    await userEvent.click(getByRole('button', { name: mockLabel }))
+
+    expect(getByText('Issue')).toBeInTheDocument()
+    expect(getByText('Type')).toBeInTheDocument()
+  })
+
+  it('should show all targetField when click All and show nothing when cancel click', async () => {
+    const { getByText, getByRole } = setup()
+    await userEvent.click(getByRole('button', { name: mockLabel }))
+    await userEvent.click(getByText('All'))
+    const listBox = within(getByRole('listbox'))
+    const names = mockTargetField.map((item) => item.name)
+
+    expect(listBox.getByRole('option', { name: names[0] })).toHaveProperty('selected', true)
+    expect(listBox.getByRole('option', { name: names[1] })).toHaveProperty('selected', true)
+
+    await userEvent.click(getByText('All'))
+
+    expect(listBox.getByRole('option', { name: names[0] })).toHaveProperty('selected', false)
+    expect(listBox.getByRole('option', { name: names[1] })).toHaveProperty('selected', false)
+  })
+
+  it('should show selected targetField when click selected field', async () => {
+    const { getByRole } = setup()
+    const names = mockTargetField.map((item) => item.name)
+    await userEvent.click(getByRole('button', { name: mockLabel }))
+    const listBox = within(getByRole('listbox'))
+    await userEvent.click(listBox.getByRole('option', { name: names[0] }))
+
+    expect(listBox.getByRole('option', { name: names[0] })).toHaveProperty('selected', true)
+    expect(listBox.getByRole('option', { name: names[1] })).toHaveProperty('selected', false)
   })
 })

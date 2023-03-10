1cd89478fe9fcbde2cde8767fcaed8cea2667496
Eason-T <awawerwer2007@163.com>
2023-03-10 16:02:45 +0800

    ADM-349 feat: create BuildKiteFeignClient

---
+++ backend/src/main/java/heartbeat/client/BuildKiteFeignClient.java
@@ -0,0 +1,16 @@
+package heartbeat.client;
+
+import heartbeat.client.dto.BuildKiteOrganizationsInfo;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.util.List;
+
+@FeignClient(name = "buildKiteFeignClient", url = "${buildKite.url}")
+public interface BuildKiteFeignClient {
+	@GetMapping(path = "/organizations")
+	@ResponseStatus(HttpStatus.OK)
+	List<BuildKiteOrganizationsInfo> getBuildKiteOrganizationsInfo();
+}

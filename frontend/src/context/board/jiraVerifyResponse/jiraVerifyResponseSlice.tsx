f92abbd7a778aa31b5183bdab4118527f002b246
zhiqiao.yang <zhiqiao.yang@thoughtworks.com>
2023-03-10 16:15:09 +0800

    ADM-364 add selectTargetFields in jiraVerifyResponseSlice

--- frontend/src/context/board/jiraVerifyResponse/jiraVerifyResponseSlice.tsx
+++ frontend/src/context/board/jiraVerifyResponse/jiraVerifyResponseSlice.tsx
@@ -29,5 +29,6 @@ export const jiraVerifyResponseSlice = createSlice({
 export const { updateJiraVerifyResponse } = jiraVerifyResponseSlice.actions
 
 export const selectUsers = (state: RootState) => state.jiraVerifyResponse.users
+export const selectTargetFields = (state: RootState) => state.jiraVerifyResponse.targetFields
 
 export default jiraVerifyResponseSlice.reducer

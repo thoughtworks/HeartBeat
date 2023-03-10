abf50ec1f24ed8b79da64964a05a1ee67ccfa86f
zhiqiao.yang <zhiqiao.yang@thoughtworks.com>
2023-03-10 16:16:36 +0800

    ADM-364 add classification setting logic

--- frontend/src/components/Metrics/MetricsStep/Classification/index.tsx
+++ frontend/src/components/Metrics/MetricsStep/Classification/index.tsx
@@ -1,20 +1,51 @@
-import { Divider } from '@src/theme'
-import { FormControl, InputLabel, Select } from '@mui/material'
+import { Divider, Title } from '@src/theme'
+import { Checkbox, FormControl, InputLabel, MenuItem, Select, ListItemText, SelectChangeEvent } from '@mui/material'
+import { useState } from 'react'
 
 interface classificationProps {
   title: string
   label: string
+  options: { name: string; key: string; flag: boolean }[]
 }
 
-export const Classification = ({ title, label }: classificationProps) => {
+export const Classification = ({ options, title, label }: classificationProps) => {
+  const [selectedTargetField, setSelectedTargetField] = useState<string[]>([])
+  const isAllSelected = selectedTargetField.length === options.length
+  const handleTargetFieldChange = (event: SelectChangeEvent<string[]>) => {
+    const value = event.target.value
+    const targetFieldNames = options.map((item) => item.name)
+    if (value[value.length - 1] === 'All') {
+      setSelectedTargetField(isAllSelected ? [] : targetFieldNames)
+      return
+    }
+    setSelectedTargetField([...value])
+  }
+
   return (
     <>
       <Divider>
-        <span>{title}</span>
+        <Title>{title}</Title>
       </Divider>
       <FormControl variant='standard'>
-        <InputLabel>{label}</InputLabel>
-        <Select defaultValue=''></Select>
+        <InputLabel id='classification-check-box'>{label}</InputLabel>
+        <Select
+          multiple
+          labelId='classification-check-box'
+          value={selectedTargetField}
+          onChange={handleTargetFieldChange}
+          renderValue={(selectedTargetField: string[]) => selectedTargetField.join(',')}
+        >
+          <MenuItem value='All'>
+            <Checkbox checked={isAllSelected} />
+            <ListItemText primary='All' />
+          </MenuItem>
+          {options.map((targetField) => (
+            <MenuItem key={targetField.key} value={targetField.name}>
+              <Checkbox checked={selectedTargetField.includes(targetField.name)} />
+              <ListItemText primary={targetField.name} />
+            </MenuItem>
+          ))}
+        </Select>
       </FormControl>
     </>
   )
--- frontend/src/components/Metrics/MetricsStep/index.tsx
+++ frontend/src/components/Metrics/MetricsStep/index.tsx
@@ -1,16 +1,16 @@
 import React from 'react'
 import { Crews } from '@src/components/Metrics/MetricsStep/Crews'
 import { useAppSelector } from '@src/hooks'
-import { selectUsers } from '@src/context/board/jiraVerifyResponse/jiraVerifyResponseSlice'
+import { selectTargetFields, selectUsers } from '@src/context/board/jiraVerifyResponse/jiraVerifyResponseSlice'
 import { Classification } from '@src/components/Metrics/MetricsStep/Classification'
 
 export const MetricsStep = () => {
   const users = useAppSelector(selectUsers)
-
+  const targetFields = useAppSelector(selectTargetFields)
   return (
     <>
       <Crews options={users} title={'Crews Setting'} label={'Included Crews'} />
-      <Classification title={'Classification Setting'} label={'Distinguished By'} />
+      <Classification options={targetFields} title={'Classification Setting'} label={'Distinguished By'} />
     </>
   )
 }

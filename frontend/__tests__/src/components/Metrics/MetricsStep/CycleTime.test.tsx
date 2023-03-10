689b3188fdce988045140e2b51ee678d85e4bdee
Yuyu Zhang <1204891354@qq.com>
2023-03-10 16:05:52 +0800

    ADM-363 add FlagCard component to consider "flag" as "block", and add unit tests for it.

--- frontend/__tests__/src/components/Metrics/MetricsStep/CycleTime.test.tsx
+++ frontend/__tests__/src/components/Metrics/MetricsStep/CycleTime.test.tsx
@@ -1,12 +1,37 @@
 import { render } from '@testing-library/react'
 import { CycleTime } from '@src/components/Metrics/MetricsStep/CycleTime'
+import { CYCLETIME_LIST } from '@src/constants'
+import userEvent from '@testing-library/user-event'
 
 const title = 'Cycle Time Settings'
-const setup = () => render(<CycleTime title={title} />)
+const FlagAsBlock = 'Consider the "Flag" as "Block"'
+const setup = () => render(<CycleTime title={title} options={CYCLETIME_LIST} />)
 
 describe('CycleTime', () => {
-  it('should show Crews when render Crews component', () => {
-    const { getByText } = setup()
-    expect(getByText(title)).toBeInTheDocument()
+  describe('CycleTime Title', () => {
+    it('should show Cycle Time title when render Crews component', () => {
+      const { getByText } = setup()
+      expect(getByText(title)).toBeInTheDocument()
+    })
+  })
+
+  describe('CycleTime Flag as Block', () => {
+    it('should show FlagAsBlock when render Crews component', () => {
+      const { getByText } = setup()
+      expect(getByText(FlagAsBlock)).toBeInTheDocument()
+    })
+
+    it('should be checked by default when initializing', () => {
+      const { getByRole } = setup()
+      expect(getByRole('checkbox')).toHaveProperty('checked', true)
+    })
+
+    it('should change checked when click', async () => {
+      const { getByRole } = setup()
+      await userEvent.click(getByRole('checkbox'))
+      expect(getByRole('checkbox')).toHaveProperty('checked', false)
+      await userEvent.click(getByRole('checkbox'))
+      expect(getByRole('checkbox')).toHaveProperty('checked', true)
+    })
   })
 })

85892cde987f84c6f95fe4109e6349d3bc3c74a8
Eason-T <awawerwer2007@163.com>
2023-03-14 07:59:49 +0800

    ADM-349 rename

--- backend/src/main/java/heartbeat/controller/pipeline/PipelineController.java
+++ backend/src/main/java/heartbeat/controller/pipeline/PipelineController.java
@@ -2,17 +2,20 @@ package heartbeat.controller.pipeline;
 
 import heartbeat.controller.pipeline.vo.BuildKiteResponse;
 import heartbeat.service.pipeline.buildKite.BuildKiteService;
-import jakarta.validation.constraints.NotBlank;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/pipeline")
+@RequestMapping("/pipelines")
 @Validated
-public class BuildKiteController {
+public class PipelineController {
 
 	private final BuildKiteService buildKiteService;
 

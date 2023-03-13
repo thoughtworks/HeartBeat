f1fd98565eedc5a92aa796cea46ee3260eaf77da
Ting Wang <ting.wang@thoughtworks.com>
2023-03-13 21:30:17 +0800

    ADM-364 delete mock data for targetField

--- frontend/src/context/board/jiraVerifyResponse/jiraVerifyResponseSlice.tsx
+++ frontend/src/context/board/jiraVerifyResponse/jiraVerifyResponseSlice.tsx
@@ -9,27 +9,7 @@ export interface jiraVerifyResponseState {
 
 const initialState: jiraVerifyResponseState = {
   jiraColumns: [],
-  targetFields: [
-    { key: 'issuetype', name: 'Issue Type', flag: false },
-    { key: 'parent', name: 'Parent', flag: false },
-    { key: 'customfield_10020', name: 'Sprint', flag: false },
-    { key: 'project', name: 'Project', flag: false },
-    { key: 'reporter', name: 'Reporter', flag: false },
-    { key: 'customfield_10021', name: 'Flagged', flag: false },
-    { key: 'fixVersions', name: 'Fix versions', flag: false },
-    { key: 'customfield_10000', name: 'Development', flag: false },
-    { key: 'priority', name: 'Priority', flag: false },
-    { key: 'customfield_10037', name: 'Partner', flag: false },
-    { key: 'labels', name: 'Labels', flag: false },
-    { key: 'timetracking', name: 'Time tracking', flag: false },
-    { key: 'customfield_10015', name: 'Start date', flag: false },
-    { key: 'customfield_10016', name: 'Story point estimate', flag: false },
-    { key: 'customfield_10038', name: 'QA', flag: false },
-    { key: 'customfield_10019', name: 'Rank', flag: false },
-    { key: 'assignee', name: 'Assignee', flag: false },
-    { key: 'customfield_10017', name: 'Issue color', flag: false },
-    { key: 'customfield_10027', name: 'Feature/Operation', flag: false },
-  ],
+  targetFields: [],
   users: [],
 }
 

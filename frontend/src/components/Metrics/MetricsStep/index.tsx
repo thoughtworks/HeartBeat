599141f09cd33b653530a4e4e4308c4d98485886
Ting Wang <ting.wang@thoughtworks.com>
2023-03-14 14:41:19 +0800

    ADM-364 control display of Classification component by requiredData

--- frontend/src/components/Metrics/MetricsStep/index.tsx
+++ frontend/src/components/Metrics/MetricsStep/index.tsx
@@ -3,14 +3,20 @@ import { Crews } from '@src/components/Metrics/MetricsStep/Crews'
 import { useAppSelector } from '@src/hooks'
 import { selectTargetFields, selectUsers } from '@src/context/board/jiraVerifyResponse/jiraVerifyResponseSlice'
 import { Classification } from '@src/components/Metrics/MetricsStep/Classification'
+import { selectMetrics } from '@src/context/config/configSlice'
+import { REQUIRED_DATA_LIST } from '@src/constants'
 
 export const MetricsStep = () => {
+  const requiredData = useAppSelector(selectMetrics)
   const users = useAppSelector(selectUsers)
   const targetFields = useAppSelector(selectTargetFields)
+
   return (
     <>
       <Crews options={users} title={'Crews Setting'} label={'Included Crews'} />
-      <Classification options={targetFields} title={'Classification Setting'} label={'Distinguished By'} />
+      {requiredData.length > 0 && requiredData.includes(REQUIRED_DATA_LIST[2]) && (
+        <Classification options={targetFields} title={'Classification Setting'} label={'Distinguished By'} />
+      )}
     </>
   )
 }

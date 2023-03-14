b3aa307ad8be3f1eb0acca531630c7bad305b844
Lanxin Wang <lanxin.wang@thoughtworks.com>
2023-03-14 17:07:22 +0800

    ADM-365 add deleteADeploymentFrequencySetting and addADeploymentFrequencySetting function

--- frontend/__tests__/src/context/pipelineMetricsSettings.test.tsx
+++ frontend/__tests__/src/context/pipelineMetricsSettings.test.tsx
@@ -1,13 +1,20 @@
 import pipelineMetricsSettingsReducer, {
   updateDeploymentFrequencySettings,
+  addADeploymentFrequencySetting,
+  deleteADeploymentFrequencySetting,
 } from '@src/context/pipelineMetricsSettings/pipelineMetricsSettingsSlice'
 
 describe('pipelineMetricsSettings reducer', () => {
+  const initPipelineMetricsSettings = {
+    deploymentFrequencySettings: [{ organization: '', pipelineName: '', steps: '' }],
+  }
+
   it('should return init DeploymentFrequencySettings when handle initial state', () => {
-    const initDeploymentFrequencySettings = [{ organization: '', pipelineName: '', steps: '' }]
     const pipelineMetricsSettings = pipelineMetricsSettingsReducer(undefined, { type: 'unknown' })
 
-    expect(pipelineMetricsSettings.deploymentFrequencySettings).toEqual(initDeploymentFrequencySettings)
+    expect(pipelineMetricsSettings.deploymentFrequencySettings).toEqual(
+      initPipelineMetricsSettings.deploymentFrequencySettings
+    )
   })
 
   it('should update deploymentFrequencySettings when handle updateDeploymentFrequencySettings given initial state', () => {
@@ -16,10 +23,46 @@ describe('pipelineMetricsSettings reducer', () => {
     ]
 
     const pipelineMetricsSettings = pipelineMetricsSettingsReducer(
-      undefined,
+      initPipelineMetricsSettings,
       updateDeploymentFrequencySettings(mockDeploymentFrequencySettings)
     )
 
     expect(pipelineMetricsSettings.deploymentFrequencySettings).toEqual(mockDeploymentFrequencySettings)
+  })
+
+  it('should add a deploymentFrequencySetting when handle addADeploymentFrequencySettings given initial state', () => {
+    const addedDeploymentFrequencySettings = [
+      { organization: '', pipelineName: '', steps: '' },
+      { organization: '', pipelineName: '', steps: '' },
+    ]
+
+    const pipelineMetricsSettings = pipelineMetricsSettingsReducer(
+      initPipelineMetricsSettings,
+      addADeploymentFrequencySetting()
+    )
+
+    expect(pipelineMetricsSettings.deploymentFrequencySettings).toEqual(addedDeploymentFrequencySettings)
+  })
+
+  it('should delete a deploymentFrequencySetting when handle deleteADeploymentFrequencySettings given some state value', () => {
+    const mockDeploymentFrequencySettings = {
+      deploymentFrequencySettings: [
+        { organization: 'mock organization 1', pipelineName: 'mock pipeline name 1', steps: 'step 1' },
+        { organization: 'mock organization 2', pipelineName: 'mock pipeline name 2', steps: 'step 2' },
+      ],
+    }
+
+    const pipelineMetricsSettings = pipelineMetricsSettingsReducer(
+      mockDeploymentFrequencySettings,
+      deleteADeploymentFrequencySetting(0)
+    )
+
+    expect(pipelineMetricsSettings.deploymentFrequencySettings).toEqual([
+      {
+        organization: 'mock organization 2',
+        pipelineName: 'mock pipeline name 2',
+        steps: 'step 2',
+      },
+    ])
   })
 })

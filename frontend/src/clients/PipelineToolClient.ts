ad431532ea693debfd86ece2e5f68ebd292ab2e2
shawn he <64237577+spadio9587@users.noreply.github.com>
2023-03-15 17:31:17 +0800

    ADM-350 check with backend about URL and add buildKite url in application.yml

--- frontend/src/clients/PipelineToolClient.ts
+++ frontend/src/clients/PipelineToolClient.ts
@@ -16,7 +16,9 @@ export class PipelineToolClient extends HttpClient {
 
   verifyPipelineTool = async (params: getVerifyPipelineToolParams) => {
     try {
-      const result = await this.axiosInstance.get('/pipeline/fetch', { params: { ...params } }).then((res) => res)
+      const result = await this.axiosInstance
+        .get(`/pipelines/${params.type}`, { params: { ...params } })
+        .then((res) => res)
       this.isPipelineToolVerified = true
       this.response = result
     } catch (e) {

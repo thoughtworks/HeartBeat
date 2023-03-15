4f8284f61ca7c85725d9ff923997bb0fb9178529
Lanxin Wang <lanxin.wang@thoughtworks.com>
2023-03-15 10:43:47 +0800

    ADM-365 store update value to redux slice

--- frontend/__tests__/src/components/Metrics/MetricsStep/DeploymentFrequencySettings/SingleSelection.test.tsx
+++ frontend/__tests__/src/components/Metrics/MetricsStep/DeploymentFrequencySettings/SingleSelection.test.tsx
@@ -1,14 +1,20 @@
 import { render } from '@testing-library/react'
 import { SingleSelection } from '@src/components/Metrics/MetricsStep/DeploymentFrequencySettings/SingleSelection'
 import userEvent from '@testing-library/user-event'
+import { Provider } from 'react-redux'
+import { store } from '@src/store'
 
 describe('SingleSelection', () => {
   const mockOptions = ['mockOptions 1', 'mockOptions 2', 'mockOptions 3']
   const mockLabel = 'mockLabel'
   const mockValue = 'mockOptions 1'
-  const setUp = () => {
-    return render(<SingleSelection options={mockOptions} label={mockLabel} value={mockValue} />)
-  }
+  const setUp = () =>
+    render(
+      <Provider store={store}>
+        <SingleSelection options={mockOptions} label={mockLabel} value={mockValue} index={0} />
+      </Provider>
+    )
+
   it('should render SingleSelection', () => {
     const { getByText } = setUp()
 
--- frontend/src/components/Metrics/MetricsStep/DeploymentFrequencySettings/SingleSelection/index.tsx
+++ frontend/src/components/Metrics/MetricsStep/DeploymentFrequencySettings/SingleSelection/index.tsx
@@ -1,25 +1,31 @@
 import { InputLabel, ListItemText, MenuItem, Select, SelectChangeEvent } from '@mui/material'
 import React, { useState } from 'react'
 import { FormControlWrapper } from './style'
+import { useAppDispatch } from '@src/hooks'
+import { updateDeploymentFrequencySettings } from '@src/context/pipelineMetricsSettings/pipelineMetricsSettingsSlice'
 
 interface Props {
   options: string[]
   label: string
   value: string
+  index: number
 }
 
-export const SingleSelection = ({ options, label, value }: Props) => {
+export const SingleSelection = ({ options, label, value, index }: Props) => {
+  const dispatch = useAppDispatch()
   const [selectedValue, setSelectedValue] = useState(value)
 
   const handleChange = (event: SelectChangeEvent) => {
     const value = event.target.value
     setSelectedValue(value)
+    dispatch(updateDeploymentFrequencySettings({ updateIndex: index, label, value }))
   }
+
   return (
     <>
       <FormControlWrapper variant='standard' required>
-        <InputLabel id='crew-data-multiple-checkbox-label'>{label}</InputLabel>
-        <Select labelId='crew-data-multiple-checkbox-label' value={selectedValue} onChange={handleChange}>
+        <InputLabel id='single-selection-label'>{label}</InputLabel>
+        <Select labelId='single-selection-label' value={selectedValue} onChange={handleChange}>
           {options.map((data) => (
             <MenuItem key={data} value={data}>
               <ListItemText primary={data} />

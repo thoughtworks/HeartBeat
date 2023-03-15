2cd054b4fc913ecf28c2315d1ae45facc52f648d
Lanxin Wang <lanxin.wang@thoughtworks.com>
2023-03-15 10:46:27 +0800

    ADM-365 add remove button logic

--- frontend/__tests__/src/components/Metrics/MetricsStep/DeploymentFrequencySettings/PipelineMetricSelection.test.tsx
+++ frontend/__tests__/src/components/Metrics/MetricsStep/DeploymentFrequencySettings/PipelineMetricSelection.test.tsx
@@ -1,9 +1,23 @@
 import { render } from '@testing-library/react'
 import { PipelineMetricSelection } from '@src/components/Metrics/MetricsStep/DeploymentFrequencySettings/PipelineMetricSelection'
+import { Provider } from 'react-redux'
+import { store } from '@src/store'
+import userEvent from '@testing-library/user-event'
+import { deleteADeploymentFrequencySetting } from '@src/context/pipelineMetricsSettings/pipelineMetricsSettingsSlice'
 
 jest.mock('@src/components/Metrics/MetricsStep/DeploymentFrequencySettings/SingleSelection', () => ({
   SingleSelection: () => <div>mock SingleSelection</div>,
 }))
+
+jest.mock('@src/hooks', () => ({
+  useAppDispatch: () => jest.fn(),
+}))
+
+jest.mock('@src/context/pipelineMetricsSettings/pipelineMetricsSettingsSlice', () => ({
+  deleteADeploymentFrequencySetting: jest.fn(),
+}))
+
+const mockIndex = 0
 
 const deploymentFrequencySetting = {
   organization: 'mock organization',
@@ -11,13 +25,41 @@ const deploymentFrequencySetting = {
   steps: 'mock steps',
 }
 
-describe('PipelineMetricSelection', () => {
-  it('should render PipelineMetricSelection', () => {
-    const { getByText, getAllByText } = render(
-      <PipelineMetricSelection deploymentFrequencySetting={deploymentFrequencySetting} />
-    )
+const setUp = async (mockIndex: number, isShowRemoveButton: boolean) => {
+  return render(
+    <Provider store={store}>
+      <PipelineMetricSelection
+        deploymentFrequencySetting={deploymentFrequencySetting}
+        index={mockIndex}
+        isShowRemoveButton={isShowRemoveButton}
+      />
+    </Provider>
+  )
+}
 
+describe('PipelineMetricSelection', () => {
+  afterEach(() => {
+    jest.resetAllMocks()
+  })
+
+  it('should render PipelineMetricSelection when isShowRemoveButton is true', async () => {
+    const { getAllByText, getByText } = await setUp(mockIndex, true)
     expect(getByText('Remove this pipeline')).toBeInTheDocument()
     expect(getAllByText('mock SingleSelection').length).toEqual(3)
+  })
+
+  it('should render PipelineMetricSelection when isShowRemoveButton is false', async () => {
+    const { getAllByText, queryByText } = await setUp(mockIndex, false)
+    expect(queryByText('Remove this pipeline')).not.toBeInTheDocument()
+    expect(getAllByText('mock SingleSelection').length).toEqual(3)
+  })
+
+  it('should call deleteADeploymentFrequencySetting function when click remove this pipeline button', async () => {
+    const { getByRole } = await setUp(mockIndex, true)
+
+    await userEvent.click(getByRole('button', { name: 'Remove this pipeline' }))
+
+    expect(deleteADeploymentFrequencySetting).toHaveBeenCalledTimes(1)
+    expect(deleteADeploymentFrequencySetting).toHaveBeenCalledWith(mockIndex)
   })
 })
--- frontend/src/components/Metrics/MetricsStep/DeploymentFrequencySettings/PipelineMetricSelection/index.tsx
+++ frontend/src/components/Metrics/MetricsStep/DeploymentFrequencySettings/PipelineMetricSelection/index.tsx
@@ -1,6 +1,8 @@
 import { RemoveButton } from '@src/components/Metrics/MetricsStep/DeploymentFrequencySettings/style'
 import React from 'react'
 import { SingleSelection } from '@src/components/Metrics/MetricsStep/DeploymentFrequencySettings/SingleSelection'
+import { useAppDispatch } from '@src/hooks'
+import { deleteADeploymentFrequencySetting } from '@src/context/pipelineMetricsSettings/pipelineMetricsSettingsSlice'
 
 interface pipelineMetricSelectionProps {
   deploymentFrequencySetting: {
@@ -8,15 +10,37 @@ interface pipelineMetricSelectionProps {
     pipelineName: string
     steps: string
   }
+  index: number
+  isShowRemoveButton: boolean
 }
 
-export const PipelineMetricSelection = ({ deploymentFrequencySetting }: pipelineMetricSelectionProps) => {
+export const PipelineMetricSelection = ({
+  deploymentFrequencySetting,
+  index,
+  isShowRemoveButton,
+}: pipelineMetricSelectionProps) => {
+  const dispatch = useAppDispatch()
+
+  const handleClick = () => {
+    dispatch(deleteADeploymentFrequencySetting(index))
+  }
+
   return (
     <>
-      <RemoveButton> Remove this pipeline</RemoveButton>
-      <SingleSelection options={['o1', 'o2']} label={'Organization'} value={deploymentFrequencySetting.organization} />
-      <SingleSelection options={['p1', 'p2']} label={'Pipeline Name'} value={deploymentFrequencySetting.pipelineName} />
-      <SingleSelection options={['s1', 's2']} label={'Steps'} value={deploymentFrequencySetting.steps} />
+      {isShowRemoveButton && <RemoveButton onClick={handleClick}>Remove this pipeline</RemoveButton>}
+      <SingleSelection
+        options={['o1', 'o2']}
+        label={'Organization'}
+        value={deploymentFrequencySetting.organization}
+        index={index}
+      />
+      <SingleSelection
+        options={['p1', 'p2']}
+        label={'Pipeline Name'}
+        value={deploymentFrequencySetting.pipelineName}
+        index={index}
+      />
+      <SingleSelection options={['s1', 's2']} label={'Steps'} value={deploymentFrequencySetting.steps} index={index} />
     </>
   )
 }

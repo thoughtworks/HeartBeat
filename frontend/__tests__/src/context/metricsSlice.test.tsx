f1939b5147208422f47feb0448287c70b2a9855a
Yuyu Zhang <1204891354@qq.com>
2023-03-15 13:36:09 +0800

    ADM-363 add test for store data in boardColumns

--- frontend/__tests__/src/context/metricsSlice.test.tsx
+++ frontend/__tests__/src/context/metricsSlice.test.tsx
@@ -1,4 +1,8 @@
-import saveMetricsSettingReducer, { updateTargetFields, updateUsers } from '@src/context/Metrics/metricsSlice'
+import saveMetricsSettingReducer, {
+  savedBoardColumns,
+  updateTargetFields,
+  updateUsers,
+} from '@src/context/Metrics/metricsSlice'
 
 describe('saveMetricsSetting reducer', () => {
   it('should show empty array when handle initial state', () => {
@@ -7,6 +11,7 @@ describe('saveMetricsSetting reducer', () => {
     expect(savedMetricsSetting.users).toEqual([])
     expect(savedMetricsSetting.targetFields).toEqual([])
     expect(savedMetricsSetting.jiraColumns).toEqual([])
+    expect(savedMetricsSetting.boardColumns).toEqual([])
   })
 
   it('should store updated targetFields when its value changed', () => {
@@ -15,26 +20,10 @@ describe('saveMetricsSetting reducer', () => {
         { key: 'issuetype', name: 'Issue Type', flag: true },
         { key: 'parent', name: 'Parent', flag: false },
         { key: 'customfield_10020', name: 'Sprint', flag: false },
-        { key: 'project', name: 'Project', flag: false },
-        { key: 'reporter', name: 'Reporter', flag: false },
-        { key: 'customfield_10021', name: 'Flagged', flag: false },
-        { key: 'fixVersions', name: 'Fix versions', flag: false },
-        { key: 'customfield_10000', name: 'Development', flag: false },
-        { key: 'priority', name: 'Priority', flag: false },
-        { key: 'customfield_10037', name: 'Partner', flag: false },
-        { key: 'labels', name: 'Labels', flag: false },
-        { key: 'timetracking', name: 'Time tracking', flag: false },
-        { key: 'customfield_10015', name: 'Start date', flag: false },
-        { key: 'customfield_10016', name: 'Story point estimate', flag: false },
-        { key: 'customfield_10038', name: 'QA', flag: false },
-        { key: 'customfield_10019', name: 'Rank', flag: false },
-        { key: 'assignee', name: 'Assignee', flag: false },
-        { key: 'customfield_10017', name: 'Issue color', flag: false },
-        { key: 'customfield_10027', name: 'Feature/Operation', flag: false },
       ],
     }
     const savedMetricsSetting = saveMetricsSettingReducer(
-      { jiraColumns: [], targetFields: [], users: [] },
+      { jiraColumns: [], targetFields: [], users: [], boardColumns: [] },
       updateTargetFields({
         targetFields: mockUpdatedTargetFields.targetFields,
       })
@@ -50,12 +39,26 @@ describe('saveMetricsSetting reducer', () => {
       users: ['userOne', 'userTwo', 'userThree'],
     }
     const savedMetricsSetting = saveMetricsSettingReducer(
-      { jiraColumns: [], targetFields: [], users: [] },
+      { jiraColumns: [], targetFields: [], users: [], boardColumns: [] },
       updateUsers({
         users: mockUpdatedUsers.users,
       })
     )
 
     expect(savedMetricsSetting.users).toEqual(mockUpdatedUsers)
+  })
+
+  it('should store saved boardColumns when its value changed', () => {
+    const mockSavedBoardColumns = {
+      boardColumns: [{ name: 'TODO', value: 'To do' }],
+    }
+    const savedMetricsSetting = saveMetricsSettingReducer(
+      { jiraColumns: [], targetFields: [], users: [], boardColumns: [] },
+      savedBoardColumns({
+        boardColumns: mockSavedBoardColumns.boardColumns,
+      })
+    )
+
+    expect(savedMetricsSetting.boardColumns).toEqual(mockSavedBoardColumns)
   })
 })

172e82cd59e92addcf56f62c7e28870849503ddf
Lanxin Wang <lanxin.wang@thoughtworks.com>
2023-03-24 11:21:01 +0800

    ADM-365 fix MetricsStepper tests

--- frontend/__tests__/src/components/Metrics/MetricsStepper/MetricsStepper.test.tsx
+++ frontend/__tests__/src/components/Metrics/MetricsStepper/MetricsStepper.test.tsx
@@ -30,6 +30,23 @@ const TODAY = dayjs()
 const INPUT_DATE_VALUE = TODAY.format('MM/DD/YYYY')
 const END_DATE_LABEL = 'To *'
 
+const mockedUsedNavigate = jest.fn()
+jest.mock('react-router-dom', () => ({
+  ...jest.requireActual('react-router-dom'),
+  useNavigate: () => mockedUsedNavigate,
+}))
+
+const mockValidationCheckContext = {
+  errorMessages: [],
+  clearErrorMessage: jest.fn(),
+  checkDuplicatedPipeLine: jest.fn(),
+  isPipelineValid: jest.fn().mockReturnValue(true),
+}
+
+jest.mock('@src/hooks/useMetricsStepValidationCheckContext', () => ({
+  useMetricsStepValidationCheckContext: () => mockValidationCheckContext,
+}))
+
 const YES = 'Yes'
 const CANCEL = 'Cancel'
 const METRICS = 'Metrics'

b1239885dcf258d4a16e5c21cf0b35dfb9787385
zhiqiao.yang <zhiqiao.yang@thoughtworks.com>
2023-03-27 17:01:11 +0800

    ADM-356 add import route and import file logic

--- frontend/__tests__/src/components/HomeGuide/HomeGuide.test.tsx
+++ frontend/__tests__/src/components/HomeGuide/HomeGuide.test.tsx
@@ -1,7 +1,9 @@
 import { HomeGuide } from '@src/components/HomeGuide'
-import { fireEvent, render } from '@testing-library/react'
+import { fireEvent, render, waitFor } from '@testing-library/react'
+import { setupStore } from '../../utils/setupStoreUtil'
+import { Provider } from 'react-redux'
+import { CREATE_NEW_PROJECT, IMPORT_PROJECT_FROM_FILE } from '../../fixtures'
 
-const CREATE_NEW_PROJECT = 'Create a new project'
 const mockedUsedNavigate = jest.fn()
 
 jest.mock('react-router-dom', () => ({
@@ -9,20 +11,52 @@ jest.mock('react-router-dom', () => ({
   useNavigate: () => mockedUsedNavigate,
 }))
 
+let store = setupStore()
+
+const setup = () => {
+  store = setupStore()
+  return render(
+    <Provider store={store}>
+      <HomeGuide />
+    </Provider>
+  )
+}
+beforeEach(() => {
+  store = setupStore()
+})
+
 describe('HomeGuide', () => {
   it('should show 2 buttons', () => {
-    const { getByText } = render(<HomeGuide />)
+    const { getByText } = setup()
 
-    expect(getByText('Import project from file')).toBeInTheDocument()
+    expect(getByText(IMPORT_PROJECT_FROM_FILE)).toBeInTheDocument()
     expect(getByText(CREATE_NEW_PROJECT)).toBeInTheDocument()
   })
 
   it('should go to Metrics page when click create a new project button', async () => {
-    const { getByText } = render(<HomeGuide />)
+    const { getByText } = setup()
 
     fireEvent.click(getByText(CREATE_NEW_PROJECT))
 
     expect(mockedUsedNavigate).toHaveBeenCalledTimes(1)
     expect(mockedUsedNavigate).toHaveBeenCalledWith('/metrics')
+  })
+
+  it('should go to Metrics page and read file when click import file button', async () => {
+    const { getByLabelText } = setup()
+
+    const file = new File(['{"projectName": "Heartbeat test"}'], 'test.json', {
+      type: 'file',
+    })
+    const input = getByLabelText('Import project from file')
+
+    Object.defineProperty(input, 'files', {
+      value: [file],
+    })
+
+    fireEvent.change(input)
+    await waitFor(() => {
+      expect(mockedUsedNavigate).toHaveBeenCalledWith('/metrics')
+    })
   })
 })
--- frontend/src/components/HomeGuide/index.tsx
+++ frontend/src/components/HomeGuide/index.tsx
@@ -4,6 +4,8 @@ import { styled } from '@mui/material/styles'
 
 import { theme } from '@src/theme'
 import { useNavigate } from 'react-router-dom'
+import { useAppDispatch } from '@src/hooks/useAppDispatch'
+import { isProjectCreated, updateBasicConfigState } from '@src/context/config/configSlice'
 
 const basicStyle = {
   backgroundColor: theme.main.backgroundColor,
@@ -28,13 +30,33 @@ const GuideButton = styled(Button)<ButtonProps>({
 
 export const HomeGuide = () => {
   const navigate = useNavigate()
+  const dispatch = useAppDispatch()
+
+  const importProject = (e: { target: any }) => {
+    const input = e.target
+    console.log(input.files)
+    const reader = new FileReader()
+    reader.onload = () => {
+      if (reader.result && typeof reader.result === 'string') {
+        dispatch(isProjectCreated(false))
+        dispatch(updateBasicConfigState(JSON.parse(reader.result)))
+        navigate('/metrics')
+      }
+    }
+    reader.readAsText(input.files[0], 'utf-8')
+  }
+  const createNewProject = () => {
+    dispatch(isProjectCreated(true))
+    navigate('/metrics')
+  }
 
   return (
     <Stack direction='column' justifyContent='center' alignItems='center' flex={'auto'}>
       <GuideButton>
-        <span>Import project from file</span>
+        <label htmlFor='importJson'>Import project from file</label>
       </GuideButton>
-      <GuideButton onClick={() => navigate('/metrics')}>Create a new project</GuideButton>
+      <input hidden type='file' id='importJson' accept='.json' onChange={importProject} />
+      <GuideButton onClick={createNewProject}>Create a new project</GuideButton>
     </Stack>
   )
 }

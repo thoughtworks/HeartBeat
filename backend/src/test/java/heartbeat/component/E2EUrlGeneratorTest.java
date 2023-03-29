6e84685f3ad1b4d4fd083d5a118b20abb3922676
Eason-T <awawerwer2007@163.com>
2023-03-29 14:30:36 +0800

    ADM-375 delete unuseful annotation in E2EUrlGeneratorTest

--- backend/src/test/java/heartbeat/component/E2EUrlGeneratorTest.java
+++ backend/src/test/java/heartbeat/component/E2EUrlGeneratorTest.java
@@ -1,18 +1,11 @@
 package heartbeat.component;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Profile;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.net.URI;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-@SpringBootTest
-@Profile("e2e")
-@ExtendWith(SpringExtension.class)
 class E2EUrlGeneratorTest {
 
 	private final E2EUrlGenerator e2EUrlGenerator = new E2EUrlGenerator();

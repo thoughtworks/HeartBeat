1de6ef63333b9ac5959fd093c3045ee8d92b4fd9
fengxinhou <fengxin.hou@thoughtworks.com>
2023-03-30 17:50:31 +0800

    ADM-368 fix codacy warning

--- frontend/src/components/Metrics/ReportStep/index.tsx
+++ frontend/src/components/Metrics/ReportStep/index.tsx
@@ -15,6 +15,7 @@ export const ReportStep = () => {
         setVelocityData(res.response?.velocity)
       }
     })
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
   return (
     <>

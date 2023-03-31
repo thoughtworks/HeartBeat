b97bc4aca8d70a308412ce0dde14e36ae41ebb99
guzhongren <guzhongren@live.cn>
2023-03-31 21:05:42 +0800

    ADM-400 deploy [stub]

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import { resolve } from 'path/posix'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   server: {
     port: 4321,

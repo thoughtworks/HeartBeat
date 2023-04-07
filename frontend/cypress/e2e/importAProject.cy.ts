650c226f1bb8d318a321557c53a5728780bcb930
ZhiqiaoYang <126433704+ZhiqiaoYang@users.noreply.github.com>
2023-04-07 15:36:47 +0800

    ADM-356 Fix name of type in e2e

--- frontend/cypress/e2e/importAProject.cy.ts
+++ frontend/cypress/e2e/importAProject.cy.ts
@@ -10,7 +10,7 @@ describe('Import project from file', () => {
     cy.get('.MuiInput-input')
       .invoke('val')
       .then((value) => {
-        expect(value).to.equal('mockProjectName')
+        expect(value).to.equal('importConfigFile')
       })
 
     configPage.verifyAndClickNextToMetrics()

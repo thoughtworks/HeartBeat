88a10969ea3dd6f2a812569b69917d596775677d
shawn he <64237577+spadio9587@users.noreply.github.com>
2023-04-12 14:42:23 +0800

    ADM-369 add cycleTime in ReportStep page

--- frontend/src/components/Metrics/ReportStep/index.tsx
+++ frontend/src/components/Metrics/ReportStep/index.tsx
@@ -5,11 +5,13 @@ import { Loading } from '@src/components/Loading'
 import { useAppSelector } from '@src/hooks'
 import { selectConfig } from '@src/context/config/configSlice'
 import { reportResponseMapper } from '@src/mapper/ReportMapper'
-import { INIT_VELOCITY_METRICS } from '@src/constants'
+import { INIT_CYCLETIME_METRICS, INIT_VELOCITY_METRICS } from '@src/constants'
+import { CycleTimeReport } from '@src/components/Metrics/ReportStep/CycleTime'
 
 export const ReportStep = () => {
   const { generateReport, isLoading } = useGenerateReportEffect()
   const [velocityData, setVelocityData] = useState(INIT_VELOCITY_METRICS)
+  const [cycleTimeData, setCycleTimeData] = useState(INIT_CYCLETIME_METRICS)
   const configData = useAppSelector(selectConfig)
   const { metrics, calendarType, dateRange } = configData.basic
   const { board, pipelineTool, sourceControl } = configData
@@ -28,9 +30,15 @@ export const ReportStep = () => {
         console.log(res.response.classification)
         const reportData = reportResponseMapper(res.response)
         setVelocityData(reportData.velocityValues)
+        setCycleTimeData(reportData.cycleValues)
       }
     })
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
-  return <>{isLoading ? <Loading /> : <Velocity title={'Velocity'} velocityData={velocityData} />}</>
+  return (
+    <>
+      {isLoading ? <Loading /> : <Velocity title={'Velocity'} velocityData={velocityData} />}
+      <CycleTimeReport title={'Cycle Time'} cycleTimeData={cycleTimeData}></CycleTimeReport>
+    </>
+  )
 }

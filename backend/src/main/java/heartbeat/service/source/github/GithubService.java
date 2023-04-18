06f6468c7eccb3905896f35edec7c608a62ba4d6
wangwang.zhang <wangwang.zhang@thoughtworks.com>
2023-04-18 16:58:17 +0800

    ADM-433 refactor: use TokenUtil to mask token

--- backend/src/main/java/heartbeat/service/source/github/GithubService.java
+++ backend/src/main/java/heartbeat/service/source/github/GithubService.java
@@ -6,6 +6,7 @@ import heartbeat.client.dto.codebase.github.GitHubOrganizationsInfo;
 import heartbeat.client.dto.codebase.github.GitHubRepos;
 import heartbeat.controller.source.dto.GitHubResponse;
 import heartbeat.exception.RequestFailedException;
+import heartbeat.util.TokenUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Service;
@@ -24,27 +25,28 @@ public class GithubService {
 
 	public GitHubResponse verifyToken(String githubToken) {
 		String token = "token " + githubToken;
-		String partialToken = githubToken.substring(0, 6);
 		try {
-			log.info("[Github] Start to query repository_url by token, token: " + partialToken);
+			log.info("[Github] Start to query repository_url by token, token: " + TokenUtil.mask(token));
 			List<String> githubReposByUser = githubFeignClient.getAllRepos(token)
 				.stream()
 				.map(GitHubRepos::getHtml_url)
 				.toList();
-			log.info("[Github] Successfully get repository_url by token, token: " + partialToken + " repos: "
+			log.info("[Github] Successfully get repository_url by token, token: " + TokenUtil.mask(token) + " repos: "
 					+ githubReposByUser);
 
-			log.info("[Github] Start to query organization_url by token, token: " + partialToken);
+			log.info("[Github] Start to query organization_url by token, token: " + TokenUtil.mask(token));
 			List<GitHubOrganizationsInfo> githubOrganizations = githubFeignClient.getGithubOrganizationsInfo(token);
-			log.info("[Github] Successfully get organizations by token, token: " + partialToken + " organizations: "
-					+ githubOrganizations);
+			log.info("[Github] Successfully get organizations by token, token: " + TokenUtil.mask(token)
+					+ " organizations: " + githubOrganizations);
 
 			LinkedHashSet<String> githubRepos = new LinkedHashSet<>(githubReposByUser);
 
-			log.info("[Github] Start to query repository_url by organization_name and token, token: " + partialToken);
+			log.info("[Github] Start to query repository_url by organization_name and token, token: "
+					+ TokenUtil.mask(token));
 			Set<String> githubReposByOrganizations = getAllGithubRepos(token, githubOrganizations);
 			githubRepos.addAll(githubReposByOrganizations);
-			log.info("[Github] Successfully get all repository_url, token: " + partialToken + " repos: " + githubRepos);
+			log.info("[Github] Successfully get all repository_url, token: " + TokenUtil.mask(token) + " repos: "
+					+ githubRepos);
 
 			return GitHubResponse.builder().githubRepos(githubRepos).build();
 		}

819a041bce5ac2c35ab2458f77602bd6ddab3140
wangwang.zhang <wangwang.zhang@thoughtworks.com>
2023-04-21 08:50:10 +0800

    ADM-433 optimize: implemented async

--- backend/src/main/java/heartbeat/service/source/github/GitHubService.java
+++ backend/src/main/java/heartbeat/service/source/github/GitHubService.java
@@ -7,41 +7,63 @@ import heartbeat.client.dto.codebase.github.GitHubRepos;
 import heartbeat.controller.source.dto.GitHubResponse;
 import heartbeat.exception.RequestFailedException;
 import heartbeat.util.TokenUtil;
+import jakarta.annotation.PreDestroy;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.stereotype.Service;
 
 import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.concurrent.CompletableFuture;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @Service
 @RequiredArgsConstructor
 @Log4j2
 public class GitHubService {
 
+	@Autowired
+	private final ThreadPoolTaskExecutor taskExecutor;
+
 	private final GitHubFeignClient gitHubFeignClient;
+
+	@PreDestroy
+	public void shutdownExecutor() {
+		taskExecutor.shutdown();
+	}
 
 	public GitHubResponse verifyToken(String githubToken) {
 		String token = "token " + githubToken;
 		String maskToken = TokenUtil.mask(token);
 		try {
 			log.info("Start to query repository_url by token, token: {}", maskToken);
-			List<String> githubReposByUser = gitHubFeignClient.getAllRepos(token)
-				.stream()
-				.map(GitHubRepos::getHtml_url)
-				.toList();
-			log.info("Successfully get repository_token: {}, githubReposByUser: {}", maskToken, githubReposByUser);
+			CompletableFuture<List<GitHubRepos>> githubReposByUserFuture = CompletableFuture
+				.supplyAsync(() -> gitHubFeignClient.getAllRepos(token), taskExecutor);
 
 			log.info("Start to query organizations_token: {}", maskToken);
-			List<GitHubOrganizationsInfo> githubOrganizations = gitHubFeignClient.getGithubOrganizationsInfo(token);
-			log.info("Successfully get organizations_token: {} organizations: {}", maskToken, githubOrganizations);
+			CompletableFuture<List<GitHubOrganizationsInfo>> githubOrganizationsFuture = CompletableFuture
+				.supplyAsync(() -> gitHubFeignClient.getGithubOrganizationsInfo(token), taskExecutor);
 
-			LinkedHashSet<String> githubRepos = new LinkedHashSet<>(githubReposByUser);
-			Set<String> githubReposByOrganizations = getAllGitHubRepos(token, githubOrganizations);
-			githubRepos.addAll(githubReposByOrganizations);
-			return GitHubResponse.builder().githubRepos(githubRepos).build();
+			CompletableFuture<GitHubResponse> combinedFuture = githubReposByUserFuture
+				.thenCombineAsync(githubOrganizationsFuture, (githubReposByUser, githubOrganizations) -> {
+					log.info("Successfully get repository_token: {}, githubReposByUser: {}", maskToken,
+							githubReposByUser);
+					log.info("Successfully get organizations_token: {} organizations: {}", maskToken,
+							githubOrganizations);
+					List<String> githubReposMapped = githubReposByUser.stream().map(GitHubRepos::getHtml_url).toList();
+					LinkedHashSet<String> githubRepos = new LinkedHashSet<>(githubReposMapped);
+					CompletableFuture<Set<String>> githubReposByOrganizations = getAllGitHubReposAsync(token,
+							githubOrganizations);
+					Set<String> allGitHubRepos = githubReposByOrganizations.join();
+					log.info("Successfully get all repositories_token: {}, repos: {}", maskToken, allGitHubRepos);
+					githubRepos.addAll(allGitHubRepos);
+					return GitHubResponse.builder().githubRepos(githubRepos).build();
+				}, taskExecutor);
+			return combinedFuture.join();
 		}
 		catch (FeignException e) {
 			log.error("Failed to call Github with token_error: {}", e);
@@ -49,19 +71,22 @@ public class GitHubService {
 		}
 	}
 
-	private Set<String> getAllGitHubRepos(String token, List<GitHubOrganizationsInfo> gitHubOrganizations) {
+	private CompletableFuture<Set<String>> getAllGitHubReposAsync(String token,
+			List<GitHubOrganizationsInfo> gitHubOrganizations) {
 		String maskToken = TokenUtil.mask(token);
-		log.info("Start to query repository by organization_token: {}, gitHubOrganizations: {}", maskToken,
-				gitHubOrganizations);
-		Set<String> allGitHubRepos = gitHubOrganizations.stream()
+		List<CompletableFuture<Stream<String>>> repoFutures = gitHubOrganizations.stream()
 			.map(GitHubOrganizationsInfo::getLogin)
-			.flatMap(org -> gitHubFeignClient.getReposByOrganizationName(org, token)
-				.stream()
-				.map(GitHubRepos::getHtml_url))
-			.collect(Collectors.toSet());
-		log.info("Successfully get all repositories_token: {}, repos: {}", maskToken, allGitHubRepos);
-		return allGitHubRepos;
+			.map(org -> CompletableFuture.supplyAsync(() -> {
+				log.info("Start to query repository by organization_token: {}, gitHubOrganization: {}", maskToken, org);
+				Stream<String> stringStream = gitHubFeignClient.getReposByOrganizationName(org, token)
+					.stream()
+					.map(GitHubRepos::getHtml_url);
+				log.info("End to queried repository by organization_token: {}, gitHubOrganization: {}", maskToken, org);
+				return stringStream;
+			}, taskExecutor)).toList();
 
+		return CompletableFuture.allOf(repoFutures.toArray(new CompletableFuture[0]))
+			.thenApply(v -> repoFutures.stream().flatMap(CompletableFuture::join).collect(Collectors.toSet()));
 	}
 
 }

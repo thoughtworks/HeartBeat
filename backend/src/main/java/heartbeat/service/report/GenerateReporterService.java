df997a28adf1ef8abd99ccd391dc5a177f67a1fb
zhiqiao.yang <zhiqiao.yang@thoughtworks.com>
2023-04-26 09:53:04 +0800

    ADM-436 fix error from rebase

--- backend/src/main/java/heartbeat/service/report/GenerateReporterService.java
+++ backend/src/main/java/heartbeat/service/report/GenerateReporterService.java
@@ -25,7 +25,6 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;

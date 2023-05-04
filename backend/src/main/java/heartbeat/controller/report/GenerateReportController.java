e098020254648ed58c64127868fb746127efcbf1
shawn he <64237577+spadio9587@users.noreply.github.com>
2023-05-04 13:55:09 +0800

    ADM-422 add test for calculate classification

--- backend/src/main/java/heartbeat/controller/report/GenerateReportController.java
+++ backend/src/main/java/heartbeat/controller/report/GenerateReportController.java
@@ -6,10 +6,7 @@ import heartbeat.controller.report.dto.response.GenerateReportResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
--- backend/src/test/java/heartbeat/service/report/CalculateClassificationTest.java
+++ backend/src/test/java/heartbeat/service/report/CalculateClassificationTest.java
@@ -17,8 +17,9 @@ import org.mockito.quality.Strictness;
 
 import java.util.*;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 @MockitoSettings(strictness = Strictness.LENIENT)
@@ -63,5 +64,179 @@ class CalculateClassificationTest {
 		assertEquals("Priority", classification.getFieldName());
 	}
 
+	@Test
+	public void testMapArrayField() {
+		Map<String, Integer> map = new HashMap<>();
+		map.put("Field1", 0);
+		map.put("Field2", 0);
+		map.put("None", 1);
+		Map<String, Map<String, Integer>> resultMap = new HashMap<>();
+		resultMap.put("TestField", map);
+
+		// 创建虚拟的List对象
+		List<Map<String, Object>> obj = new ArrayList<>();
+		Map<String, Object> item1 = new HashMap<>();
+		item1.put("displayName", "Value1");
+		obj.add(item1);
+		Map<String, Object> item2 = new HashMap<>();
+		item2.put("displayName", "Value2");
+		obj.add(item2);
+		Map<String, Object> item3 = new HashMap<>();
+		item3.put("None", "Value3");
+		obj.add(item3);
+
+		// 调用函数
+		calculateClassification.mapArrayField(resultMap, "TestField", obj);
+
+		// 验证结果
+		Map<String, Integer> result = resultMap.get("TestField");
+		assertEquals(1, result.get("Value1").intValue());
+		assertEquals(1, result.get("Value2").intValue());
+		assertEquals(0, result.get("None").intValue());
+
+	}
+
+	@Test
+	public void testMapSingleField() {
+		Map<String, Integer> map = new HashMap<>();
+		map.put("Field1", 0);
+		map.put("Field2", 0);
+		map.put("None", 1);
+		Map<String, Map<String, Integer>> resultMap = new HashMap<>();
+		resultMap.put("TestField", map);
+
+		// 创建虚拟的List对象
+		Map<String, Object> obj = new HashMap<>();
+		obj.put("displayName", "Value1");
+
+		String displayName = CalculateClassification.pickDisplayNameFromObj(obj);
+		Integer count = map.get(displayName);
+
+		// 验证结果
+		Map<String, Integer> result = resultMap.get("TestField");
+		assertEquals(1, result.get("None").intValue());
+	}
+
+	@Test
+	public void testUpdateMap() {
+		// 准备测试数据
+		Map<String, Integer> map = new HashMap<>();
+		map.put("value1", 1);
+		map.put("value2", 2);
+		map.put("value3", 3);
+		map.put("NONE", 4);
+
+		Object obj = new Object() {
+			public String toString() {
+				return "value1";
+			}
+		};
+
+		// 调用方法
+		String displayName = CalculateClassification.pickDisplayNameFromObj(obj);
+		Integer count = map.get(displayName);
+		map.put(displayName, count != null ? count + 1 : 1);
+		map.put("NONE", map.get("NONE") - 1);
+
+		// 验证displayName是否被正确获取
+		assertEquals("value1", displayName);
+
+		// 验证count是否被正确获取
+		assertEquals(1, (int) count);
+
+		// 验证map是否被正确更新
+		assertEquals(2, (int) map.get("value1"));
+		assertEquals(3, (int) map.get("NONE"));
+
+	}
+
+	@Test
+	public void testNoneProgress() {
+		// 准备测试数据
+		Map<String, Integer> map = new HashMap<>();
+		map.put("value1", 1);
+		map.put("value2", 2);
+		map.put("value3", 3);
+		map.put("NONE", 0);
+
+		// 调用方法
+		if (map.get("NONE") == 0) {
+			map.remove("NONE");
+		}
+
+		// 验证map是否被正确更新
+		assertNull(map.get("NONE"));
+		assertNotNull(map.get("value1"));
+		assertNotNull(map.get("value2"));
+		assertNotNull(map.get("value3"));
+	}
+
+	@Test
+	public void testMapArrayFieldFunctionCalled() {
+		CalculateClassification mockClassification = mock(CalculateClassification.class);
+		Map<String, Integer> map = new HashMap<>();
+		map.put("Field1", 0);
+		map.put("Field2", 0);
+		map.put("None", 1);
+		Map<String, Map<String, Integer>> resultMap = new HashMap<>();
+		resultMap.put("TestField", map);
+
+		List<Map<String, Object>> obj = new ArrayList<>();
+		Map<String, Object> item1 = new HashMap<>();
+		item1.put("displayName", "Value1");
+		obj.add(item1);
+		Map<String, Object> item2 = new HashMap<>();
+		item2.put("displayName", "Value2");
+		obj.add(item2);
+		Map<String, Object> item3 = new HashMap<>();
+		item3.put("None", "Value3");
+		obj.add(item3);
+		mockClassification.mapArrayField(resultMap, "TestField", obj);
+
+		verify(mockClassification, times(1)).mapArrayField(resultMap, "TestField", obj);
+
+	}
+
+	@Test
+	public void testMapArrayFieldFunction() {
+}
+
+	@Test
+	public void testPickDisplayNameFromObj() {
+		// Create a mock map object
+		Map<String, Object> mockMap = new HashMap<>();
+		mockMap.put("displayName", "Test Display Name");
+
+		// Test when the input is a Map object with "displayName" key
+		String result = calculateClassification.pickDisplayNameFromObj(mockMap);
+		assertEquals("Test Display Name", result);
+
+		// Test when the input is a Map object with "name" key
+		mockMap.clear();
+		mockMap.put("name", "Test Name");
+		result = calculateClassification.pickDisplayNameFromObj(mockMap);
+		assertEquals("Test Name", result);
+
+		// Test when the input is a Map object with "key" key
+		mockMap.clear();
+		mockMap.put("key", "Test Key");
+		result = calculateClassification.pickDisplayNameFromObj(mockMap);
+		assertEquals("Test Key", result);
+
+		// Test when the input is a Map object with "value" key
+		mockMap.clear();
+		mockMap.put("value", "Test Value");
+		result = calculateClassification.pickDisplayNameFromObj(mockMap);
+		assertEquals("Test Value", result);
+
+		// Test when the input is a String with "name" pattern
+		String mockString = "id=123,name=Test Name,value=456";
+		result = calculateClassification.pickDisplayNameFromObj(mockString);
+		assertEquals("Test Name", result);
+
+		// Test when the input is a null object
+		result = calculateClassification.pickDisplayNameFromObj(null);
+		assertEquals("None", result);
+	}
 
 }

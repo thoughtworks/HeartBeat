8a31c5ba780cce3c3fa21a7b8d3553fb134e6c44
Eason-T <awawerwer2007@163.com>
2023-05-12 11:05:58 +0800

    ADM-434 fix performance issue for Holiday api

--- backend/src/main/java/heartbeat/service/report/WorkDay.java
+++ backend/src/main/java/heartbeat/service/report/WorkDay.java
@@ -25,23 +25,40 @@ public class WorkDay {
 
 	private final HolidayFeignClient holidayFeignClient;
 
+	private Map<String, Boolean> holidayMap = new HashMap<>();
+
 	public Map<String, Boolean> loadHolidayList(String year) {
 
 		log.info("Start to get chinese holiday by year: {}", year);
 		List<HolidayDTO> tempHolidayList = holidayFeignClient.getHolidays(year).getDays();
 		log.info("Successfully get holiday list:{}", tempHolidayList);
 
-		Map<String, Boolean> holidayMap = new HashMap<>();
 		for (HolidayDTO tempHoliday : tempHolidayList) {
 			holidayMap.put(tempHoliday.getDate(), tempHoliday.getIsOffDay());
 		}
 		return holidayMap;
 	}
 
+	private void checkHolidayList(String year) {
+		if (holidayMap.size() == 0) {
+			loadHolidayList(year);
+		}
+		else if (!isContainYear(year)) {
+			loadHolidayList(year);
+		}
+	}
+
+	private boolean isContainYear(String year) {
+		boolean hasYear = false;
+		for (String dateString : holidayMap.keySet()) {
+			hasYear = dateString.startsWith(year);
+		}
+		return hasYear;
+	}
+
 	public boolean verifyIfThisDayHoliday(long time) {
 		String dateString = convertTimeToDateString(time);
-		int year = LocalDate.parse(dateString, DATE_FORMATTER).getYear();
-		Map<String, Boolean> holidayMap = loadHolidayList(String.valueOf(year));
+		checkHolidayList(String.valueOf(LocalDate.parse(dateString, DATE_FORMATTER).getYear()));
 		if (holidayMap.containsKey(dateString)) {
 			return holidayMap.get(dateString);
 		}

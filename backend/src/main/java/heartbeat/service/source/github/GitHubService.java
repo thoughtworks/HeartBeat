4b018a6e8cb2152868f3fd8e314cc9a0ee2f75c9
ting.li <ting.li@thoughtworks.com>
2023-05-25 14:23:49 +0800

    ADM-373: rebase main

--- backend/src/main/java/heartbeat/service/source/github/GitHubService.java
+++ backend/src/main/java/heartbeat/service/source/github/GitHubService.java
@@ -19,6 +19,7 @@ import jakarta.annotation.PreDestroy;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.stereotype.Service;
 
@@ -38,7 +39,6 @@ import java.util.stream.Collectors;
 @Log4j2
 public class GitHubService {
 
-	@Autowired
 	@Qualifier("taskExecutor")
 	private final ThreadPoolTaskExecutor taskExecutor;
 

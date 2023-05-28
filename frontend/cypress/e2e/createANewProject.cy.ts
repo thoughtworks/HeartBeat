46d53c252fa4531cb38fc8013cf8f1a56dbd885a
Eason-T <awawerwer2007@163.com>
2023-05-28 17:08:19 +0800

    refactor report e2e test use POM

--- frontend/cypress/e2e/createANewProject.cy.ts
+++ frontend/cypress/e2e/createANewProject.cy.ts
@@ -2,7 +2,31 @@ import homePage from '../pages/home'
 import configPage from '../pages/metrics/config'
 import metricsPage from '../pages/metrics/metrics'
 import reportPage from '../pages/metrics/report'
-
+const cycleTimeData = [
+  { label: 'Name', value: 'Value' },
+  { label: 'Average cycle time', value: '6.71(days/SP)' },
+  { label: '8.39(days/card)' },
+  { label: 'Total development time / Total cycle time', value: '0.64' },
+  { label: 'Total waiting for testing time / Total cycle time', value: '0.02' },
+  { label: 'Total block time / Total cycle time', value: '0.28' },
+  { label: 'Total review time / Total cycle time', value: '0.04' },
+  { label: 'Total testing time / Total cycle time', value: '0.01' },
+  { label: 'Average development time', value: '4.32(days/SP)' },
+  { label: '5.4(days/card)' },
+  { label: 'Average waiting for testing time', value: '0.16(days/SP)' },
+  { label: '0.2(days/card)' },
+  { label: 'Average block time', value: '1.88(days/SP)' },
+  { label: '2.35(days/card)' },
+  { label: 'Average review time', value: '0.26(days/SP)' },
+  { label: '0.32(days/card)' },
+  { label: 'Average testing time', value: '0.1(days/SP)' },
+  { label: '0.12(days/card)' },
+]
+const velocityData = [
+  { label: 'Name', value: 'Value' },
+  { label: 'Velocity(Story Point)', value: '20' },
+  { label: 'Throughput(Cards Count)', value: '16' },
+]
 describe('Create a new project', () => {
   it('Should create a new project manually', () => {
     homePage.navigate()
@@ -61,6 +85,8 @@ describe('Create a new project', () => {
 
     metricsPage.goReportStep()
 
-    reportPage.checkVelocity()
+    reportPage.checkVelocity('[data-test-id="Velocity"]', velocityData)
+
+    reportPage.checkCycleTime('[data-test-id="Cycle time"]', cycleTimeData)
   })
 })
--- frontend/cypress/pages/metrics/report.ts
+++ frontend/cypress/pages/metrics/report.ts
@@ -1,58 +1,36 @@
+interface BoardDataItem {
+  label: string
+  value?: string
+}
+
 class Report {
-  checkVelocity() {
+  private readonly velocityTitle = () => {
     cy.contains('Velocity').should('exist')
+  }
+  private readonly cycleTimeTitle = () => {
     cy.contains('Cycle time').should('exist')
-    cy.get('[data-test-id="Velocity"]')
-      .find('tr')
-      .first()
-      .within(() => {
-        cy.contains('Name').should('exist')
-        cy.contains('Value').should('exist')
-      })
-    cy.get('[data-test-id="Velocity"]')
-      .find('tr')
-      .eq(1)
-      .within(() => {
-        cy.contains('Velocity(Story Point)').should('exist')
-        cy.contains('20').should('exist')
-      })
-    cy.get('[data-test-id="Velocity"]')
-      .find('tr')
-      .eq(2)
-      .within(() => {
-        cy.contains('Throughput(Cards Count)').should('exist')
-        cy.contains('16').should('exist')
-      })
-    const cycleTimeData = [
-      { label: 'Name', value: 'Value' },
-      { label: 'Average cycle time', value: '6.71(days/SP)' },
-      { label: '8.39(days/card)' },
-      { label: 'Total development time / Total cycle time', value: '0.64' },
-      { label: 'Total waiting for testing time / Total cycle time', value: '0.02' },
-      { label: 'Total block time / Total cycle time', value: '0.28' },
-      { label: 'Total review time / Total cycle time', value: '0.04' },
-      { label: 'Total testing time / Total cycle time', value: '0.01' },
-      { label: 'Average development time', value: '4.32(days/SP)' },
-      { label: '5.4(days/card)' },
-      { label: 'Average waiting for testing time', value: '0.16(days/SP)' },
-      { label: '0.2(days/card)' },
-      { label: 'Average block time', value: '1.88(days/SP)' },
-      { label: '2.35(days/card)' },
-      { label: 'Average review time', value: '0.26(days/SP)' },
-      { label: '0.32(days/card)' },
-      { label: 'Average testing time', value: '0.1(days/SP)' },
-      { label: '0.12(days/card)' },
-    ]
-    cy.get('[data-test-id="Cycle time"]')
+  }
+  private readonly checkBoardCalculation = (testId: string, boardData: BoardDataItem[]) => {
+    cy.get(testId)
       .find('tr')
       .each((row, index) => {
         cy.wrap(row).within(() => {
-          cy.contains(cycleTimeData[index].label).should('exist')
-          if (cycleTimeData[index].value) {
-            cy.contains(cycleTimeData[index].value).should('exist')
+          cy.contains(boardData[index].label).should('exist')
+          if (boardData[index].value) {
+            cy.contains(boardData[index].value).should('exist')
           }
         })
       })
+  }
+
+  checkVelocity(testId: string, velocityData: BoardDataItem[]) {
+    this.velocityTitle()
+    this.checkBoardCalculation(testId, velocityData)
+  }
+
+  checkCycleTime(testId: string, cycleTimeData: BoardDataItem[]) {
+    this.cycleTimeTitle()
+    this.checkBoardCalculation(testId, cycleTimeData)
   }
 
   BackToMetricsStep() {

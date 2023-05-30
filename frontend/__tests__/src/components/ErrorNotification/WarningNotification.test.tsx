3179530d569882cfb8d568801bc89699ab4a70c8
ting.li <ting.li@thoughtworks.com>
2023-05-30 17:42:40 +0800

    ADM-373 test: rename

--- frontend/__tests__/src/components/ErrorNotification/WarningNotification.test.tsx
+++ frontend/__tests__/src/components/ErrorNotification/WarningNotification.test.tsx
@@ -1,8 +1,9 @@
 import { act, render, waitFor } from '@testing-library/react'
-import { WarningNotification } from 'src/components/Common/WarningNotification'
 import { setupStore } from '../../utils/setupStoreUtil'
 import { Provider } from 'react-redux'
 import { ERROR_MESSAGE_TIME_DURATION } from '@src/constants'
+import React from 'react'
+import { WarningNotification } from '@src/components/Common/WarningNotification'
 
 let store = null
 jest.useFakeTimers()

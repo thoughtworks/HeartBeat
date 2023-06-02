59cf343490668b0a5e69c9569d96bf3dbaaedc28
TingLiMio <112382375+TingLiMio@users.noreply.github.com>
2023-06-02 16:15:52 +0800

    ADM-373.4: [frontend] [backend] use absolute path to generate csv (#479)

--- backend/src/main/java/heartbeat/service/report/CSVFileGenerator.java
+++ backend/src/main/java/heartbeat/service/report/CSVFileGenerator.java
@@ -46,8 +46,9 @@ public class CSVFileGenerator {
 		}
 
 		String fileName = CSVFileNameEnum.PIPELINE.getValue() + "-" + csvTimeStamp + ".csv";
+		String absolutePath = new File(fileName).getAbsolutePath();
 
-		try (CSVWriter csvWriter = new CSVWriter(new FileWriter(fileName))) {
+		try (CSVWriter csvWriter = new CSVWriter(new FileWriter(absolutePath))) {
 			String[] headers = { "Pipeline Name", "Pipeline Step", "Build Number", "Committer",
 					"First Code Committed Time In PR", "Code Committed Time", "PR Created Time", "PR Merged Time",
 					"Deployment Completed Time", "Total Lead Time (HH:mm:ss)",

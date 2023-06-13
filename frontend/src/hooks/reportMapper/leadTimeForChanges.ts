874c95b04ae03fddb4d285f36bb102974488d585
Lanxin Wang <lanxin.wang@thoughtworks.com>
2023-06-13 16:49:07 +0800

    ADM-502: fix bug about ui display

--- frontend/src/hooks/reportMapper/leadTimeForChanges.ts
+++ frontend/src/hooks/reportMapper/leadTimeForChanges.ts
@@ -1,43 +1,63 @@
 import { LeadTimeForChangesResponse } from '@src/clients/report/dto/response'
-import { ReportDataWithThreeColumns } from '@src/hooks/reportMapper/reportUIDataStructure'
 
 export const leadTimeForChangesMapper = ({
   leadTimeForChangesOfPipelines,
   avgLeadTimeForChanges,
 }: LeadTimeForChangesResponse) => {
-  const mappedLeadTimeForChangesValue: ReportDataWithThreeColumns[] = []
-
   const formatDuration = (duration: number) => {
     const minutesPerDay = 1440
     const minutesPerHour = 60
     const days = Math.floor(duration / minutesPerDay)
     const hours = Math.floor((duration % minutesPerDay) / minutesPerHour)
     const minutes = Math.floor(duration % minutesPerHour)
-    let result = ''
-    if (days > 0) {
-      result += days + 'day '
-    }
-    if (hours > 0) {
-      result += hours + 'hours '
-    }
-    if (minutes > 0) {
-      result += minutes + 'minutes'
-    }
-    return result.trim()
+    return (
+      (days ? days + 'day ' : '') +
+      (hours ? hours + 'hours ' : '') +
+      (0 < minutes && minutes <= 1 ? '1minutes' : minutes + 'minutes').trim()
+    )
   }
-  leadTimeForChangesOfPipelines.map((item, index) => {
-    const deploymentFrequencyValue: ReportDataWithThreeColumns = {
+
+  const calculateTotalTime = (mergeDelayTime: number, pipelineDelayTime: number): string => {
+    const formatMergeDelayTime = formatDuration(mergeDelayTime)
+    const formatPipelineDelayTime = formatDuration(pipelineDelayTime)
+
+    const [mergeDelayDays, mergeDelayHours, mergeDelayMinutes] = formatMergeDelayTime.match(/\d+/g) || ['0', '0', '0']
+    const [pipelineDelayDays, pipelineDelayHours, pipelineDelayMinutes] = formatPipelineDelayTime.match(/\d+/g) || [
+      '0',
+      '0',
+      '0',
+    ]
+
+    let totalDays: number = parseInt(mergeDelayDays, 10) + parseInt(pipelineDelayDays, 10)
+    let totalHours: number = parseInt(mergeDelayHours, 10) + parseInt(pipelineDelayHours, 10)
+    let totalMinutes: number = parseInt(mergeDelayMinutes, 10) + parseInt(pipelineDelayMinutes, 10)
+
+    if (totalMinutes >= 60) {
+      totalHours += Math.floor(totalMinutes / 60)
+      totalMinutes %= 60
+    }
+
+    if (totalHours >= 24) {
+      totalDays += Math.floor(totalHours / 24)
+      totalHours %= 24
+    }
+
+    return `${totalDays}day ${totalHours}hours ${totalMinutes}minutes`
+  }
+
+  const mappedLeadTimeForChangesValue = leadTimeForChangesOfPipelines.map((item, index) => {
+    return {
       id: index,
       name: `${item.name}/${item.step}`,
       valuesList: Object.entries(item)
         .slice(-3)
-        .map(([name, value]) => ({
+        .map(([name, value], index) => ({
           name: name,
-          value: formatDuration(value),
+          value: index == 2 ? calculateTotalTime(item.mergeDelayTime, item.pipelineDelayTime) : formatDuration(value),
         })),
     }
-    mappedLeadTimeForChangesValue.push(deploymentFrequencyValue)
   })
+
   mappedLeadTimeForChangesValue.push({
     id: mappedLeadTimeForChangesValue.length,
     name: avgLeadTimeForChanges.name,

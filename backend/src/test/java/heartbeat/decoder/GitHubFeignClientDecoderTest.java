95025ba51eafeb9ff6356225edebd3e9092f0eb1
Guoqing Li <guoqing.li@thoughtworks.com>
2023-07-05 16:58:36 +0800

    ADM-471 fix: GitHubFeignClientDecoderTest.

--- backend/src/test/java/heartbeat/decoder/GitHubFeignClientDecoderTest.java
+++ backend/src/test/java/heartbeat/decoder/GitHubFeignClientDecoderTest.java
@@ -5,8 +5,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import feign.Response;
 import heartbeat.exception.HBTimeoutException;
-import heartbeat.exception.NotFoundException;
-import heartbeat.exception.PermissionDenyException;
+import heartbeat.exception.RateLimitExceededException;
 import heartbeat.exception.RequestFailedException;
 import heartbeat.exception.UnauthorizedException;
 import org.junit.jupiter.api.BeforeEach;
@@ -16,14 +15,14 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.HttpStatus;
 
 @ExtendWith(MockitoExtension.class)
-class BuildKiteFeignClientDecoderTest {
+class GitHubFeignClientDecoderTest {
 
 	private final ResponseMockUtil responseMock = new ResponseMockUtil();
-	private BuildKiteFeignClientDecoder decoder;
+	private GitHubFeignClientDecoder decoder;
 
 	@BeforeEach
 	void setup() {
-		decoder = new BuildKiteFeignClientDecoder();
+		decoder = new GitHubFeignClientDecoder();
 	}
 
 	@Test
@@ -37,35 +36,8 @@ class BuildKiteFeignClientDecoderTest {
 	}
 
 	@Test
-	void testDecode_NotFoundException() {
-		int statusCode = HttpStatus.NOT_FOUND.value();
-
-		Exception exception = decoder.decode("methodKey", responseMock.getMockResponse(statusCode));
-
-		assertEquals(NotFoundException.class, exception.getClass());
-	}
-
-	@Test
-	void testDecode_HBTimeoutException() {
-		int statusCode = HttpStatus.SERVICE_UNAVAILABLE.value();
-
-		Exception exception = decoder.decode("methodKey", responseMock.getMockResponse(statusCode));
-
-		assertEquals(HBTimeoutException.class, exception.getClass());
-	}
-
-	@Test
-	void testDecode_PermissionDenyException() {
-		int statusCode = HttpStatus.FORBIDDEN.value();
-
-		Exception exception = decoder.decode("methodKey", responseMock.getMockResponse(statusCode));
-
-		assertEquals(PermissionDenyException.class, exception.getClass());
-	}
-
-	@Test
 	void testDecode_4xxRequestFailedException() {
-		int statusCode = HttpStatus.METHOD_NOT_ALLOWED.value();
+		int statusCode = HttpStatus.NOT_FOUND.value();
 
 		Exception exception = decoder.decode("methodKey", responseMock.getMockResponse(statusCode));
 
@@ -91,6 +63,24 @@ class BuildKiteFeignClientDecoderTest {
 
 		assertEquals(RequestFailedException.class, exception.getClass());
 		assertTrue(exception.getMessage().contains("UnKnown Error"));
+	}
+
+	@Test
+	void testDecode_HBTimeoutException() {
+		int statusCode = HttpStatus.SERVICE_UNAVAILABLE.value();
+
+		Exception exception = decoder.decode("methodKey", responseMock.getMockResponse(statusCode));
+
+		assertEquals(HBTimeoutException.class, exception.getClass());
+	}
+
+	@Test
+	void testDecode_PermissionDenyException() {
+		int statusCode = HttpStatus.FORBIDDEN.value();
+
+		Exception exception = decoder.decode("methodKey", responseMock.getMockResponse(statusCode));
+
+		assertEquals(RateLimitExceededException.class, exception.getClass());
 	}
 
 }
--- backend/src/test/java/heartbeat/decoder/JiraFeignClientDecoderTest.java
+++ backend/src/test/java/heartbeat/decoder/JiraFeignClientDecoderTest.java
@@ -4,9 +4,11 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import feign.Response;
+import heartbeat.exception.BadRequestException;
 import heartbeat.exception.HBTimeoutException;
+import heartbeat.exception.NoContentException;
 import heartbeat.exception.NotFoundException;
-import heartbeat.exception.PermissionDenyException;
+import heartbeat.exception.RateLimitExceededException;
 import heartbeat.exception.RequestFailedException;
 import heartbeat.exception.UnauthorizedException;
 import org.junit.jupiter.api.BeforeEach;
@@ -16,14 +18,14 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.HttpStatus;
 
 @ExtendWith(MockitoExtension.class)
-class BuildKiteFeignClientDecoderTest {
+class JiraFeignClientDecoderTest {
 
 	private final ResponseMockUtil responseMock = new ResponseMockUtil();
-	private BuildKiteFeignClientDecoder decoder;
+	private JiraFeignClientDecoder decoder;
 
 	@BeforeEach
 	void setup() {
-		decoder = new BuildKiteFeignClientDecoder();
+		decoder = new JiraFeignClientDecoder();
 	}
 
 	@Test
@@ -37,30 +39,21 @@ class BuildKiteFeignClientDecoderTest {
 	}
 
 	@Test
+	void testDecode_NoContentException() {
+		int statusCode = HttpStatus.NO_CONTENT.value();
+
+		Exception exception = decoder.decode("methodKey", responseMock.getMockResponse(statusCode));
+
+		assertEquals(NoContentException.class, exception.getClass());
+	}
+
+	@Test
 	void testDecode_NotFoundException() {
 		int statusCode = HttpStatus.NOT_FOUND.value();
 
 		Exception exception = decoder.decode("methodKey", responseMock.getMockResponse(statusCode));
 
 		assertEquals(NotFoundException.class, exception.getClass());
-	}
-
-	@Test
-	void testDecode_HBTimeoutException() {
-		int statusCode = HttpStatus.SERVICE_UNAVAILABLE.value();
-
-		Exception exception = decoder.decode("methodKey", responseMock.getMockResponse(statusCode));
-
-		assertEquals(HBTimeoutException.class, exception.getClass());
-	}
-
-	@Test
-	void testDecode_PermissionDenyException() {
-		int statusCode = HttpStatus.FORBIDDEN.value();
-
-		Exception exception = decoder.decode("methodKey", responseMock.getMockResponse(statusCode));
-
-		assertEquals(PermissionDenyException.class, exception.getClass());
 	}
 
 	@Test
@@ -93,4 +86,12 @@ class BuildKiteFeignClientDecoderTest {
 		assertTrue(exception.getMessage().contains("UnKnown Error"));
 	}
 
+	@Test
+	void testDecode_HBTimeoutException() {
+		int statusCode = HttpStatus.SERVICE_UNAVAILABLE.value();
+
+		Exception exception = decoder.decode("methodKey", responseMock.getMockResponse(statusCode));
+
+		assertEquals(HBTimeoutException.class, exception.getClass());
+	}
 }

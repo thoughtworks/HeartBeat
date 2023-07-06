1de2bfe284d60315b1280d24533c763baa546100
shuaiwang <shuai.wang@thoughtworks.com>
2023-07-06 14:32:19 +0800

    ADM-503 add text for emoji support

--- frontend/__tests__/src/components/Metrics/MetricsStepper/MetricsStepper.test.tsx
+++ frontend/__tests__/src/components/Metrics/MetricsStepper/MetricsStepper.test.tsx
@@ -1,5 +1,5 @@
 import { act, fireEvent, render, screen } from '@testing-library/react'
-import { exportToJsonFile } from '@src/utils/util'
+import { BuildKiteEmoji, exportToJsonFile } from '@src/utils/util'
 import MetricsStepper from '@src/components/Metrics/MetricsStepper'
 import { Provider } from 'react-redux'
 import { setupStore } from '../../../utils/setupStoreUtil'
@@ -70,6 +70,10 @@ jest.mock('@src/context/config/configSlice', () => ({
 jest.mock('@src/utils/util', () => ({
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   exportToJsonFile: jest.fn((_filename: string, _json: object) => {
+    //Mock for test
+  }),
+  getEmojiUrls: jest.fn().mockReturnValue(['https://buildkiteassets.com/emojis/img-buildkite-64/charger64.png']),
+  removeExtraEmojiName: jest.fn((_input: string) => {
     //Mock for test
   }),
 }))

fc86a42f0b828a71be0038ea320d475072b28788
ting.li <ting.li@thoughtworks.com>
2023-07-11 10:48:45 +0800

    Revert "ADM-503 multi emoji support for report page"
    This reverts commit 8f52eec4bb8fc45f5b3ac9e0039b562d3897fd49.

--- frontend/src/components/Common/ReportForThreeColumns/style.tsx
+++
@@ -1,8 +0,0 @@
-import { styled } from '@mui/material/styles'
-import { Avatar } from '@mui/material'
-
-export const StyledAvatar = styled(Avatar)({
-  width: '20px',
-  height: '20px',
-  marginRight: '4px',
-})

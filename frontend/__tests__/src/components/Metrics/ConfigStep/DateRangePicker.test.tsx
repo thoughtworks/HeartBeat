2ca358c3847f55c954b677cba522740ac163dd69
YG1022 <958065532@qq.com>
2023-08-09 19:24:11 +0800

    ADM-551:[frontend][fix] justify the time interval automatically generated on the config page and its related tests

--- frontend/__tests__/src/components/Metrics/ConfigStep/DateRangePicker.test.tsx
+++ frontend/__tests__/src/components/Metrics/ConfigStep/DateRangePicker.test.tsx
@@ -53,7 +53,7 @@ describe('DateRangePicker', () => {
 
   it('should Auto-fill endDate which is after startDate 13 days when fill right startDate ', () => {
     const { getByRole } = setup()
-    const endDate = TODAY.add(14, 'day')
+    const endDate = TODAY.add(13, 'day')
     const startDateInput = getByRole('textbox', { name: START_DATE_LABEL }) as HTMLInputElement
     const endDateInput = getByRole('textbox', { name: END_DATE_LABEL }) as HTMLInputElement
 

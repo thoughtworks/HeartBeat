3c76940a5ec835bc2a4fc8b97eb2bea9728ca0a7
ZhiqiaoYang <126433704+ZhiqiaoYang@users.noreply.github.com>
2023-08-09 09:28:55 +0800

    Revert "ADM-550: [Fox][frontend] - Fix Real done reset logic" (#577)

--- frontend/src/components/Metrics/MetricsStep/CycleTime/index.tsx
+++ frontend/src/components/Metrics/MetricsStep/CycleTime/index.tsx
@@ -11,7 +11,6 @@ import {
 } from '@src/context/Metrics/metricsSlice'
 import { useAppSelector } from '@src/hooks'
 import { WarningNotification } from '@src/components/Common/WarningNotification'
-import { DONE } from '@src/constants'
 
 interface cycleTimeProps {
   title: string
@@ -22,7 +21,6 @@ export const CycleTime = ({ title }: cycleTimeProps) => {
   const { cycleTimeSettings } = useAppSelector(selectMetricsContent)
   const warningMessage = useAppSelector(selectCycleTimeWarningMessage)
   const [cycleTimeOptions, setCycleTimeOptions] = useState(cycleTimeSettings)
-  const [saveDone, setSaveDone] = useState<string[]>([])
 
   const saveCycleTimeOptions = (name: string, value: string) => {
     setCycleTimeOptions(
@@ -35,13 +33,7 @@ export const CycleTime = ({ title }: cycleTimeProps) => {
           : item
       )
     )
-    if (value === DONE) {
-      setSaveDone([...saveDone, name])
-      dispatch(saveDoneColumn([]))
-    } else if (saveDone.includes(name)) {
-      setSaveDone(saveDone.filter((e) => e !== name))
-      dispatch(saveDoneColumn([]))
-    }
+    dispatch(saveDoneColumn([]))
   }
 
   useEffect(() => {

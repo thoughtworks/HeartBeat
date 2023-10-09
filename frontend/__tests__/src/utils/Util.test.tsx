b70453eadef67079a5b060ea0ab9f92969e4de6b
LoisHuang2022 <Huang_Wenting@163.com>
2023-10-09 18:10:46 +0800

    ADM-552:[frontend]fix: fix the test of function

--- frontend/__tests__/src/utils/Util.test.tsx
+++ frontend/__tests__/src/utils/Util.test.tsx
@@ -1,4 +1,4 @@
-import { exportToJsonFile, transformToCleanedBuildKiteEmoji, sortArrayWithoutEmoji } from '@src/utils/util'
+import { exportToJsonFile, transformToCleanedBuildKiteEmoji } from '@src/utils/util'
 import { CleanedBuildKiteEmoji, OriginBuildKiteEmoji } from '@src/emojis/emoji'
 
 describe('exportToJsonFile function', () => {
@@ -29,27 +29,5 @@ describe('transformToCleanedBuildKiteEmoji function', () => {
     const [result] = transformToCleanedBuildKiteEmoji([mockOriginEmoji])
 
     expect(result).toEqual(expectedCleanedEmoji)
-  })
-})
-
-describe('sortArrayWithoutEmoji function', () => {
-  it('should sort array without emoji', () => {
-    const mockOriginArray: string[] = [':cloudformation: Deploy infra', ':java: Build Backend', ':lock: Check Security']
-
-    const expectedArray: string[] = [':java: Build Backend', ':lock: Check Security', ':cloudformation: Deploy infra']
-
-    const result = sortArrayWithoutEmoji(mockOriginArray)
-
-    expect(result).toEqual(expectedArray)
-  })
-
-  it('should return a empty array if array is empty', () => {
-    const mockOriginArray: string[] = []
-
-    const expectedArray: string[] = []
-
-    const result = sortArrayWithoutEmoji(mockOriginArray)
-
-    expect(result).toEqual(expectedArray)
   })
 })

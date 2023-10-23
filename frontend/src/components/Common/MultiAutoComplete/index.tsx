6748654f3a436a6da613e06e32a6f33e424dfab1
7758521yyy <tinghaoyuan@gmail.com>
2023-10-23 16:59:39 +0800

    Create autocomplete component with filter

---
+++ frontend/src/components/Common/MultiAutoComplete/index.tsx
@@ -0,0 +1,54 @@
+import { Checkbox, createFilterOptions, FormHelperText, TextField } from '@mui/material'
+import React from 'react'
+import { StyledAutocompleted } from './styles'
+import { DEFAULT_HELPER_TEXT } from '@src/constants'
+type Props = {
+  optionList: string[]
+  selectedOption: string[]
+  onChangeHandler: (event: React.SyntheticEvent, value: string[], reason?: string, details?: string) => void
+  isSelectAll: boolean
+  textFieldLabel: string
+  isError: boolean
+}
+const MultiAutoComplete = ({
+  optionList,
+  selectedOption,
+  onChangeHandler,
+  isSelectAll,
+  textFieldLabel,
+  isError,
+}: Props) => {
+  const filter = createFilterOptions()
+
+  return (
+    <StyledAutocompleted
+      multiple
+      options={optionList}
+      disableCloseOnSelect
+      value={selectedOption}
+      filterOptions={(options, params) => {
+        const filtered = filter(options, params)
+        return ['All', ...filtered]
+      }}
+      getOptionLabel={(option: string) => option}
+      onChange={onChangeHandler}
+      renderOption={(props, option, { selected }) => {
+        const selectAllProps =
+          option === 'All' // To control the state of 'select-all' checkbox
+            ? { checked: isSelectAll }
+            : {}
+        return (
+          <li {...props}>
+            <Checkbox style={{ marginRight: 8 }} checked={selected} {...selectAllProps} />
+            {option}
+          </li>
+        )
+      }}
+      renderInput={(params) => (
+        <TextField {...params} required error={isError} variant='standard' label={textFieldLabel} />
+      )}
+    />
+  )
+}
+
+export default MultiAutoComplete
---
+++ frontend/src/components/Common/MultiAutoComplete/styles.ts
@@ -0,0 +1,9 @@
+import { styled } from '@mui/material/styles'
+import { Autocomplete } from '@mui/material'
+
+export const StyledAutocompleted = styled(Autocomplete)`
+  & .MuiAutocomplete-tag {
+    background-color: transparent;
+    border: 1px solid rgba(0, 0, 0, 0.26);
+  }
+`

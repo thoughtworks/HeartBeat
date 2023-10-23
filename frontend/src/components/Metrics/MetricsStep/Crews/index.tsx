b9e6d87c6b4f2730f19d402098cf19e1e56b5748
7758521yyy <tinghaoyuan@gmail.com>
2023-10-23 17:00:31 +0800

    feat: replace the select in crew

--- frontend/src/components/Metrics/MetricsStep/Crews/index.tsx
+++ frontend/src/components/Metrics/MetricsStep/Crews/index.tsx
@@ -1,19 +1,12 @@
-import {
-  Checkbox,
-  FormControl,
-  FormHelperText,
-  InputLabel,
-  ListItemText,
-  MenuItem,
-  Select,
-  SelectChangeEvent,
-} from '@mui/material'
-import { DEFAULT_HELPER_TEXT, SELECTED_VALUE_SEPARATOR } from '@src/constants'
+import { Checkbox, createFilterOptions, FormHelperText, TextField } from '@mui/material'
+import { DEFAULT_HELPER_TEXT } from '@src/constants'
 import React, { useEffect, useState } from 'react'
 import { MetricsSettingTitle } from '@src/components/Common/MetricsSettingTitle'
 import { useAppDispatch } from '@src/hooks/useAppDispatch'
 import { saveUsers, selectMetricsContent } from '@src/context/Metrics/metricsSlice'
 import { useAppSelector } from '@src/hooks'
+import { styled } from '@mui/material/styles'
+import MultiAutoComplete from '@src/components/Common/MultiAutoComplete'
 
 interface crewsProps {
   options: string[]
@@ -21,19 +14,26 @@ interface crewsProps {
   label: string
 }
 
+export const WarningMessage = styled('p')({
+  color: 'red',
+})
+
 export const Crews = ({ options, title, label }: crewsProps) => {
   const dispatch = useAppDispatch()
   const [isEmptyCrewData, setIsEmptyCrewData] = useState<boolean>(false)
   const { users } = useAppSelector(selectMetricsContent)
-  const [selectedCrews, setSelectedCrews] = useState(users)
+  const [selectedCrews, setSelectedCrews] = useState<string[]>(users)
   const isAllSelected = options.length > 0 && selectedCrews.length === options.length
 
   useEffect(() => {
     setIsEmptyCrewData(selectedCrews.length === 0)
   }, [selectedCrews])
 
-  const handleCrewChange = (event: SelectChangeEvent<string[]>) => {
-    const value = event.target.value
+  useEffect(() => {
+    dispatch(saveUsers(selectedCrews))
+  }, [selectedCrews, dispatch])
+
+  const handleCrewChange = (event, value) => {
     if (value[value.length - 1] === 'All') {
       setSelectedCrews(selectedCrews.length === options.length ? [] : options)
       return
@@ -41,44 +41,26 @@ export const Crews = ({ options, title, label }: crewsProps) => {
     setSelectedCrews([...value])
   }
 
-  useEffect(() => {
-    dispatch(saveUsers(selectedCrews))
-  }, [selectedCrews, dispatch])
-
   return (
     <>
       <MetricsSettingTitle title={title} />
-      <FormControl variant='standard' required error={isEmptyCrewData}>
-        <InputLabel id='crew-data-multiple-checkbox-label'>{label}</InputLabel>
-        <Select
-          labelId='crew-data-multiple-checkbox-label'
-          multiple
-          error={isEmptyCrewData}
-          value={selectedCrews}
-          onChange={handleCrewChange}
-          renderValue={(selectedCrews: string[]) => selectedCrews.join(SELECTED_VALUE_SEPARATOR)}
-        >
-          <MenuItem value='All'>
-            <Checkbox checked={isAllSelected} />
-            <ListItemText primary='All' />
-          </MenuItem>
-          {options.map((data) => (
-            <MenuItem key={data} value={data}>
-              <Checkbox checked={selectedCrews.includes(data)} />
-              <ListItemText primary={data} />
-            </MenuItem>
-          ))}
-        </Select>
-        <FormHelperText>
-          {isEmptyCrewData ? (
-            <>
-              {label} is <strong>required</strong>
-            </>
-          ) : (
-            DEFAULT_HELPER_TEXT
-          )}
-        </FormHelperText>
-      </FormControl>
+      <MultiAutoComplete
+        optionList={options}
+        isError={isEmptyCrewData}
+        isSelectAll={isAllSelected}
+        onChangeHandler={handleCrewChange}
+        selectedOption={selectedCrews}
+        textFieldLabel={label}
+      />
+      <FormHelperText>
+        {isEmptyCrewData ? (
+          <WarningMessage>
+            {label} is <strong>required</strong>
+          </WarningMessage>
+        ) : (
+          DEFAULT_HELPER_TEXT
+        )}
+      </FormHelperText>
     </>
   )
 }

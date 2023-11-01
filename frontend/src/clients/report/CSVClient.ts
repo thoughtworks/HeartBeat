343b20a9f480c105e226f7f5f15041a988bd01a1
7758521yyy <tinghaoyuan@gmail.com>
2023-11-01 14:49:14 +0800

    ADM-589:[frontend]feat: add the collection duration info in the exported csv file name

--- frontend/src/clients/report/CSVClient.ts
+++ frontend/src/clients/report/CSVClient.ts
@@ -4,14 +4,18 @@ import dayjs from 'dayjs'
 import { downloadCSV } from '@src/utils/util'
 
 export class CSVClient extends HttpClient {
-  parseTimeStampToHumanDate = (csvTimeStamp: number | undefined): string =>
-    dayjs(csvTimeStamp).format('YYYY-MM-DD-HH-mm-ss')
+  parseTimeStampToHumanDate = (csvTimeStamp: number | undefined): string => dayjs(csvTimeStamp).format('SSS')
+  parseCollectionDateToHumanDate = (date: string) => dayjs(date).format('YYYYMMDD')
 
   exportCSVData = async (params: CSVReportRequestDTO) => {
     await this.axiosInstance
       .get(`/reports/${params.dataType}/${params.csvTimeStamp}`, { responseType: 'blob' })
       .then((res) => {
-        const exportedFilename = `${params.dataType}-data-${this.parseTimeStampToHumanDate(params.csvTimeStamp)}.csv`
+        const exportedFilename = `${params.dataType}-${this.parseCollectionDateToHumanDate(
+          params.startDate
+        )}-to-${this.parseCollectionDateToHumanDate(params.endDate)}-${this.parseTimeStampToHumanDate(
+          params.csvTimeStamp
+        )}.csv`
         downloadCSV(exportedFilename, res.data)
       })
       .catch((e) => {

80ddf15a97c478ce3c158641ccadc358a951a27c
Tingyu Dong <tingyu.dong@thoughtworks.com>
2023-11-13 14:44:06 +0800

    ADM-588:[backend]feat: convert velocity and classification data to CSV

--- backend/src/main/java/heartbeat/service/report/CSVFileNameEnum.java
+++ backend/src/main/java/heartbeat/service/report/CSVFileNameEnum.java
@@ -2,7 +2,7 @@ package heartbeat.service.report;
 
 public enum CSVFileNameEnum {
 
-	BOARD("./csv/board"), PIPELINE("./csv/pipeline");
+	METRIC("./csv/metric"), BOARD("./csv/board"), PIPELINE("./csv/pipeline");
 
 	private final String value;
 

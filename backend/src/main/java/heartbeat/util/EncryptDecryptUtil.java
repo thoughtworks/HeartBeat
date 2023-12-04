6b00a101cd87a550efcf54bb83b1cb71043c0bf3
junbo.dai <junbo.dai@thoughtworks.com>
2023-12-04 14:37:19 +0800

    ADM-619:[backend]feat: can get data from system and encrypt by sha-64

--- backend/src/main/java/heartbeat/util/EncryptDecryptUtil.java
+++ backend/src/main/java/heartbeat/util/EncryptDecryptUtil.java
@@ -1,12 +1,45 @@
 package heartbeat.util;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Map;
+import java.util.Objects;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 @Component
+@RequiredArgsConstructor
 public class EncryptDecryptUtil {
 
+	public static final String SHA_256 = "SHA256";
+
+	private static final String BACKEND_SECRET_KEY = "BACKEND_SECRET_KEY";
+
+	private static final String FIXED_SALT = "FIXED_SALT";
+
+	public final SystemUtil systemUtil;
+
 	public String getSecretKey(String password) {
-		return "";
+		Map<String, String> envMap = systemUtil.getEnvMap();
+		if (Objects.isNull(envMap.get(BACKEND_SECRET_KEY)) || Objects.isNull(envMap.get(FIXED_SALT))) {
+			throw new RuntimeException();
+		}
+		String passwordWithSecretKeySalt = envMap.get(BACKEND_SECRET_KEY) + password
+				+ Objects.nonNull(envMap.get(FIXED_SALT));
+		try {
+			MessageDigest sha256 = MessageDigest.getInstance(SHA_256);
+			byte[] secretKeyByteList = sha256.digest(passwordWithSecretKeySalt.getBytes(StandardCharsets.UTF_8));
+			return IntStream.range(0, secretKeyByteList.length)
+				.mapToObj(i -> String.format("%02x", 0xff & secretKeyByteList[i]))
+				.collect(Collectors.joining());
+		}
+		catch (NoSuchAlgorithmException e) {
+			throw new RuntimeException(e);
+		}
 	}
 
 	public String getEncryptedData(String iv, String secretKey, String configData) {
--- backend/src/test/java/heartbeat/util/EncryptDecryptUtilTest.java
+++ backend/src/test/java/heartbeat/util/EncryptDecryptUtilTest.java
@@ -1,18 +1,27 @@
 package heartbeat.util;
 
 import org.apache.logging.log4j.util.Strings;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class EncryptDecryptUtilTest {
 
 	@InjectMocks
 	EncryptDecryptUtil encryptDecryptUtil;
+
+	@Mock
+	SystemUtil systemUtil;
 
 	@Test
 	void shouldToFix() {
@@ -24,6 +33,18 @@ class EncryptDecryptUtilTest {
 		assertEquals(Strings.EMPTY, secretKey);
 		assertEquals(Strings.EMPTY, encryptedData);
 		assertEquals(Strings.EMPTY, macBytes);
+	}
+
+	@Test
+	void shouldEncryptPasswordToSecretKeyBySha256() {
+		Map<String, String> envMap = new HashMap<>();
+		envMap.put("BACKEND_SECRET_KEY", "fakeSecretKey");
+		envMap.put("FIXED_SALT", "fakeFixedSalt");
+		when(systemUtil.getEnvMap()).thenReturn(envMap);
+		String secretKey1 = encryptDecryptUtil.getSecretKey("fakePassword");
+		String secretKey2 = encryptDecryptUtil.getSecretKey("Password");
+		assertEquals(64, secretKey1.length());
+		assertEquals(64, secretKey2.length());
 	}
 
 }

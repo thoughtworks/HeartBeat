84248066d0da2dfad43acc6bed50b607400ad2f7
weiran.sun <907221539@qq.com>
2023-12-05 18:44:40 +0800

    ADM-649:[backend]refactor: extract 400 exception status

--- backend/src/main/java/heartbeat/exception/GithubRepoEmptyException.java
+++ backend/src/main/java/heartbeat/exception/GithubRepoEmptyException.java
@@ -1,12 +1,13 @@
 package heartbeat.exception;
 
 import lombok.Getter;
+import org.springframework.http.HttpStatus;
 
 @Getter
 public class GithubRepoEmptyException extends BaseException {
 
 	public GithubRepoEmptyException(String message) {
-		super(message, 400);
+		super(message, HttpStatus.BAD_REQUEST.value());
 	}
 
 }

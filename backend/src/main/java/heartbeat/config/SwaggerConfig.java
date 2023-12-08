6792e8f5f24161b90407d457b98a3ff69065a885
junbo.dai <junbo.dai@thoughtworks.com>
2023-12-08 14:37:59 +0800

    ADM-620:[backend]fix: hard code new server url in SwaggerConfig

--- backend/src/main/java/heartbeat/config/SwaggerConfig.java
+++ backend/src/main/java/heartbeat/config/SwaggerConfig.java
@@ -7,8 +7,6 @@ import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.net.InetAddress;
-import java.net.UnknownHostException;
 import java.util.List;
 
 @Configuration
@@ -18,18 +16,7 @@ public class SwaggerConfig {
 	public OpenAPI customOpenAPI() {
 		return new OpenAPI().components(new Components())
 			.info(new Info().title("Backend API").version("1.0"))
-			.servers(List.of(new Server().url(getCurrentHost() + "/api/v1")));
-	}
-
-	private String getCurrentHost() {
-		try {
-			InetAddress localHost = InetAddress.getLocalHost();
-			String hostAddress = localHost.getHostAddress();
-			return "http://" + hostAddress + ":4321";
-		}
-		catch (UnknownHostException e) {
-			return "http://backend:4322";
-		}
+			.servers(List.of(new Server().url("http://13.214.14.43:4321/api/v1")));
 	}
 
 }

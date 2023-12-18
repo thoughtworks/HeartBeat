2b42a84837f42c41af6dde5a24b8c556233365ce
Simon Tal <simon_tal@hotmail.com>
2023-12-18 11:34:46 +0800

    ADM-573:[Frontend] refactor script

--- frontend/scripts/runE2eWithServer.ts
+++ frontend/scripts/runE2eWithServer.ts
@@ -3,14 +3,14 @@ import path from 'path'
 import process from 'process'
 
 const PORT = {
-  FE: 4321,
-  BE: 4322,
+  FRONT_END: 4321,
+  BACK_END: 4322,
   STUB: 4323,
 }
 
 const DIR = {
-  FE: path.resolve(__dirname, '../'),
-  BE: path.resolve(__dirname, '../../backend'),
+  FRONT_END: path.resolve(__dirname, '../'),
+  BACK_END: path.resolve(__dirname, '../../backend'),
   STUB: path.resolve(__dirname, '../../stubs'),
 }
 
@@ -20,11 +20,11 @@ const main = async (args: string[]) => {
   const checkPorts = async () => {
     const ports = Object.values(PORT)
     const checkPorts = ports.map((port) => tcpPortUsed.check(port))
-
-    const usingPorts = await Promise.all(checkPorts).then((result) => ports.filter((_, idx) => result[idx]))
+    const checkingResult = await Promise.all(checkPorts)
+    const usingPorts = ports.filter((_, idx) => checkingResult[idx])
     const availablePorts = ports.filter((port) => !usingPorts.includes(port))
 
-    console.log(`E2E: Currently available ports: ${availablePorts.join(',')}`)
+    console.log(`Successfully detect available ports: ${availablePorts.join(',')}`)
 
     return {
       availablePorts,
@@ -32,11 +32,11 @@ const main = async (args: string[]) => {
     }
   }
 
-  const startFE = () => $({ cwd: DIR.FE, stderr: 'inherit', shell: true })`pnpm run start`
+  const startFE = () => $({ cwd: DIR.FRONT_END, stderr: 'inherit', shell: true })`pnpm run start`
 
   const startBE = () =>
     $({
-      cwd: DIR.BE,
+      cwd: DIR.BACK_END,
       stderr: 'inherit',
       shell: true,
     })`./gradlew bootRun --args='--spring.profiles.active=local --MOCK_SERVER_URL=http://localhost:4323'`
@@ -48,6 +48,16 @@ const main = async (args: string[]) => {
       shell: true,
     })`docker-compose up`
 
+  const waitForPort = async (port: number) => {
+    try {
+      await tcpPortUsed.waitUntilUsed(port, 1000, 30000)
+      console.log(`Successfully run service on port ${port}`)
+    } catch (err) {
+      console.error(`Failed to run service on port ${port} for reason ${err}`)
+      process.exit(1)
+    }
+  }
+
   const startServices = async (availablePorts: number[]) => {
     if (availablePorts.length <= 0) {
       return []
@@ -55,29 +65,19 @@ const main = async (args: string[]) => {
 
     const processes = availablePorts.map((port) => {
       switch (port) {
-        case PORT.BE:
-          console.log(`E2E: Try to start BE service`)
+        case PORT.BACK_END:
+          console.log(`Start to run back-end service`)
           return startBE()
-        case PORT.FE:
-          console.log(`E2E: Try to start FE service`)
+        case PORT.FRONT_END:
+          console.log(`Start to run front-end service`)
           return startFE()
         case PORT.STUB:
-          console.log(`E2E: Try to start STUB service`)
+          console.log(`Start to run stub service`)
           return startSTUB()
       }
     })
 
-    await Promise.all(
-      availablePorts.map((port) =>
-        tcpPortUsed
-          .waitUntilUsed(port, 1000, 30000)
-          .then(() => console.log(`E2E: Port ${port} is now started`))
-          .catch((err) => {
-            console.error(`E2E: Port ${port} start failed for reason ${err}`)
-            process.exit(1)
-          })
-      )
-    )
+    await Promise.all(availablePorts.map((port) => waitForPort(port)))
 
     return processes
   }
@@ -86,13 +86,13 @@ const main = async (args: string[]) => {
 
   const { availablePorts } = await checkPorts()
   const newlyStartProcesses = await startServices(availablePorts)
-  const e2eProcess = $({ cwd: DIR.FE, stdout: 'inherit', stderr: 'inherit', shell: true })`${e2eCommand}`
+  const e2eProcess = $({ cwd: DIR.FRONT_END, stdout: 'inherit', stderr: 'inherit', shell: true })`${e2eCommand}`
 
   e2eProcess.on('close', (code: number, signal: string) => {
-    console.log(`E2E: Testing done. Code: ${code}; signal: ${signal}.`)
+    console.log(`Successfully finish E2E testing. Code: ${code}; signal: ${signal}.`)
 
     newlyStartProcesses.forEach((pro) => {
-      console.log(`E2E: clean up process ${pro?.pid}`)
+      console.log(`Start to clean up process ${pro?.pid}`)
       pro?.kill()
     })
 
@@ -100,6 +100,6 @@ const main = async (args: string[]) => {
   })
 }
 
-const [execPath, scriptPath, ...args] = process.argv
+const [_, __, ...args] = process.argv
 
 main(args)

4d14f56ecd96fe868b2ea2204396fd2fa6138ac7
Yunsong <Mikeyangyun@gmail.com>
2023-12-20 16:32:48 +0800

    ADM-681:[backend]feat: put and get metrics data ready

--- backend/src/main/java/heartbeat/handler/AsyncReportRequestHandler.java
+++ backend/src/main/java/heartbeat/handler/AsyncReportRequestHandler.java
@@ -1,10 +1,13 @@
 package heartbeat.handler;
 
+import heartbeat.controller.report.dto.response.MetricsDataReady;
 import heartbeat.controller.report.dto.response.ReportResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.stream.Collectors;
@@ -17,16 +20,37 @@ public class AsyncReportRequestHandler {
 
 	private final Map<String, ReportResponse> reportMap = new ConcurrentHashMap<>();
 
-	public void put(String reportId, ReportResponse e) {
+	private final Map<String, MetricsDataReady> metricsDataReadyMap = new ConcurrentHashMap<>();
+
+	public void putReport(String reportId, ReportResponse e) {
 		reportMap.put(reportId, e);
 	}
 
-	public ReportResponse get(String reportId) {
+	public ReportResponse getReport(String reportId) {
 		return reportMap.remove(reportId);
 	}
 
-	public boolean isReportIsExists(String reportId) {
+	public void putMetricsDataReady(String timeStamp, MetricsDataReady metricsDataReady) {
+		metricsDataReadyMap.put(timeStamp, metricsDataReady);
+	}
+
+	public MetricsDataReady getMetricsDataReady(String timeStamp) {
+		return metricsDataReadyMap.get(timeStamp);
+	}
+
+	public boolean isReportExists(String reportId) {
 		return reportMap.containsKey(reportId);
+	}
+
+	public boolean isReportReady(String timeStamp) {
+		MetricsDataReady metricsDataReady = metricsDataReadyMap.get(timeStamp);
+		if (Objects.isNull(metricsDataReady))
+			return false;
+		Boolean boardMetricsReady = metricsDataReady.getBoardMetricsReady();
+		Boolean pipelineMetricsReady = metricsDataReady.getPipelineMetricsReady();
+		Boolean sourceControlMetricsReady = metricsDataReady.getSourceControlMetricsReady();
+		List<Boolean> metricsReady = List.of(boardMetricsReady, pipelineMetricsReady, sourceControlMetricsReady);
+		return metricsReady.stream().filter(Objects::nonNull).allMatch(Boolean::valueOf);
 	}
 
 	public void deleteExpireReport(long currentTimeStamp) {

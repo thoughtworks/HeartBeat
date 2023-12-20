55ec010cf39a37a6f3d2e11e4142251f01a86869
Yunsong <Mikeyangyun@gmail.com>
2023-12-20 16:29:31 +0800

    ADM-681:[backend]feat: update IdUtil for board and dora

---
+++ backend/src/main/java/heartbeat/util/IdUtil.java
@@ -0,0 +1,17 @@
+package heartbeat.util;
+
+public interface IdUtil {
+
+	String BOARD_REPORT_PREFIX = "board-";
+
+	String DORA_REPORT_PREFIX = "dora-";
+
+	static String getBoardReportId(String timeStamp) {
+		return BOARD_REPORT_PREFIX + timeStamp;
+	}
+
+	static String getDoraReportId(String timeStamp) {
+		return DORA_REPORT_PREFIX + timeStamp;
+	}
+
+}

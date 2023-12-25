18d97b8ac8f8fb0a912caf075fbee9a3b8f0385f
Kai Zhou <kai.zhou@thoughtworks.com>
2023-12-25 16:21:58 +0800

    [kai.zhou][fix]: use mui palette

--- frontend/src/components/Common/DateRangeViewer/style.tsx
+++ frontend/src/components/Common/DateRangeViewer/style.tsx
@@ -1,26 +1,28 @@
 import styled from '@emotion/styled'
 import { ArrowForward, CalendarToday } from '@mui/icons-material'
+import { theme } from '@src/theme'
 
 export const DateRangeContainer = styled.div({
   display: 'flex',
   justifyContent: 'flex-start',
   alignItems: 'center',
-  backgroundColor: '#f3f3f3',
+  backgroundColor: theme.palette.secondary.dark,
   borderRadius: '0.4rem',
-  border: '0.07rem solid #d8d8d8',
+  border: '0.07rem solid',
+  borderColor: theme.palette.grey[400],
   width: 'fit-content',
   padding: '.6rem',
-  color: '#7c7c7c',
+  color: theme.palette.text.disabled,
 })
 
 export const StyledArrowForward = styled(ArrowForward)({
-  color: '#7c7c7c',
+  color: theme.palette.text.disabled,
   margin: '0 .25rem',
   fontSize: '1rem',
 })
 
 export const StyledCalendarToday = styled(CalendarToday)({
-  color: '#7c7c7c',
+  color: theme.palette.text.disabled,
   marginLeft: '.25rem',
   fontSize: '1rem',
 })

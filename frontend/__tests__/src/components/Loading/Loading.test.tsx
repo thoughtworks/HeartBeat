c0b52531f10e73c4d7cbbf05b1832cbf42319368
JiangRu1 <3246736839@qq.com>
2023-12-26 15:27:49 +0800

    ADM-685: [frontend] test: add unit test for loading

--- frontend/__tests__/src/components/Loading/Loading.test.tsx
+++ frontend/__tests__/src/components/Loading/Loading.test.tsx
@@ -8,4 +8,10 @@ describe('Loading', () => {
     expect(container.getElementsByTagName('svg')).toHaveLength(1)
     expect(container.getElementsByTagName('span')[0].getAttribute('role')).toEqual('progressbar')
   })
+
+  it('should show Loading message when has message', () => {
+    const { getByText } = render(<Loading message={'loading...'} />)
+
+    expect(getByText('loading...')).toBeInTheDocument()
+  })
 })

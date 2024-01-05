eb6ef71031887061ff97af192bf98e6c52037eb8
xuebing <xuebing.li@thoughtworks.com>
2024-01-05 09:36:18 +0800

    ADM-652 [frontend] feat: enhance router logic

--- frontend/src/components/Metrics/ReportButtonGroup/style.tsx
+++ frontend/src/components/Metrics/ReportButtonGroup/style.tsx
@@ -18,7 +18,7 @@ export const StyledButtonGroup = styled('div')`
   align-items: center;
   text-align: center;
   margin: 0 auto;
-  justify-content: ${(props) => (props.isShowSaveButton ? 'space-between' : 'flex-end')};
+  justify-content: ${(props) => (props.isFromDetailPage ? 'flex-end' : 'space-between')};
   width: 100%;
   padding-top: 2rem;
 `
--- frontend/src/components/Metrics/ReportStep/BoradMetrics/style.tsx
+++ frontend/src/components/Metrics/ReportStep/BoradMetrics/style.tsx
@@ -1,5 +1,6 @@
 import { styled } from '@mui/material/styles'
 import { theme } from '@src/theme'
+import { Link } from 'react-router-dom'
 
 export const StyledMetricsSection = styled('div')({
   marginTop: '2rem',
@@ -11,8 +12,9 @@ export const StyledTitleWrapper = styled('div')({
   marginBottom: '1rem',
 })
 
-export const StyledShowMore = styled('div')({
+export const StyledShowMore = styled(Link)({
   marginLeft: '0.5rem',
   fontSize: '0.8rem',
+  textDecoration: 'none',
   color: theme.palette.link,
 })

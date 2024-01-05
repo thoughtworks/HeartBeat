cdfc5cd691acb8d93c401a87f193114cf17d80a5
xuebing <xuebing.li@thoughtworks.com>
2024-01-05 11:00:05 +0800

    ADM-652 [frontend] feat: add show more for Dora metrics

--- frontend/src/components/Metrics/ReportStep/DoraMetrics/style.tsx
+++ frontend/src/components/Metrics/ReportStep/DoraMetrics/style.tsx
@@ -1,5 +1,20 @@
 import { styled } from '@mui/material/styles'
+import { Link } from 'react-router-dom'
+import { theme } from '@src/theme'
 
 export const StyledMetricsSection = styled('div')({
   marginTop: '2rem',
+})
+
+export const StyledTitleWrapper = styled('div')({
+  display: 'flex',
+  alignItems: 'center',
+  marginBottom: '1rem',
+})
+
+export const StyledShowMore = styled(Link)({
+  marginLeft: '0.5rem',
+  fontSize: '0.8rem',
+  textDecoration: 'none',
+  color: theme.palette.link,
 })

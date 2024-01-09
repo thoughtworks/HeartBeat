cb4b88c60070793724f7401bef80e902a4eedef7
Nathan Wang <chao.wang3@thoughtworks.com>
2024-01-09 09:48:54 +0800

    ADM-714: [frontend]: feat: add a new client to pass through the error to downstream so that business clients could leverage some more info(like error.response.status).

---
+++ frontend/src/clients/newHttpClient.ts
@@ -0,0 +1,17 @@
+import axios, { AxiosInstance } from 'axios'
+
+export class HttpClient {
+  protected httpTimeout = 300000
+  protected axiosInstance: AxiosInstance
+
+  constructor() {
+    this.axiosInstance = axios.create({
+      baseURL: '/api/v1',
+      timeout: this.httpTimeout,
+    })
+    this.axiosInstance.interceptors.response.use(
+      (res) => res,
+      (error) => error
+    )
+  }
+}

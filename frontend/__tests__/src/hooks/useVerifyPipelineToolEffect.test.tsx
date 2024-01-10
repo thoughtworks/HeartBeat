63f4e1b0e2dc4a5312d0690fcb2542789f104aaa
Nathan Wang <chao.wang3@thoughtworks.com>
2024-01-10 18:35:58 +0800

    ADM-714: [frontend]: test: add test to cover the error message clearance case.

--- frontend/__tests__/src/hooks/useVerifyPipelineToolEffect.test.tsx
+++ frontend/__tests__/src/hooks/useVerifyPipelineToolEffect.test.tsx
@@ -1,4 +1,4 @@
-import { act, renderHook } from '@testing-library/react'
+import { act, renderHook, waitFor } from '@testing-library/react'
 import { useVerifyPipelineToolEffect } from '@src/hooks/useVerifyPipelineToolEffect'
 import { pipelineToolClient } from '@src/clients/pipeline/PipelineToolClient'
 import { MOCK_PIPELINE_VERIFY_REQUEST_PARAMS, VERIFY_FAILED } from '../fixtures'
@@ -25,5 +25,22 @@ describe('use verify pipelineTool state', () => {
     expect(result.current.errorMessage).toEqual(
       `${MOCK_PIPELINE_VERIFY_REQUEST_PARAMS.type} ${VERIFY_FAILED}: error message`
     )
+  })
+
+  it('should clear error message when error message exists', async () => {
+    pipelineToolClient.verifyPipelineTool = jest.fn().mockImplementation(() => {
+      throw new InternalServerException('error message', HttpStatusCode.InternalServerError)
+    })
+    const { result } = renderHook(() => useVerifyPipelineToolEffect())
+
+    act(() => {
+      result.current.verifyPipelineTool(MOCK_PIPELINE_VERIFY_REQUEST_PARAMS)
+    })
+
+    result.current.clearErrorMessage()
+
+    await waitFor(() => {
+      expect(result.current.errorMessage).toEqual('')
+    })
   })
 })

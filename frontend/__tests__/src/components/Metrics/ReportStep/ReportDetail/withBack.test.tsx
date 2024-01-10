1e651450a3004b83383bda063a9d9be59f7d0a96
xuebing <xuebing.li@thoughtworks.com>
2024-01-10 15:15:25 +0800

    ADM-652 [frontend] test: fix test for withBack

--- frontend/__tests__/src/components/Metrics/ReportStep/ReportDetail/withBack.test.tsx
+++ frontend/__tests__/src/components/Metrics/ReportStep/ReportDetail/withBack.test.tsx
@@ -8,19 +8,19 @@ describe('withGoBack', () => {
   afterEach(jest.clearAllMocks)
 
   it('should render a link with back', () => {
-    const Component = withGoBack(() => <div>test1</div>)
+    const Component = withGoBack(() => <div>{'test1'}</div>)
     render(<Component onBack={onBack} />)
     expect(screen.getByText('Back')).toBeInTheDocument()
   })
 
   it('should render the icon', () => {
-    const Component = withGoBack(() => <div>test2</div>)
+    const Component = withGoBack(() => <div>{'test2'}</div>)
     render(<Component onBack={onBack} />)
     expect(screen.getByTestId('ArrowBackIcon')).toBeInTheDocument()
   })
 
   it('should call onBack when the back is clicked', () => {
-    const Component = withGoBack(() => <div>test3</div>)
+    const Component = withGoBack(() => <div>{'test3'}</div>)
     render(<Component onBack={onBack} />)
     fireEvent.click(screen.getByText('Back'))
     expect(onBack).toBeCalled()

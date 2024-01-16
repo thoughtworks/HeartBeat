e4e7f651da5bc3dd02d29c8c91b7ac5c66405fd1
xuebing <xuebing.li@thoughtworks.com>
2024-01-16 15:14:29 +0800

    ADM-728 [frontend] feat: add loading for board detail

--- frontend/src/containers/ReportStep/ReportDetail/board.tsx
+++ frontend/src/containers/ReportStep/ReportDetail/board.tsx
@@ -7,11 +7,19 @@ import { ReportDataWithTwoColumns } from '@src/hooks/reportMapper/reportUIDataSt
 import { Optional } from '@src/utils/types';
 import { withGoBack } from './withBack';
 import { METRICS_TITLE } from '@src/constants/resources';
+import { Loading } from '@src/components/Loading';
+import { styled } from '@mui/material/styles';
 
 interface Property {
   data: ReportResponseDTO;
   onBack: () => void;
 }
+
+export const StyledLoadingWrapper = styled('div')({
+  position: 'relative',
+  height: '5rem',
+  width: '100%',
+});
 
 const showSectionWith2Columns = (title: string, value: Optional<ReportDataWithTwoColumns[]>) =>
   value && <ReportForTwoColumns title={title} data={value} />;
@@ -23,13 +31,17 @@ export const BoardDetail = withGoBack(({ data }: Property) => {
     <>
       {showSectionWith2Columns(METRICS_TITLE.VELOCITY, mappedData.velocityList)}
       {showSectionWith2Columns(METRICS_TITLE.CYCLE_TIME, mappedData.cycleTimeList)}
-      {mappedData.classification && (
+      {mappedData.classification ? (
         <ReportForThreeColumns
           title={METRICS_TITLE.CLASSIFICATION}
           fieldName={'Field Name'}
           listName={'Subtitle'}
           data={mappedData.classification}
         />
+      ) : (
+        <StyledLoadingWrapper>
+          <Loading size='1.5rem' backgroundColor='transparent' />
+        </StyledLoadingWrapper>
       )}
     </>
   );
--- frontend/src/containers/ReportStep/index.tsx
+++ frontend/src/containers/ReportStep/index.tsx
@@ -2,7 +2,7 @@ import React, { useEffect, useLayoutEffect, useState } from 'react';
 import { useGenerateReportEffect } from '@src/hooks/useGenerateReportEffect';
 import { useAppSelector } from '@src/hooks';
 import { isSelectBoardMetrics, isSelectDoraMetrics, selectConfig } from '@src/context/config/configSlice';
-import { MESSAGE, REPORT_PAGE_TYPE } from '@src/constants/resources';
+import { MESSAGE, REPORT_PAGE_TYPE, REQUIRED_DATA } from '@src/constants/resources';
 import { StyledCalendarWrapper, StyledErrorNotification } from '@src/containers/ReportStep/style';
 import { ErrorNotification } from '@src/components/ErrorNotification';
 import { useNavigate } from 'react-router-dom';
@@ -44,12 +44,21 @@ const ReportStep = ({ notification, handleSave }: ReportStepProps) => {
 
   const startDate = configData.basic.dateRange.startDate ?? '';
   const endDate = configData.basic.dateRange.endDate ?? '';
+  const metrics = configData.basic.metrics;
 
   const { updateProps, resetProps } = notification;
   const [errorMessage, setErrorMessage] = useState<string>();
 
   const shouldShowBoardMetrics = useAppSelector(isSelectBoardMetrics);
   const shouldShowDoraMetrics = useAppSelector(isSelectDoraMetrics);
+
+  useEffect(() => {
+    setPageType(
+      metrics.length === 1 && metrics[0] === REQUIRED_DATA.CLASSIFICATION
+        ? REPORT_PAGE_TYPE.BOARD
+        : REPORT_PAGE_TYPE.SUMMARY
+    );
+  }, []);
 
   useLayoutEffect(() => {
     exportValidityTimeMin &&

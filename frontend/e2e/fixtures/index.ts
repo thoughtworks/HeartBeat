4f93e874fe82003138410d343444739b6719696a
Nathan Wang <chao.wang3@thoughtworks.com>
2024-03-11 15:45:49 +0800

    chore: extend the timeout of e2e local assertion because of the poor condition of local api.

--- frontend/e2e/fixtures/index.ts
+++ frontend/e2e/fixtures/index.ts
@@ -4,4 +4,5 @@ export const E2E_PROJECT_NAME = 'E2EProjectName';
 
 export const CONFIG_STEP_SAVING_FILENAME = 'config-step.json';
 export const METRICS_STEP_SAVING_FILENAME = 'metrics-step.json';
-export const E2E_EXPECT_TIMEOUT = 30 * 1000;
+export const E2E_EXPECT_LOCAL_TIMEOUT = 30 * 1000;
+export const E2E_EXPECT_CI_TIMEOUT = 30 * 3 * 1000;

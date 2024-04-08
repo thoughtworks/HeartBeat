eed90dc2ee055d2b0601db4ddfb22877effbf7a7
YaoZhang87 <162096287+YaoZhang87@users.noreply.github.com>
2024-04-08 10:59:59 +0800

    ADM-833: [frontend] fix: fix env error (#1341)
    * ADM-833:[frontend]feat:add e2e unhappy path scenarios
    
    ADM-833: [frontend] add e2e test scenarios
    
    ADM-833: [frontend] fix: fix code style error
    
    ADM-833: [frontend] feat: add unhappy path scenoria
    
    ADM-833: [frontend] feat: optimize add and remove new pipeline,add unhappy path template
    
    ADM-833: [frontend] feat: optimize remove new pipeline,unhappy path template
    
    ADM-833: [frontend] feat: optimize remove new pipeline,unhappy path template
    
    ADM-833:[frontend]feat:merge unhappy path scenario
    
    ADM-833: [frontend] update: update  e2e test scenarios
    
    ADM-833: [frontend] fix: fix code style error
    
    ADM-833: [frontend] update: update  e2e test scenarios
    
    ADM-833: [frontend] update: update  e2e test scenarios
    
    ADM-833: [frontend] fix: fix e2e test error
    
    ADM-833:[frontend]feat:distinguish major and unhappy path test in local environment
    
    ADM-833: [frontend] feat: sperate major and all
    
    ADM-833[frontend]feat:change branch invalid error message
    
    ADM-833: [frontend] fix: fix index.ts error
    
    ADM-833: [frontend] fix: remove the log file
    
    ADM-833: [frontend] fix: fix conflict with main branch
    
    ADM-833: [frontend] fix: revert readme
    
    ADM-833: [frontend] fix: remove log file
    
    ADM-833: [frontend] fix: modify .gitignore, add omit logs
    
    * ADM-833 fix path
    
    * ADM-833: [frontend] fix: update the spec location
    
    * ADM-833: [frontend] fix: fix daterange error
    
    * ADM-833: [frontend] fix: fix review error
    
    * ADM-833: [frontend] fix: fix review error
    
    * ADM-833: [frontend] fix: fix env error
    
    ---------
    
    Co-authored-by: YaoZhang-Daniel <zyao1987@163.com>
    Co-authored-by: guzhongren <guzhongren@live.cn>

--- frontend/scripts/generate-config-files.sh
+++ frontend/scripts/generate-config-files.sh
@@ -29,14 +29,6 @@ cat ./e2e/fixtures/input-files/add-flag-as-block-config-file.template.json > "${
 sed -i -e "s/<E2E_TOKEN_FLAG_AS_BLOCK_JIRA>/${E2E_TOKEN_FLAG_AS_BLOCK_JIRA}/g" "${import_file_name}"
 echo "Successfully generate ${import_file_name}"
 
-import_file_name='./e2e/fixtures/input-files/pipeline-no-org-config-file.json'
-echo "Start to generate ${import_file_name}"
-cat ./e2e/fixtures/input-files/pipeline-no-org-config-file.template.json > "${import_file_name}"
-sed -i -e "s/<E2E_TOKEN_JIRA>/${E2E_TOKEN_JIRA}/g" "${import_file_name}"
-sed -i -e "s/<E2E_TOKEN_PIPELINE_NO_ORG_CONFIG_BUILD_KITE>/${E2E_TOKEN_PIPELINE_NO_ORG_CONFIG_BUILD_KITE}/g" "${import_file_name}"
-sed -i -e "s/<E2E_TOKEN_GITHUB>/${E2E_TOKEN_GITHUB}/g" "${import_file_name}"
-echo "Successfully generate ${import_file_name}"
-
 import_file_name='./e2e/fixtures/input-files/unhappy-path-config-file.json'
 echo "Start to generate ${import_file_name}"
 cat ./e2e/fixtures/input-files/unhappy-path-config-file.template.json > "${import_file_name}"

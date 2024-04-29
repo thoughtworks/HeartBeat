6c7b877f332e219a189e4f585137e37e6ff6ac83
guzhongren <guzhongren@live.cn>
2024-04-29 23:30:16 +0800

    fix(e2e): format

--- frontend/playwright.config.ts
+++ frontend/playwright.config.ts
@@ -35,7 +35,7 @@ export default defineConfig({
     /* Base URL to use in actions like `await page.goto('/')`. */
     baseURL: process.env.APP_ORIGIN,
     viewport: VIEWPORT_DEFAULT,
-    timezoneId: "Asia/Shanghai",
+    timezoneId: 'Asia/Shanghai',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on',

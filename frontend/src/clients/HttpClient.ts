e707f8aa53189f7c7a4e95d7e125dc7d9b3bf8e2
zhou-yinyuan <yinyuan.zhou@thoughtworks.com>
2024-05-09 16:55:50 +0800

    ADM-940 [frontend][backend]: fix the bug about deleting expired file

--- frontend/src/clients/HttpClient.ts
+++ frontend/src/clients/HttpClient.ts
@@ -42,7 +42,10 @@ export class HttpClient {
               throw new ForbiddenError(errorMessage, HttpStatusCode.Forbidden, description);
             default:
               if (status >= 500) {
-                window.location.href = ROUTE.ERROR_PAGE;
+                if (error.response.config.url.startsWith('/reports')) {
+                } else {
+                  window.location.href = ROUTE.ERROR_PAGE;
+                }
                 throw new InternalServerError(errorMessage, status, description);
               }
               throw new UnknownError();
